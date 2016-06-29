2b54c7d5ab62604bd8e7c6e55b90901e7faff68c
briley <brian.riley@ucop.edu>
2016-06-29 13:29:38 -0700

    Added line to have tests auto-run db:migrate

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,9 @@
 ENV["RAILS_ENV"] = "test"
+
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+
+ActiveRecord::Migration.maintain_test_schema!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
