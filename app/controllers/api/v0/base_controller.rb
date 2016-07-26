c1e1c0522d12e8f5011a279af2ac04f2ceb75c94
xsrust <xsrust@gmail.com>
2016-07-26 09:02:21 +0100

    bugfixing auth for api

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -132,6 +132,7 @@ module Api
         #   end
         # end
         OrgTokenPermission.where(organisation_id: @user.organisation_id).find_each do |org_token_permission|
+          logger.debug "#{org_token_permission.token_permission_type.token_type}"
           if org_token_permission.token_permission_type.token_type == auth_type
             auth= true
           end
