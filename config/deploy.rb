fc4df442555b540d8304fd2dafdc738a18ae813c
briley <brian.riley@ucop.edu>
2016-08-08 15:18:52 -0700

    configuring capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,12 +5,13 @@ set :user, 'dmp'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch, 'development'
 
 # Default deploy_to directory is /var/www/my_app_name
 set :deploy_to, '/dmp/apps/roadmap'
 
 # Default value for :scm is :git
-set :scm, :git
+# set :scm, :git
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
@@ -23,16 +24,21 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, 'config/database.yml', 'config/secrets.yml', 'ldap.yml'
+append :linked_files, 'config/database.yml', 'config/secrets.yml'#, 'ldap.yml'
 
 # Default value for linked_dirs is []
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'uploads'
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "/dmp/local/bin:$PATH" }
 
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
+
+#set :deploy_via, :remote_cache
+#set :copy_exclude, ['.git']
+#set :ssh_options, { :forward_agent => true }
+#set :use_sudo, false
 
 namespace :deploy do
   
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,7 +2,7 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
-server 'uc3-roadmap-dev.cdlib.org', user: 'dmp', roles: %w{app db web}, my_property: :my_value
+#server 'uc3-roadmap-dev.cdlib.org', user: 'dmp', roles: %w{app db web}, my_property: :my_value
 
 set :branch, 'dmptool'
 
@@ -14,9 +14,9 @@ set :branch, 'dmptool'
 # property set. Specify the username and a domain or IP for the server.
 # Don't use `:all`, it's a meta role.
 
-# role :app, %w{deploy@example.com}, my_property: :my_value
-# role :web, %w{user1@primary.com user2@additional.com}, other_property: :other_value
-# role :db,  %w{deploy@example.com}
+role :app, %w{uc3-roadmap-dev.cdlib.org}
+role :web, %w{uc3-roadmap-dev.cdlib.org}
+role :db,  %w{uc3-roadmap-dev.cdlib.org}
 
 
 
