a0b2bda259841fd498e5017e49a4c3f1795fdbe7
briley <brian.riley@ucop.edu>
2016-08-08 14:10:59 -0700

    Capified the application

---
+++ config/deploy.rb
@@ -0,0 +1,46 @@
+set :application, 'dmptool'
+set :repo_url, 'https://github.com/CDLUC3/roadmap.git'
+
+set :user, 'dmp'
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/dmp/apps/roadmap'
+
+# Default value for :scm is :git
+set :scm, :git
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+append :linked_files, 'config/database.yml', 'config/secrets.yml', 'ldap.yml'
+
+# Default value for linked_dirs is []
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'uploads'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+namespace :deploy do
+  
+  after :restart, :clear_cache do
+    within release_path do
+      execute :rake, 'cache:clear'
+    end
+  end
+  
+  after :deploy, :cleanup
+end
