3d39c897f412acc36921a0fe3b209ad7db67ffe3
xsrust <xsrust@gmail.com>
2016-08-11 14:08:39 +0100

    installed new gem pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   include GlobalHelpers
+  include Pundit
   
   helper_method GlobalHelpers.instance_methods
 
