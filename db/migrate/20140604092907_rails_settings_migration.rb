a63a206a82e507dba3f78f43a3918f76e00b5962
briley <brian.riley@ucop.edu>
2016-08-11 10:35:18 -0700

    added length specification to a few indexes that are causing travis mysql db setup issues

--- db/migrate/20140604092907_rails_settings_migration.rb
+++ db/migrate/20140604092907_rails_settings_migration.rb
@@ -6,7 +6,7 @@ class RailsSettingsMigration < ActiveRecord::Migration
       t.references :target, :null => false, :polymorphic => true
       t.timestamps
     end
-    add_index :settings, [ :target_type, :target_id, :var ], :unique => true
+    add_index :settings, [ :target_type, :target_id, :var ], :unique => true, :length => {:var => 191}
   end
 
   def self.down
