e4159199ec94df6c245718224406272d8ce30295
xsrust <xsrust@gmail.com>
2016-08-15 14:05:05 +0100

    proof of concept for pundit -got the pundit policy file working -verified that it blocked/allowed users with correct permissions -fixed a bug in user model where the magic strings had been poorly defined -

--- app/controllers/token_permission_types_controller.rb
+++ app/controllers/token_permission_types_controller.rb
@@ -2,14 +2,12 @@ class TokenPermissionTypesController < ApplicationController
 
 
     def index
-        if user_signed_in? && current_user.organisation.token_permission_types.count > 0
-            @user = current_user
-            respond_to do |format|
-                format.html
-            end
-        else
-            render(file: File.join(Rails.root, 'public/403.html'),status: 403, layout: false)
-        end
+      logger.debug "#{current_user}"
+      authorize TokenPermissionType.first
+      @user = current_user
+      respond_to do |format|
+        format.html
+      end
     end
 
 end
