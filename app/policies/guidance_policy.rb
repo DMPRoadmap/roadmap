6883c8ae41763965bc153f2dd0981eed45173082
xsrust <xsrust@gmail.com>
2016-08-16 11:01:27 +0100

    added pundit auth to guidances

---
+++ app/policies/guidance_policy.rb
@@ -0,0 +1,37 @@
+class GuidancePolicy
+  attr_reader :user, :guidance
+
+  def initialize(user, guidance)
+    @user = user
+    @guidance = guidance
+  end
+
+  def admin_show?
+    user.can_modify_guidance?
+  end
+
+  def admin_edit?
+    user.can_modify_guidance?
+  end
+
+  def admin_update?
+    user.can_modify_guidance?
+  end
+
+  def admin_index?
+    user.can_modify_guidance?
+  end
+
+  def admin_new?
+    user.can_modify_guidance?
+  end
+
+  def admin_create?
+    user.can_modify_guidance?
+  end
+
+  def admin_destroy?
+    user.can_modify_guidance?
+  end
+
+end
