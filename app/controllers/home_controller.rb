ea62bbc79679fead53cb59ff2ae723c1e0fa8f8d
xsrust <xsrust@gmail.com>
2016-09-13 14:40:38 +0100

    added auth to home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,16 +1,16 @@
 class HomeController < ApplicationController
+  after_action :verify_authorized
+
   def index
+    authorize User
   	if user_signed_in?
   		name = current_user.name(false)
-  		if name.nil? || name == "" then
+  		if name.blank?
   			redirect_to edit_user_registration_path
   		else
   			redirect_to projects_url
   		end
   	end
-  end
-  
-  def about_us
   end
 
 end
