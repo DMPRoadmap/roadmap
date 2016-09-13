e718059a7d0570986e35798c6a2f8dd824d431f7
xsrust <xsrust@gmail.com>
2016-09-13 14:01:02 +0100

    forced all actions to be authenticated through pundit

--- app/policies/application_policy.rb
+++ app/policies/application_policy.rb
@@ -9,7 +9,7 @@ class ApplicationPolicy
   end
 
   def index?
-    false
+    true
   end
 
   def show?
@@ -17,7 +17,7 @@ class ApplicationPolicy
   end
 
   def create?
-    false
+    true
   end
 
   def new?
@@ -25,7 +25,7 @@ class ApplicationPolicy
   end
 
   def update?
-    false
+    true
   end
 
   def edit?
@@ -33,7 +33,7 @@ class ApplicationPolicy
   end
 
   def destroy?
-    false
+    true
   end
 
   def scope
