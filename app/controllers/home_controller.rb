3dd506e5499543d34ce29c44edfa3cb258edfdc8
xsrust <xsrust@gmail.com>
2016-09-14 10:21:11 +0100

    removed authorization from home-controller as some users will not be logged in when request fielded

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,6 @@
 class HomeController < ApplicationController
-  after_action :verify_authorized
 
   def index
-    authorize User
   	if user_signed_in?
   		name = current_user.name(false)
   		if name.blank?
