9bd0a56d857a0b07c6e8852502999ca0946922cb
briley <brian.riley@ucop.edu>
2016-11-02 15:27:17 -0700

    Corrected spelling of text_desription field in token_permission_types

--- app/models/token_permission_type.rb
+++ app/models/token_permission_type.rb
@@ -1,6 +1,6 @@
 class TokenPermissionType < ActiveRecord::Base
 
-  attr_accessible :token_type, :text_desription, :as => [:default, :admin]
+  attr_accessible :token_type, :text_description, :as => [:default, :admin]
 
   #associations between tables
   #has_and_belongs_to_many :org_token_permissions, join_table: "org_token_permissions"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161021100420) do
+ActiveRecord::Schema.define(version: 20161102221313) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "text"
