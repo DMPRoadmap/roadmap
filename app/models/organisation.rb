c235671388abc5640ac9317d4b249ed2fb1a8b69
xsrust <xsrust@gmail.com>
2016-11-15 11:55:38 +0000

    removed option_warnings table and unused code surrounding the structure

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -9,7 +9,6 @@ class Organisation < ActiveRecord::Base
   has_many :dmptemplates
   has_many :sections
   has_many :users
-  has_many :option_warnings
   has_many :suggested_answers
   has_and_belongs_to_many :token_permission_types, join_table: "org_token_permissions"
 
@@ -146,20 +145,6 @@ class Organisation < ActiveRecord::Base
 			return self
 		else
 			return parent.root
-		end
-	end
-
-  ##
-  # takes in the id of, and returns an OptionWarning
-  #
-  # @param option_id [number] the id of the desired warning
-  # @return [OptionWarning] the specified warning
-	def warning(option_id)
-		warning = option_warnings.find_by_option_id(option_id)
-		if warning.nil? && !parent.nil? then
-			return parent.warning(option_id)
-		else
-			return warning
 		end
 	end
 
