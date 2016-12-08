0d84e54162d57f41dd32a7dbb7bba63053a9170f
briley <brian.riley@ucop.edu>
2016-12-08 12:26:00 -0800

    removed unused link_to_function and link_to_add_object methods from application_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,28 +20,6 @@ module ApplicationHelper
   end
   
   # ---------------------------------------------------------------------------
-  def link_to_add_object(name, f, association, css_class, i)
-    new_object = f.object.class.reflect_on_association(association).klass.new
-    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
-      j = i + 1
-      new_object.number = j
-      render(association.to_s.singularize + "_fields", :f => builder)
-    end
-    link_to_function(name, "add_object(this, \"#{association}\", \"#{escape_javascript(fields)}\")", :class => css_class)
-  end
-  
-  # ---------------------------------------------------------------------------
-  def link_to_function(name, *args, &block)
-    html_options = args.extract_options!.symbolize_keys
-
-    function = block_given? ? update_page(&block) : args[0] || ''
-    onclick = "#{"#{html_options[:onclick]}; " if html_options[:onclick]}#{function}; return false;"
-    href = html_options[:href] || '#'
-
-    content_tag(:a, name, html_options.merge(:href => href, :onclick => onclick))
-  end
-  
-  # ---------------------------------------------------------------------------
   def hash_to_json_object(obj_name, hash)
     "<script type=\"text/javascript\">var #{obj_name} = #{hash.to_json};</script>".html_safe
   end
