4d45d919685dcddc78c350c11c4d1d576feabe4f
xsrust <xsrust@gmail.com>
2016-12-13 11:13:24 +0000

    replaced plan roles with bitflags

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,8 +1,18 @@
 class Role < ActiveRecord::Base
+  include FlagShihTzu
+
   ##
   # Associations
   belongs_to :user
   belongs_to :plan
+
+  ##
+  # Define Bit Field Values
+  # Column access
+  has_flags 1 => :creator,
+            2 => :administrator,
+            3 => :editor,
+            column: 'access'
 
 
 
