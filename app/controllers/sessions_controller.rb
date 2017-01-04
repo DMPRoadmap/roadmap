fe1d5f21ba869b0fbd30214fc1e5fcdf47346a3d
xsrust <xsrust@gmail.com>
2017-01-04 12:13:24 +0000

    code style change in sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,7 @@ class SessionsController < Devise::SessionsController
 
   # Capture the user's shibboleth id if they're coming in from an IDP
   def create
-    existing_user = User.find_by_email(params[:user][:email])
-        
+    existing_user = User.find_by(email: params[:user][:email])
     if !existing_user.nil? && !params[:shibboleth_data].nil? then
       #after authentication verify if session[:shibboleth] exists
       existing_user.update_attributes(shibboleth_id: session[:shibboleth_data][:uid])
