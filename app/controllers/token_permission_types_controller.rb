1f52a74e77d0f02d2cb2e818f537988a9adaeb76
xsrust <xsrust@gmail.com>
2017-01-10 11:42:39 +0000

    updated token_permission_types controller and policy file

--- app/controllers/token_permission_types_controller.rb
+++ app/controllers/token_permission_types_controller.rb
@@ -1,10 +1,12 @@
 class TokenPermissionTypesController < ApplicationController
+  respond_to :html
+
+  ##
+  # GET - Lists all TokenPermissionTypes available to the user
+  # also lists their description
   def index
     authorize TokenPermissionType
     @user = current_user
     @token_types = @user.org.token_permission_types
-    respond_to do |format|
-      format.html
-    end
   end
 end
