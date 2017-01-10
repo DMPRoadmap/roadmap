7ee69a542150bb5ccab63cc48e87024962266e67
xsrust <xsrust@gmail.com>
2017-01-10 10:48:51 +0000

    saved roles_controller changes

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -35,32 +35,27 @@ class RolesController < ApplicationController
 	end
 
 	def update
-    	@role = Role.find(params[:id])
-      authorize @role
-    	@role.access_level = params[:role][:access_level].to_i
-			if @role.update_attributes(params[:role])
-				flash[:notice] = I18n.t('helpers.project.sharing_updated')
-				UserMailer.permissions_change_notification(@role).deliver
-				redirect_to controller: 'plans', action: 'share', id: @role.plan.slug
-			else
-				render action: "edit"
-			end
-  	end
+  	@role = Role.find(params[:id])
+    authorize @role
+  	@role.access_level = params[:role][:access_level].to_i
+		if @role.update_attributes(params[:role])
+			flash[:notice] = I18n.t('helpers.project.sharing_updated')
+			UserMailer.permissions_change_notification(@role).deliver
+			redirect_to controller: 'plans', action: 'share', id: @role.plan.slug
+		else
+			render action: "edit"
+		end
+	end
 
 	def destroy
 		@role = Role.find(params[:id])
     authorize @role
-		if (user_signed_in?) && @role.plan.administerable_by(current_user.id) then
-			user = @role.user
-			plan = @role.plan
-			@role.destroy
-			respond_to do |format|
-				flash[:notice] = I18n.t('helpers.project.access_removed')
-				UserMailer.project_access_removed_notification(user, plan).deliver
-				format.html { redirect_to controller: 'plans', action: 'share', id: @role.plan.slug }
-			end
-		else
-			render(file: File.join(Rails.root, 'public/403.html'), status: 403, layout: false)
-		end
+		user = @role.user
+		plan = @role.plan
+		@role.destroy
+
+		flash[:notice] = I18n.t('helpers.project.access_removed')
+		UserMailer.project_access_removed_notification(user, plan).deliver
+	  redirect_to controller: 'plans', action: 'share', id: @role.plan.slug
 	end
 end
