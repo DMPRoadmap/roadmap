efaedccf8c53c34fe008eb1c25febcbd539a97e1
briley <brian.riley@ucop.edu>
2017-01-24 16:01:26 -0800

    added in new files for public plans page. added code coverage to .gitignore

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170124231538) do
+ActiveRecord::Schema.define(version: 20170124235829) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "text",        limit: 65535
