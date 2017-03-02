d2f7ea8940b4df33e2fff6f4da1ad385416e4fef
xsrust <xsrust@gmail.com>
2017-03-02 09:27:22 +0000

    updated statistics api endpoint to be in line with the new data model

--- app/controllers/api/v0/statistics_controller.rb
+++ app/controllers/api/v0/statistics_controller.rb
@@ -8,19 +8,16 @@ module Api
       # @return a count of users who joined DMPonline between the optional specified dates
       # users are scoped to the organisation of the user initiating the call
       def users_joined
-        if has_auth(constant("token_permission_types.statistics"))
-          users = restrict_date_range(@user.org.users)
-          confirmed_users = []
-          users.each do |user|
-            unless user.confirmed_at.blank?
-              confirmed_users += [user]
-            end
+        raise Pundit::NotAuthorizedError unless Api::V0::StatisticsPolicy.new(@user, :statistics).users_joined?
+        users = restrict_date_range(@user.org.users)
+        confirmed_users = []
+        users.each do |user|
+          unless user.confirmed_at.blank?
+            confirmed_users += [user]
           end
-          @users_count = confirmed_users.count
-          respond_with @users_count
-        else
-          render json: I18n.t("api.no_auth_for_endpoint"), status: 401
         end
+        @users_count = confirmed_users.count
+        respond_with @users_count
       end
 
 
@@ -29,17 +26,10 @@ module Api
       # @return the number of DMPs using the specified template between the optional specified dates
       # ensures that the template is owned/created by the caller's organisation
       def using_template
-        if has_auth(constant("token_permission_types.statistics"))
-          template = Template.find(params[:dmptemplate_id])
-          if template.org == @user.org
-            @template_count = restrict_date_range(template.plans).count
-            respond_with @template_count
-          else
-            #no auth to view statistics for this template
-          end
-        else
-          render json: I18n.t("api.no_auth_for_endpoint"), status: 401
-        end
+        template = Template.find(params[:id])
+        raise Pundit::NotAuthorizedError unless Api::V0::StatisticsPolicy.new(@user, template).using_template?
+        @template_count = restrict_date_range(template.plans).count
+        respond_with @template_count
       end
 
       ##
@@ -48,20 +38,17 @@ module Api
       # the uses are restricted to DMPs created by users of the same organisation
       # as the user who ititiated the call
       def plans_by_template
-        if has_auth(constant("token_permission_types.statistics"))
-          @org_projects = []
-          @user.org.users.each do |user|
-            user.plans.each do |plan|
-              unless @org_projects.include? plan
-                @org_projects += [plan]
-              end
+        raise Pundit::NotAuthorizedError unless Api::V0::StatisticsPolicy.new(@user, :statistics).plans_by_template?
+        @org_projects = []
+        @user.org.users.each do |user|
+          user.plans.each do |plan|
+            unless @org_projects.include? plan
+              @org_projects += [plan]
             end
           end
-          @org_projects = restrict_date_range(@org_projects)
-          respond_with @org_projects
-        else
-          render json: I18n.t("api.no_auth_for_endpoint"), status: 401
         end
+        @org_projects = restrict_date_range(@org_projects)
+        respond_with @org_projects
       end
 
       ##
@@ -70,20 +57,17 @@ module Api
       # DMPs must be owned by a user who's organisation is the same as the user
       # who generates the call
       def plans
-        if has_auth(constant("token_permission_types.statistics"))
-          @org_projects = []
-          @user.org.users.each do |user|
-            user.plans.each do |plan|
-              unless @org_projects.include? plan
-                @org_projects += [plan]
-              end
+        raise Pundit::NotAuthorizedError unless Api::V0::StatisticsPolicy.new(@user, :statistics).plans?
+        @org_plans = []
+        @user.org.users.each do |user|
+          user.plans.each do |plan|
+            unless @org_plans.include? plan
+              @org_plans += [plan]
             end
           end
-          @org_projects = restrict_date_range(@org_projects)
-          respond_with @org_projects
-        else
-          render json: I18n.t("api.no_auth_for_endpoint"), status: 401
         end
+        @org_plans = restrict_date_range(@org_plans)
+        respond_with @org_plans
       end
 
 
