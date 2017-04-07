d291b7124d376696d0552b774c607acf17cd5410
Brian Riley <riley.bri@gmail.com>
2017-04-07 07:50:17 -0700

    Uncommented the show_export method DMPRoadmap/roadmap#227

--- app/policies/plan_policy.rb
+++ app/policies/plan_policy.rb
@@ -28,10 +28,9 @@ class PlanPolicy < ApplicationPolicy
     @plan.readable_by?(@user.id)
   end
 
-# TODO: There is no route to this function on the controller
-#  def show_export?
-#    @plan.readable_by?(@user.id)
-#  end
+  def show_export?
+    @plan.readable_by?(@user.id)
+  end
 
   def update?
     @plan.editable_by?(@user.id)
