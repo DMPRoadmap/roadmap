98e46ca3c57068e33f0eb836558457be7452f84c
Brian Riley <riley.bri@gmail.com>
2017-04-14 13:18:38 -0700

    updated remaining mailers to use fastgettext default locale and fixed spacing issues. DMPRoadmap/roadmap#239

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,26 +3,34 @@ class UserMailer < ActionMailer::Base
 	
 	def sharing_notification(role)
     @role = role
-		mail(to: @role.user.email, 
-         subject: _("You have been given access to a Data Management Plan"))
+    FastGettext.with_locale FastGettext.default_locale do
+  		mail(to: @role.user.email, 
+           subject: _("You have been given access to a Data Management Plan"))
+    end
 	end
 	
 	def permissions_change_notification(role)
 		@role = role
-		mail(to: @role.user.email, 
-         subject: _("DMP permissions changed"))
+		FastGettext.with_locale FastGettext.default_locale do
+      mail(to: @role.user.email, 
+           subject: _("DMP permissions changed"))
+    end
 	end
 	
 	def project_access_removed_notification(user, plan)
 		@user = user
 		@plan = plan
-		mail(to: @user.email, 
-         subject: _("DMP access removed"))
+    FastGettext.with_locale FastGettext.default_locale do
+  		mail(to: @user.email, 
+           subject: _("DMP access removed"))
+    end
 	end
 
   def api_token_granted_notification(user)
       @user = user
-      mail(to: @user.email, 
-           subject: _('API Permission Granted'))
+      FastGettext.with_locale FastGettext.default_locale do
+        mail(to: @user.email, 
+             subject: _('API Permission Granted'))
+      end
   end
 end
