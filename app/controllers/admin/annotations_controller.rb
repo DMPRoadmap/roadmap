ea203a8bc5ebab5fff393873043cdc35a4e79327
Jimmy Angelakos <jimmy.angelakos@ed.ac.uk>
2017-05-19 17:39:38 +0100

    Fix for super admin (new annotation model)

--- app/controllers/admin/annotations_controller.rb
+++ app/controllers/admin/annotations_controller.rb
@@ -5,14 +5,14 @@ module Admin
     #
     # def index
     #   super
-    #   @resources = SuggestedAnswer.
+    #   @resources = Annotation.
     #     page(params[:page]).
     #     per(10)
     # end
 
     # Define a custom finder by overriding the `find_resource` method:
     # def find_resource(param)
-    #   SuggestedAnswer.find_by!(slug: param)
+    #   Annotation.find_by!(slug: param)
     # end
 
     # See https://administrate-prototype.herokuapp.com/customizing_controller_actions
