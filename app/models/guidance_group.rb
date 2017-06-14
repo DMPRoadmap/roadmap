6bacd7b23a8a3b62a705f25b18f60c62c1ea40b2
xsrust <xsrust@gmail.com>
2017-06-14 13:01:28 +0100

    fixes error with prefetching, now passes tests

--- app/models/guidance_group.rb
+++ app/models/guidance_group.rb
@@ -103,7 +103,7 @@ class GuidanceGroup < ActiveRecord::Base
     # @return [Array<GuidanceGroup>] a list of all "viewable" guidance groups to a user
   def self.all_viewable(user)
     # first find all groups owned by the Managing Curation Center
-    managing_org_groups = Org.includes(guidance_groups: [guidance: :themes]).managing_orgs.collect{|org| org.guidance_groups}
+    managing_org_groups = Org.includes(guidance_groups: [guidances: :themes]).managing_orgs.collect{|org| org.guidance_groups}
 
     # find all groups owned by  a Funder organisation
     funder_groups = Org.includes(:guidance_groups).funders.collect{|org| org.guidance_groups}
