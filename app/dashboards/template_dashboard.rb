8b0f32ee60706d7af20701f844bbc1a0a1abbeea
Jimmy Angelakos <vyruss000@gmail.com>
2017-06-30 11:36:40 +0100

    Update template_dashboard.rb

--- app/dashboards/template_dashboard.rb
+++ app/dashboards/template_dashboard.rb
@@ -38,6 +38,8 @@ class TemplateDashboard < Administrate::BaseDashboard
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
   COLLECTION_ATTRIBUTES = [
+    :title,
+    :description,
     :org,
     :plans,
     :phases,
