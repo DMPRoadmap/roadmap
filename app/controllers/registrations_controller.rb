3ae6c30a3c47e8cf6cb66e582705881bd0984872
Jose Lloret <jose.lloret@ed.ac.uk>
2017-07-11 11:34:30 +0100

    FormHelpers for registrations views. DMPRoadmap/roadmap#483
    FormHelpers for plan views. DMPRoadmap/roadmap#483
    
    FormHelpers for shared views. DMPRoadmap/roadmap#483
    
    fixed typo at views/plans/_available_templates.html.erb. DMPRoadmap/roadmap#483

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   def edit
     @user.create_default_preferences if @user.prefs == {}
-    @languages = Language.all.order("name")
+    @languages = Language.sorted_by_abbreviation
     @orgs = Org.where(parent_id: nil).order("name")
     @other_organisations = Org.where(parent_id: nil, is_other: true).pluck(:id)
     @identifier_schemes = IdentifierScheme.where(active: true).order(:name)
