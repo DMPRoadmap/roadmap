d7a8968a8f4f5e01770789c654648e228603357c
xsrust <xsrust@gmail.com>
2017-09-05 12:53:57 +0100

    updated controller and test to reflect each other's behaviors

--- app/controllers/roles_controller.rb
+++ app/controllers/roles_controller.rb
@@ -8,6 +8,7 @@ class RolesController < ApplicationController
     authorize @role
     access_level = params[:role][:access_level].to_i
     set_access_level(access_level)
+    message = ''
     if params[:user].present?
       if @role.plan.owner.present? && @role.plan.owner.email == params[:user]
         flash[:notice] = _('Cannot share plan with %{email} since that email matches with the owner of the plan.') % {email: params[:user]}
@@ -16,13 +17,13 @@ class RolesController < ApplicationController
         if Role.find_by(plan: @role.plan, user: user) # role already exists
           flash[:notice] = _('Plan is already shared with %{email}.') % {email: params[:user]}
         else
-          message = _('Plan shared with %{email}.') % {email: user.email}
           if user.nil?
             registered = false
             User.invite!(email: params[:user])
-            message = _('Invitation to %{email} issued successfully.') % {email: params[:user]}
+            message = _('Invitation to %{email} issued successfully. \n') % {email: params[:user]}
             user = User.find_by(email: params[:user])
           end
+          message += _('Plan shared with %{email}.') % {email: user.email}
           @role.user = user
           if @role.save
             if registered then UserMailer.sharing_notification(@role, current_user).deliver_now end
