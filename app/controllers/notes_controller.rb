be550a81165ca4f96483f82439a2888e3bdf759d
xsrust <xsrust@gmail.com>
2017-10-27 16:58:19 +0100

    updates notes_controller for pre-gened answers, added rake tasks to remove duplicate answers and init answers for all questions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -10,37 +10,31 @@ class NotesController < ApplicationController
     @note.user_id = user_id
     answer_id = params[:new_note][:answer_id]
     question_id = params[:new_note][:question_id]
-    plan_id = params[:new_note][:plan_id]
 
     # create answer if we dont already have one
-    if answer_id.present?
-      answer = Answer.find(answer_id)
-    else
-      answer = Answer.new
-      answer.plan_id = plan_id
-      answer.question_id = question_id
-      answer.user_id = user_id
-      answer.save!
-    end
-
-    @note.answer= answer
-    @note.text = params["#{question_id}new_note_text"]
-
-    authorize @note
-
-    @plan = answer.plan
-    @answer = answer
-    @question = Question.find(question_id)
-
-    if @note.save
-      @status = true
-      @notice = _('Comment was successfully created.')
-    else
-      @status = false
+    if answer_id.blank?
       @notice = failed_create_error(@note, _('note'))
+    else
+      answer = Answer.find(answer_id)
+      @note.answer= answer
+      @note.text = params["#{question_id}new_note_text"]
+
+      authorize @note
+
+      @plan = answer.plan
+      @answer = answer
+      @question = Question.find(question_id)
+
+      if @note.save
+        @status = true
+        @notice = _('Comment was successfully created.')
+      else
+        @status = false
+        @notice = failed_create_error(@note, _('note'))
+      end
+      notes = answer.notes.all
+      @num_notes = notes.count
     end
-    notes = answer.notes.all
-    @num_notes = notes.count
   end
 
 
