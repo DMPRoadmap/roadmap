e5156e75e3201f22c88b717933fb9a59d7a7e694
Jose Lloret <jose.lloret@ed.ac.uk>
2018-01-10 11:03:10 +0000

    user and theme using generic search from paginable. DMPRoadmap/roadmap#815
    search event handlers for paginate.js. DMPRoadmap/roadmap#815
    
    plan using generic search for paginable. DMPRoadmap/roadmap#815

--- app/controllers/paginable/plans_controller.rb
+++ app/controllers/paginable/plans_controller.rb
@@ -3,20 +3,24 @@ class Paginable::PlansController < ApplicationController
   # /paginable/plans/privately_visible/:page
   def privately_visible
     raise Pundit::NotAuthorizedError unless Paginable::PlanPolicy.new(current_user).privately_visible?
-    if params[:page] == 'ALL'
-      plans = current_user.active_plans
+    plans = Plan.active(current_user)
+    if params[:search].present?
+      plans = plans.search(params[:search])
+      plans = params[:page] == 'ALL' ? plans.page(1) : plans.page(params[:page])
     else
-      plans = current_user.active_plans.page(params[:page])
+      plans = params[:page] == 'ALL' ? plans : plans.page(params[:page])
     end
     paginable_renderise(partial: 'privately_visible', scope: plans)
   end
   # GET /paginable/plans/organisationally_or_publicly_visible/:page
   def organisationally_or_publicly_visible
     raise Pundit::NotAuthorizedError unless Paginable::PlanPolicy.new(current_user).organisationally_or_publicly_visible?
-    if params[:page] == 'ALL'
-      plans = Plan.organisationally_or_publicly_visible(current_user)
+    plans = Plan.organisationally_or_publicly_visible(current_user)
+    if params[:search].present?
+      plans = plans.search(params[:search])
+      plans = params[:page] == 'ALL' ? plans.page(1) : plans.page(params[:page])
     else
-      plans = Plan.organisationally_or_publicly_visible(current_user).page(params[:page])
+      plans = params[:page] == 'ALL' ? plans : plans.page(params[:page])
     end
     paginable_renderise(partial: 'organisationally_or_publicly_visible', scope: plans)
   end
