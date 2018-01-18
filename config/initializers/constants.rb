5b1b22ac34481e7249cb1a0ceefeebef0952f56c
briley <brian.riley@ucop.edu>
2018-01-18 10:50:51 -0800

    added ORG LINK limit

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,6 +1,7 @@
 LANGUAGES = (ActiveRecord::Base.connection.table_exists? 'languages') ? Language.sorted_by_abbreviation : []
 MANY_LANGUAGES = LANGUAGES.length > 1
 TABLE_FILTER_MIN_ROWS = 10
+MAX_NUMBER_LINKS_ORG = 3
 MAX_NUMBER_LINKS_FUNDER = 5
 MAX_NUMBER_LINKS_SAMPLE_PLAN = 5
 MAX_NUMBER_THEMES_PER_COLUMN = 5
