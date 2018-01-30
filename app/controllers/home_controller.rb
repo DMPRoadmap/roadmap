1b763715939bdd2cfaab38e558a88d1f24bfd12d
briley <brian.riley@ucop.edu>
2018-01-30 14:42:30 -0800

    css tweaks and home page content changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,16 +22,43 @@ class HomeController < ApplicationController
       end
     end
     
+    # Usage stats
+    stats = Rails.cache.read('stats') || {}
+    if stats.empty?
+      stats[:user_count] = User.select(:id).count
+      stats[:completed_plan_count] = Plan.select(:id).count
+      stats[:institution_count] = Org.select(:id).count
+      cache_content('stats', stats)
+    end
+    
+    # Top 5 templates
+    top_5 = Rails.cache.read('top_5')
+    if top_5.nil?
+      end_date = Date.today
+      start_date = (end_date - 60)
+      ids = Plan.group(:template_id).where('created_at BETWEEN ? AND ?', start_date, end_date).order('count_id DESC').count(:id).keys
+      top_5 = Template.where(id: ids[0..4]).pluck(:title)
+      cache_content('top_5', top_5)
+    end
+    
     # Retrieve/cache the DMPTool blog's latest posts
-    @rss = Rails.cache.read('rss')
-    if @rss.nil?
-      begin
-        rss_xml = open(Rails.application.config.rss).read
-        @rss = RSS::Parser.parse(rss_xml, false).items.first(5)
-        Rails.cache.write("rss", @rss, :expires_in => 15.minutes)
-      rescue Exception => e
-        logger.error("Caught exception RSS parse: #{e}.")
-      end
+    rss = Rails.cache.read('rss')
+    if rss.nil?
+      rss_xml = open(Rails.application.config.rss).read
+      rss = RSS::Parser.parse(rss_xml, false).items.first(5)
+      cache_content('rss', rss)  
+    end
+
+    render 'index', locals: { image: 2, stats: stats, top_5: top_5, rss: rss } 
+  end
+
+
+  private
+  def cache_content(type, data)
+    begin
+      Rails.cache.write(type, data, :expires_in => 15.minutes)
+    rescue Exception => e
+      logger.error("Caught exception RSS parse: #{e}.")
     end
   end
 
