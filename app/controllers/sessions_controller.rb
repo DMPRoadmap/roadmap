38ff88cf2724eac6e2b5e994163c18fff4bbbd98
Jimmy Angelakos <jimmy.angelakos@ed.ac.uk>
2018-02-01 11:08:08 +0000

    New login workflow changes (post-shibboleth authentication) - New dual choice layout in app/views/devise/registrations/new.html.erb - Changes to omniauth_callbacks/application/registrations/sessions controllers to support choice post-shibboleth auth:   - Do you have an account? Sign in to link your shibboleth credentials   - If you don't have an account, sign up and this will link your shibboleth credentials - Changes to sign in, create account, and password details forms for multiple onscreen "show password" tickboxes - Aesthetic change to clear-field (X) icon (align it with input field and bring it inside)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,22 +3,29 @@ class SessionsController < Devise::SessionsController
   def new
     redirect_to(root_path)
   end
+
   # Capture the user's shibboleth id if they're coming in from an IDP
   # ---------------------------------------------------------------------
   def create
     existing_user = User.find_by(email: params[:user][:email])
     if !existing_user.nil?
       
-# TODO: Not sure why we check for shib data in params and then use session value below. We should move this to the 
-#       new user_identifiers table
-      if !params[:shibboleth_data].nil? 
-        #after authentication verify if session[:shibboleth] exists
-        existing_user.update_attributes(shibboleth_id: session[:shibboleth_data][:uid])
+      # Until ORCID login is supported
+      if !session['devise.shibboleth_data'].nil?
+        if u = UserIdentifier.create(identifier_scheme: IdentifierScheme.find_by(name: 'shibboleth'),
+                                 identifier: session['devise.shibboleth_data']['uid'],
+                                 user: existing_user)
+          success = _('Your account has been successfully linked to your institutional credentials. You will now be able to sign in with them.')
+        end
+        existing_user.update_attributes(shibboleth_id: session['devise.shibboleth_data'][:uid])
       end
       session[:locale] = existing_user.get_locale unless existing_user.get_locale.nil?
       set_gettext_locale  #Method defined at controllers/application_controller.rb
     end
     super
+    if success
+      flash[:notice] = success
+    end
   end
 
   def destroy
