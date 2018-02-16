0a12ba4f2f9db6d9eb3b93eab539723e02d642c7
xsrust <xsrust@gmail.com>
2018-02-16 17:33:03 +0000

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -385,8 +385,8 @@ ActiveRecord::Schema.define(version: 20180212124444) do
     t.string   "email",                  default: "", null: false
     t.string   "orcid_id"
     t.string   "shibboleth_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
     t.string   "encrypted_password",     default: ""
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
