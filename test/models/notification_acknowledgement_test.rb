be6b7e238f042e4369983431aa37826d5d8952ed
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-07 11:55:05 +0200

    Added tests for NotificationAcknowledgement model

--- test/models/notification_acknowledgement_test.rb
+++ test/models/notification_acknowledgement_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class NotificationAcknowledgementTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users, :notifications
+
+  test 'has' do
+    assert(NotificationAcknowledgement.has?(
+      users(:super_admin),
+      notifications(:notification_1)
+    ))
+  end
 end
