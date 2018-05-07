7d2b3db43992d0b1d1a573f22dcd28b1ae29f868
FAURE Benjamin <benjamin.faure@inist.fr>
2018-05-07 11:53:06 +0200

    Removed DCC news feed from static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,12 +1,6 @@
 class StaticPagesController < ApplicationController
 
   def about_us
-		dcc_news_feed_url = "http://www.dcc.ac.uk/news/dmponline-0/feed"
-		@dcc_news_feed = Feedjira::Feed.fetch_and_parse dcc_news_feed_url
-		respond_to do |format|
-			format.rss { redirect_to dcc_news_feed_url }
-			format.html
-		end
   end
 
   def contact_us
