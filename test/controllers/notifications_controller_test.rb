dff6ad646ae2e8b16af0f695e1f64d7881ca4bbd
Quentin Sonrel <sudiukil@gmx.fr>
2018-05-07 12:16:28 +0200

    Added testing for unauthorized user in notification controller

--- test/controllers/notifications_controller_test.rb
+++ test/controllers/notifications_controller_test.rb
@@ -1,13 +1,12 @@
 require 'test_helper'
 module SuperAdmin
   class NotificationsControllerTest < ActionController::TestCase
-    fixtures :notifications, :notification_types, :users, :perms
-    self.use_instantiated_fixtures = true
+    fixtures :notifications, :users
     include Devise::Test::ControllerHelpers
 
     setup do
-      sign_in @super_admin
-      @notification = @valid
+      sign_in users(:super_admin)
+      @notification = notifications(:notification_1)
     end
 
     test 'should get index' do
@@ -37,14 +36,10 @@ module SuperAdmin
       assert_redirected_to super_admin_notifications_url
     end
 
-    # test 'should show notification' do
-    #   get :show, id: @notification
-    #   assert_response :success
-    # end
-
     test 'should get edit' do
       get :edit, id: @notification
       assert_response :success
+      assert_not_nil assigns(:notification)
     end
 
     test 'should update notification' do
@@ -66,6 +61,44 @@ module SuperAdmin
       end
 
       assert_redirected_to super_admin_notifications_url
+    end
+
+    test 'unauthorized redirections' do
+      sign_in users(:normal_user)
+
+      get :index
+      assert_redirected_to(plans_url)
+
+      get :new
+      assert_redirected_to(plans_url)
+
+      post :create, notification: {
+        notification_type_id: @notification.notification_type_id,
+        title: @notification.title,
+        level: @notification.level,
+        body: @notification.body,
+        dismissable: @notification.dismissable,
+        starts_at: @notification.starts_at,
+        expires_at: @notification.expires_at
+      }
+      assert_redirected_to(plans_url)
+
+      get :edit, id: @notification
+      assert_redirected_to(plans_url)
+
+      patch :update, id: @notification, notification: {
+        notification_type_id: @notification.notification_type_id,
+        title: @notification.title,
+        level: @notification.level,
+        body: @notification.body,
+        dismissable: @notification.dismissable,
+        starts_at: @notification.starts_at,
+        expires_at: @notification.expires_at
+      }
+      assert_redirected_to(plans_url)
+
+      delete :destroy, id: @notification
+      assert_redirected_to(plans_url)
     end
   end
 end
