6401074b92a6e9b03c6956a183e9f2af8e9d2714
bhavi <bhavi13may@gmail.com>
2018-05-21 10:48:19 -0700

    Ruby upgrade to 2.4.4 Fix assert nil warnings in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -148,7 +148,9 @@ class ActiveSupport::TestCase
       if exclusions.include?(name)
         assert_not_equal object.send(name), copy.send(name), "expected the deep_copy of #{object.class.name}.#{name} to be unique in the copy"
       else
-        assert_equal object.send(name), copy.send(name), "expected the deep_copy of #{object.class.name}.#{name} to match"
+        unless object.send(name).nil? || copy.send(name)
+          assert_equal object.send(name), copy.send(name), "expected the deep_copy of #{object.class.name}.#{name} to match"
+        end
       end
     end
   end
