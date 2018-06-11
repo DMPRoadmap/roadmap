b8f9bdd1cf8f84cad4bafef22638288286d1d946
briley <brian.riley@ucop.edu>
2018-06-11 09:27:24 -0700

    updated sync

--- sync_roadmap.sh
+++ sync_roadmap.sh
@@ -2,13 +2,13 @@ if ! git ls-remote roadmap; then
   echo "No remote found for DMPRoadmap/roadmap ... adding one now"
   git remote add roadmap https://github.com/DMPRoadmap/roadmap.git
 fi
-git fetch origin master
+git fetch origin development
 echo "Fetching latest roadmap branch"
-git fetch origin roadmap_master
-git checkout roadmap_master
-echo "Pulling down latest changes from DMPRoadmap:master"
-git pull roadmap master
+git fetch origin roadmap
+git checkout roadmap
+echo "Pulling down latest changes from DMPRoadmap:development"
+git pull roadmap development
 echo "Pushing updated branch"
-git push origin roadmap_master
-git checkout master
-echo "Switching to the master branch"
+git push origin roadmap
+git checkout development
+echo "Switching to the development branch"
