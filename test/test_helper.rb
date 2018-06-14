21daf94b8b58889bb725e7a9bf7bb407cb548283
briley <brian.riley@ucop.edu>
2018-06-14 08:39:57 -0700

    made contact_email an email input type and added validation to model
    fixed issue with app name in tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -412,7 +412,7 @@ class ActiveSupport::TestCase
     follow_redirects
 
     assert_response :success
-    assert_select 'main h1', _('Welcome to DMPRoadmap.')
+    assert_select 'main h1', _("Welcome to #{Rails.configuration.branding[:application][:name]}.")
   end
 
   # ----------------------------------------------------------------------
