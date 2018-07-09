6026800f38d04642ae5e9b816bcd1066ee700acf
Quentin Sonrel <sudiukil@gmx.fr>
2018-07-09 11:02:19 +0200

    Added template link copy to admin area (organisation templates)

--- app/helpers/template_helper.rb
+++ app/helpers/template_helper.rb
@@ -33,14 +33,14 @@ module TemplateHelper
     a.join(separator)
   end
 
-  def direct_link(template)
+  def direct_link(template, hidden = false)
     params = {
       org_id: template.org.id,
       funder_id: '-1',
       template_id: template.id,
     }
 
-    link_to(plans_url(plan: params), method: :post, title: _('Create plan'), class: 'direct-link') do
+    link_to(plans_url(plan: params), method: :post, title: _('Create plan'), class: 'direct-link', style: hidden ? 'display: none' : '') do
       '<span class="fa fa-plus-square"></span>'.html_safe
     end
   end
