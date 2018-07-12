19f0a5a7054d6ec7f1ac4071d064d7e79ead2928
Bodacious <gavin@katanacode.com>
2018-07-12 16:09:10 +0100

    Add annotate gem for models and Gemfile

--- app/models/user_identifier.rb
+++ app/models/user_identifier.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: user_identifiers
+#
+#  id                   :integer          not null, primary key
+#  identifier           :string
+#  created_at           :datetime
+#  updated_at           :datetime
+#  identifier_scheme_id :integer
+#  user_id              :integer
+#
+# Indexes
+#
+#  index_user_identifiers_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (identifier_scheme_id => identifier_schemes.id)
+#  fk_rails_...  (user_id => users.id)
+#
+
 class UserIdentifier < ActiveRecord::Base
   belongs_to :user
   belongs_to :identifier_scheme
