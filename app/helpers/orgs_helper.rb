94e1f72a1232cc4eb1c1b583904a9640e2571208
Bodacious <gavin@katanacode.com>
2018-07-12 10:39:46 +0100

    Fix how strings are wrapped in translation helper

--- app/helpers/orgs_helper.rb
+++ app/helpers/orgs_helper.rb
@@ -1,19 +1,17 @@
 module OrgsHelper
+  # frozen_string_literal: true
 
-  DEFAULT_EMAIL = '%{organisation_email}'.freeze
+  DEFAULT_EMAIL = '%{organisation_email}'
 
   # Tooltip string for Org feedback form.
   #
   # @param org [Org] The current Org we're updating feedback form for.
   # @return [String] The tooltip message
   def tooltip_for_org_feedback_form(org)
-    output = <<~TEXT
-      Someone will respond to your request within 48 hours. If you have
-      questions pertaining to this action please contact us at
-      %{email}.
-    TEXT
-    email = org.contact_email.present? ? org.contact_email : DEFAULT_EMAIL
-    _(output % { email: email })
+    email = org.contact_email.presence || DEFAULT_EMAIL
+    _("Someone will respond to your request within 48 hours. If you have \
+    questions pertaining to this action please contact us at %{email}") % {
+      email: email
+    }
   end
-
 end
