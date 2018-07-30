ce040312172450a3424f4012372a7b4471cd5cd8
Bodacious <gavin@katanacode.com>
2018-07-30 16:49:24 +0100

    Add integration specs for Sessions

---
+++ spec/features/sessions_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.feature "Sessions", type: :feature do
+
+  let(:user) { create(:user) }
+
+
+  scenario "User signs in successfully with email and password", :js do
+    # Setup
+    visit root_path
+
+    # Action
+    fill_in :signin_user_email, with: user.email
+    fill_in :signin_user_password, with: user.password
+    click_button "Sign in"
+
+    # Expectation
+    expect(current_path).to eql(plans_path)
+    expect(page).to have_text(user.firstname)
+    expect(page).to have_text(user.surname)
+  end
+
+  scenario "User fails sign in with email and password", :js do
+    # Setup
+    visit root_path
+
+    # Action
+    fill_in :signin_user_email, with: user.email
+    fill_in :signin_user_password, with: "rong-password"
+    click_button "Sign in"
+
+    # Expectation
+    expect(current_path).to eql(root_path)
+    expect(page).not_to have_text(user.firstname)
+    expect(page).not_to have_text(user.surname)
+    expect(page).to have_text("Error")
+  end
+
+end
