ac4859446eb9935637f40971b015b24b73142597
Bodacious <gavin@katanacode.com>
2018-07-31 20:11:30 +0100

    Update RSpec suite to include helpers

---
+++ spec/support/helpers/capybara_helper.rb
@@ -0,0 +1,14 @@
+module CapybaraHelper
+  def clear_cookies!
+    browser = Capybara.current_session.driver.browser
+    if browser.respond_to?(:clear_cookies)
+      # Rack::MockSession
+      browser.clear_cookies
+    elsif browser.respond_to?(:manage) and browser.manage.respond_to?(:delete_all_cookies)
+      # Selenium::WebDriver
+      browser.manage.delete_all_cookies
+    else
+      raise "Don't know how to clear cookies. Weird driver?"
+    end
+  end
+end
