b6589e0000a51ce06c3d660a444c14066a7219ed
Bodacious <gavin@katanacode.com>
2018-07-31 20:11:30 +0100

    Update RSpec suite to include helpers

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
-SCREEN_SIZE  = [2400, 1350]
-DIMENSION = Selenium::WebDriver::Dimension.new(*SCREEN_SIZE)
+require_relative "helpers/capybara_helper"
+require_relative "helpers/sessions_helper"
+require_relative "helpers/tiny_mce_helper"
+
+SCREEN_SIZE = [2400, 1350]
+DIMENSION   = Selenium::WebDriver::Dimension.new(*SCREEN_SIZE)
 
 Capybara.default_driver = :rack_test
 
@@ -20,6 +24,12 @@ end
 
 Capybara.configure do |config|
   config.default_max_wait_time = 5 # seconds
+  config.server                = :webrick
+  config.raise_server_errors   = true
 end
 
-Capybara.server = :webrick
+RSpec.configure do |config|
+  config.include(CapybaraHelper, type: :feature)
+  config.include(SessionsHelper, type: :feature)
+  config.include(TinyMceHelper,  type: :feature)
+end
