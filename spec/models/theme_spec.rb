e85581bdb712d5b2e445c317e3db6897be5ac452
Bodacious <gavin@katanacode.com>
2018-08-02 11:48:55 +0100

    Remove unused validation spec from Theme spec

--- spec/models/theme_spec.rb
+++ spec/models/theme_spec.rb
@@ -6,8 +6,6 @@ RSpec.describe Theme, type: :model do
 
     it { is_expected.to validate_presence_of(:title) }
 
-    it { is_expected.to validate_presence_of(:description) }
-
   end
 
   context "associations" do
