8e89adaa8f19b111aaee93f0929735043fc66e8b
Bodacious <gavin@katanacode.com>
2018-08-09 16:24:37 +0100

    Remove ID search from Org abbreviation fix
    The IDs are different on each DB.

--- lib/data_cleanup/rules/org/fix_blank_abbreviation.rb
+++ lib/data_cleanup/rules/org/fix_blank_abbreviation.rb
@@ -14,11 +14,10 @@ module DataCleanup
           if File.exists?(YAML_FILE_PATH)
             YAML.load_file(YAML_FILE_PATH).each do |attributes|
               attributes   = attributes.with_indifferent_access
-              id           = attributes['id']
               name         = attributes['name']
               abbreviation = attributes['abbreviation']
               log("Adding abbreviation #{abbreviation} to Org '#{name}'")
-              ::Org.where(name: name, id: id).update_all(abbreviation: abbreviation)
+              ::Org.where(name: name).update_all(abbreviation: abbreviation)
             end
           else
             raise "Please create a YAML file at #{YAML_FILE_PATH}"
