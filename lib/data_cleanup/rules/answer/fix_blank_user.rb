b6984fd6b9c1c71edcc7f4e0b215f2c387d413c8
Bodacious <gavin@katanacode.com>
2018-08-10 12:28:43 +0100

    Update rule for answers with blank user

--- lib/data_cleanup/rules/answer/fix_blank_user.rb
+++ lib/data_cleanup/rules/answer/fix_blank_user.rb
@@ -13,9 +13,20 @@ module DataCleanup
           ::Answer.joins("LEFT OUTER JOIN users ON users.id = answers.user_id")
                   .where(users: { id: nil })
                   .includes(plan: { roles: :user }).each do |answer|
-            log("Updating Answer##{answer.id} with user: #{answer.plan.owner}")
-            answer.update(user: answer.plan.owner)
+
+            if answer.plan.owner.present?
+              log("Updating Answer##{answer.id} with user: #{answer.plan.owner}")
+              answer.update(user: answer.plan.owner)
+            elsif answer.plan.roles.any?
+              user = answer.plan.roles.first.user
+              log("Updating Answer##{answer.id} with user: #{user}")
+              answer.update(user: user)
+            else
+              log("Destroying orphaned Answer##{answer.id}")
+              answer.destroy
+            end
           end
+
         end
 
       end
