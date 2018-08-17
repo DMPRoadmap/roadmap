14a5f8b9d7df6edb855856a58ba541b461bda862
Bodacious <gavin@katanacode.com>
2018-08-17 17:59:22 +0100

    Add tests for locale setting
    - Adds Faker::Language module for testing languages with real-ish data
    - Refactors global constants into Language model (See Issue#1696)
    - Add specs to test locale change works

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,18 +1,18 @@
-def get_available_locales
-    languages = LANGUAGES # LANGUAGES is defined in config/initializers/constants.rb
-    locales = []
-    languages.each do |l|
-        locales << l.abbreviation
-    end
-    return locales.empty? ? ['en_GB'] : locales 
+def default_locale
+  Language.default.try(:abbreviation) || 'en_GB'
 end
 
-def get_default_locale
-    language = LANGUAGES.empty? ? nil : Language.default()
-    return language.nil? ? 'en_GB' : language.abbreviation
+def available_locales
+  Language.sorted_by_abbreviation.pluck(:abbreviation).presence || [default_locale]
 end
 
-FastGettext.add_text_domain 'app', :path => 'config/locale', :type => :po, :ignore_fuzzy => true, :report_warning => false
-FastGettext.default_text_domain = 'app'
-FastGettext.default_available_locales = get_available_locales()
-FastGettext.default_locale = get_default_locale()
+FastGettext.add_text_domain('app', {
+  path: 'config/locale',
+  type: :po,
+  ignore_fuzzy: true,
+  report_warning: false,
+})
+
+FastGettext.default_text_domain       = 'app'
+FastGettext.default_locale            = default_locale
+FastGettext.default_available_locales = available_locales
