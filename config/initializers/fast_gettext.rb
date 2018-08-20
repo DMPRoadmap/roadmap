34a3ba9ed478540f5cb2995eb0ba54a6d6a59fe3
Bodacious <gavin@katanacode.com>
2018-08-20 11:56:20 +0100

    Add DB check in FastGettext initializer

--- config/initializers/fast_gettext.rb
+++ config/initializers/fast_gettext.rb
@@ -1,9 +1,20 @@
-def default_locale
-  Language.default.try(:abbreviation) || 'en_GB'
-end
+# When Travis runs this, the DB isn't always built yet.
+if Language.table_exists?
+  def default_locale
+    Language.default.try(:abbreviation) || 'en_GB'
+  end
 
-def available_locales
-  Language.sorted_by_abbreviation.pluck(:abbreviation).presence || [default_locale]
+  def available_locales
+    Language.sorted_by_abbreviation.pluck(:abbreviation).presence || [default_locale]
+  end
+else
+  def default_locale
+    Rails.application.config.i18n.available_locales.first
+  end
+
+  def available_locales
+    Rails.application.config.i18n.available_locales
+  end
 end
 
 FastGettext.add_text_domain('app', {
