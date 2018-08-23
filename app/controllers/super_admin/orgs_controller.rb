d178e943ee37ae22d90ca42a325172796c825bce
briri <brian.riley@ucop.edu>
2018-08-23 15:32:29 -0700

    started work on rails model validations for UI

--- app/controllers/super_admin/orgs_controller.rb
+++ app/controllers/super_admin/orgs_controller.rb
@@ -6,32 +6,32 @@ module SuperAdmin
       authorize Org
       render 'index', locals: { orgs: Org.includes(:templates, :users) }
     end
-    
+
     def new
       org = Org.new
       authorize org
       org.links = {"org": []}
-      render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"), 
+      render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"),
                                          method: 'POST', url: super_admin_orgs_path }
     end
-    
+
     def create
       authorize Org
       org = Org.new(org_params)
       org.logo = params[:logo] if params[:logo]
       if params[:org_links].present?
-        org.links = JSON.parse(params[:org_links]) 
+        org.links = JSON.parse(params[:org_links])
       else
         org.links = { org: [] }
       end
-    
+
       begin
         org.funder = params[:funder].present?
         org.institution = params[:institution].present?
-        org.organisation = params[:organisation].present?      
-    
+        org.organisation = params[:organisation].present?
+
         # Handle Shibboleth identifiers if that is enabled
-        if Rails.application.config.shibboleth_use_filtered_discovery_service 
+        if Rails.application.config.shibboleth_use_filtered_discovery_service
           shib = IdentifierScheme.find_by(name: 'shibboleth')
 
           if params[:shib_id].present? || params[:shib_domain].present?
@@ -42,25 +42,25 @@ module SuperAdmin
             )
           end
         end
-      
+
         if org.save
-          redirect_to admin_edit_org_path(org.id), notice: success_message(_('organisation'), _('created'))
+          redirect_to admin_edit_org_path(org.id), notice: success_message(_('created'), _('organisation'))
         else
-          flash[:alert] = failed_create_error(org, _('organisation'))
-          render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"), 
+          flash.now[:alert] = failure_message('create', _('organisation'))
+          render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"),
                                              method: 'POST', url: super_admin_orgs_path }
         end
       rescue Dragonfly::Job::Fetch::NotFound => dflye
         redirect_to admin_edit_org_path(org), alert: _('There seems to be a problem with your logo. Please upload it again.')
-        render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"), 
+        render 'orgs/admin_edit', locals: {org: org, languages: Language.all.order("name"),
                                            method: 'POST', url: super_admin_orgs_path }
       end
     end
-    
+
     def destroy
       org = Org.includes(:users, :templates, :guidance_groups).find(params[:id])
       authorize org
-      
+
       # Only allow the delete if the org has no dependencies
       unless org.users.length > 0 || org.templates.length > 0
         org.guidance_groups.delete_all
@@ -68,16 +68,16 @@ module SuperAdmin
         if org.destroy!
           redirect_to super_admin_orgs_path, notice: success_message(_('organisation'), _('removed'))
         else
-          redirect_to super_admin_orgs_path, alert: failed_destroy_error(org, _('organisation'))
+          redirect_to super_admin_orgs_path, alert: failure_message('remove', _('organisation'))
         end
       end
     end
-    
+
     private
       def org_params
         params.require(:org).permit(:name, :abbreviation, :logo, :contact_email, :contact_name, :remove_logo,
                                     :feedback_enabled, :feedback_email_subject, :feedback_email_msg)
       end
-    
+
   end
 end
--- app/controllers/super_admin/themes_controller.rb
+++ app/controllers/super_admin/themes_controller.rb
@@ -1,74 +1,61 @@
-module SuperAdmin
-  class ThemesController < ApplicationController
-    helper PaginableHelper
-    def index
-      authorize(Theme)
-      render(:index, locals: { themes: Theme.all.page(1) })
-    end
+# frozen_string_literal: true
 
-    def new
-      authorize(Theme)
-      render(:new_edit, locals: { theme: Theme.new, options: { url: super_admin_themes_path, method: :POST, title: _('New Theme') }})
-    end
+class SuperAdmin::ThemesController < ApplicationController
 
-    def create
-      authorize(Theme)
-      begin
-        pparams = permitted_params
-        Theme.create!(pparams)
-        flash[:notice] = _('Theme created successfully')
-      rescue ActionController::ParameterMissing
-        flash[:alert] = _('Unable to save since theme parameter is missing')
-      rescue ActiveRecord::RecordInvalid => e
-        flash[:alert] = e.message
-      end
-      redirect_to(action: :index)
-    end
+  helper PaginableHelper
+  def index
+    authorize(Theme)
+    render(:index, locals: { themes: Theme.all.page(1) })
+  end
 
-    def edit
-      authorize(Theme)
-      begin
-        theme = Theme.find(params[:id])
-        render(:new_edit, locals: { theme: theme, options: { url: super_admin_theme_path(theme), method: :PUT, title: _('Edit Theme') }})
-      rescue ActiveRecord::RecordNotFound
-        flash[:alert] = _('There is no theme associated with id %{id}') % { :id => params[:id] }
-        redirect_to(action: :index)
-      end
-    end
+  def new
+    authorize(Theme)
+    @theme = Theme.new
+  end
 
-    def update
-      authorize(Theme)
-      begin
-        pparams = permitted_params
-        Theme.find(params[:id]).update_attributes!(pparams)
-        flash[:notice] = _('Theme updated successfully')
-      rescue ActiveRecord::RecordNotFound
-        flash[:alert] = _('There is no theme associated with id %{id}') % { :id => params[:id] }
-      rescue ActionController::ParameterMissing
-        flash[:alert] = _('Unable to save since theme parameter is missing')
-      rescue ActiveRecord::RecordInvalid => e
-        flash[:alert] = e.message
-      end
-      redirect_to(action: :index)
-    end
-
-    def destroy
-      authorize(Theme)
-      begin
-        Theme.find(params[:id]).destroy!
-        flash[:notice] = _('Successfully deleted your theme')
-      rescue ActiveRecord::RecordNotFound
-        flash[:alert] = _('There is no theme associated with id %{id}') % { :id => params[:id] }
-      rescue ActiveRecord::RecordNotDestroyed # Unlikely to happen since we don't have callback associated to destroy! but put for safety
-        flash[:alert] = _('The theme with id %{id} could not be destroyed') % { :id => params[:id] }
-      end
-      redirect_to(action: :index)
-    end
-    # Private instance methods
-    private
-
-    def permitted_params
-      params.require(:theme).permit(:title, :description)
+  def create
+    authorize(Theme)
+    @theme = Theme.new(permitted_params)
+    if @theme.save
+      flash.now[:notice] = success_message(_("created"), _("theme"))
+      render :edit
+    else
+      flash.now[:alert] = failure_message(_("create"), _("theme"))
+      render :new
     end
   end
+
+  def edit
+    authorize(Theme)
+    @theme = Theme.find(params[:id])
+  end
+
+  def update
+    authorize(Theme)
+    @theme = Theme.find(params[:id])
+    if @theme.update_attributes(permitted_params)
+      flash.now[:notice] = success_message(_("updated"), _("theme"))
+    else
+      flash.now[:alert] = failure_message(_("update"), _("theme"))
+    end
+    render :edit
+  end
+
+  def destroy
+    authorize(Theme)
+    @theme = Theme.find(params[:id])
+    if @theme.destroy
+      redirect_to super_admin_themes_path, notice: success_message(_("deleted"), _("theme"))
+    else
+      flash.now[:alert] = failure_message(_("delete"), _("theme"))
+      render :edit
+    end
+  end
+  # Private instance methods
+  private
+
+  def permitted_params
+    params.require(:theme).permit(:title, :description)
+  end
+
 end
