585f20ea1c9c8bac8eb6c8715600a289b0744bfc
Bodacious <gavin@katanacode.com>
2018-09-03 17:15:23 +0100

    Fix broken test in org swaps

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,6 +3,7 @@
 require_relative "helpers/capybara_helper"
 require_relative "helpers/sessions_helper"
 require_relative "helpers/tiny_mce_helper"
+require_relative "helpers/combobox_helper"
 
 SCREEN_SIZE = [2400, 1350]
 DIMENSION   = Selenium::WebDriver::Dimension.new(*SCREEN_SIZE)
@@ -46,4 +47,5 @@ RSpec.configure do |config|
   config.include(CapybaraHelper, type: :feature)
   config.include(SessionsHelper, type: :feature)
   config.include(TinyMceHelper,  type: :feature)
+  config.include(ComboboxHelper, type: :feature)
 end
