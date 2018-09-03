345477797e9e58595dee0b5964cd73d68415a07c
Bodacious <gavin@katanacode.com>
2018-09-03 17:20:02 +0100

    Add length constraints to versionable_id

--- db/migrate/20180903104704_add_versionable_id_to_phases_sections_and_questions.rb
+++ db/migrate/20180903104704_add_versionable_id_to_phases_sections_and_questions.rb
@@ -1,9 +1,9 @@
 class AddVersionableIdToPhasesSectionsAndQuestions < ActiveRecord::Migration
   def change
-    add_column :phases, :versionable_id, :string
-    add_column :sections, :versionable_id, :string
-    add_column :questions, :versionable_id, :string
-    add_column :annotations, :versionable_id, :string
+    add_column :phases, :versionable_id, :string, limit: 36
+    add_column :sections, :versionable_id, :string, limit: 36
+    add_column :questions, :versionable_id, :string, limit: 36
+    add_column :annotations, :versionable_id, :string, limit: 36
 
     add_index :phases, :versionable_id
     add_index :sections, :versionable_id
