de17dd74d94c2f58e1d76adf070fc715e10cfc17
Bodacious <gavin@katanacode.com>
2018-09-03 13:01:48 +0100

    Refactor how locales are set in specs

--- spec/support/locales.rb
+++ spec/support/locales.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'shoulda/matchers'
+
+AVAILABLE_TEST_LOCALES = %w[ en en_GB fr de ]
 
 RSpec.configure do |config|
   config.before(:each, type: :feature) do
-    Rails.application.config.i18n.available_locales = ['en', 'en_GB', 'fr', 'de']
+    Rails.application.config.i18n.available_locales = AVAILABLE_TEST_LOCALES
   end
 end
