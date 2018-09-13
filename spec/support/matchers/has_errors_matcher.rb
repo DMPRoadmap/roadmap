1f268e4497741a95c8f6c70486224adfb7a23238
Gavin Morrice <gavin@katanacode.com>
2018-09-13 17:24:49 +0100

    Repair broken or pending specs (#1874)
    - Fix pending Template model specs
    
    - Fix pending model specs
    
    - Fix broken/pending specs, including random "errors" bug

--- spec/support/matchers/has_errors_matcher.rb
+++ spec/support/matchers/has_errors_matcher.rb
@@ -3,7 +3,7 @@ require 'rspec/expectations'
 RSpec::Matchers.define :have_errors do |expected|
 
   match do
-    actual.body.match(/Error/)
+    actual.body.match(/Error\:/)
   end
 
   failure_message do |actual|
