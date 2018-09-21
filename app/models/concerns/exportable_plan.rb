a733427514d1328570748507827e532de56e10ec
xsrust <xsrust@gmail.com>
2018-09-21 13:56:03 +0100

    updated section/question export logic in helper

--- app/models/concerns/exportable_plan.rb
+++ app/models/concerns/exportable_plan.rb
@@ -75,7 +75,7 @@ module ExportablePlan
                        .joins(phases: { sections: { questions: :question_format } })
                        .where(id: self.template_id)
                        .order('sections.number', 'questions.number').first
-
+    hash[:customization] = template.customization_of.present?
     hash[:title] = self.title
     hash[:answers] = self.answers
 
