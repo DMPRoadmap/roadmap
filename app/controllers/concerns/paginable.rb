e939b3b46e6cf2baeb5ceee1b1060a06e6e9f999
Bodacious <gavin@katanacode.com>
2018-09-25 20:13:01 +0100

    Fix bug in paginable table sorting
    - Wasn't flipping direction when clicked
    - JS was loading multiple nested duplicate .paginable objects

--- app/controllers/concerns/paginable.rb
+++ app/controllers/concerns/paginable.rb
@@ -8,6 +8,9 @@ module Paginable
   # Regex to validate sort_field param is safe
   SORT_COLUMN_FORMAT = /[\w\_]+\.[\w\_]/
 
+  PAGINATION_QUERY_PARAMS = [:page, :sort_field, :sort_direction,
+                             :search, :controller, :action]
+
   private
 
   # Renders paginable layout with the partial view passed
@@ -75,24 +78,12 @@ module Paginable
 
   # Returns the base url of the paginable route for a given page passed
   def paginable_base_url(page = 1)
-    url_for(
-      @paginable_path_params.merge(
-        controller: @paginable_params[:controller],
-        action: @paginable_params[:action], page: page
-      )
+    url_params = @paginable_path_params.merge(
+      controller: @paginable_params[:controller],
+      action: @paginable_params[:action],
+      page: page
     )
-  end
-
-  # Returns the base url of the paginable router for a given page passed together with
-  # its query_params. It is used to retain context, i.e. search, sort_field,
-  # sort_direction, etc
-  def paginable_base_url_with_query_params(page: 1, **stringify_query_params_options)
-    base_url = paginable_base_url(page)
-    stringified_query_params = stringify_query_params(stringify_query_params_options)
-    if stringified_query_params.present?
-      return "#{base_url}?#{stringified_query_params}"
-    end
-    base_url
+    url_for(url_params)
   end
 
   # Generates an HTML link to sort given a sort field.
@@ -101,8 +92,9 @@ module Paginable
     link_to(
       sort_link_name(sort_field),
       sort_link_url(sort_field),
-      "data-remote": true,
-      class: "paginable-action", "aria-label": "#{sort_field}"
+      class: "paginable-action",
+      data: { remote: true },
+      aria: { label: sort_field }
     )
   end
 
@@ -114,20 +106,6 @@ module Paginable
   # Determines whether or not the scoped query is paginated or not
   def paginable?
     @refined_scope.respond_to?(:total_pages)
-  end
-
-  # Returns the upcase string (e.g ASC or DESC) if sort_direction param is present in any
-  # of the forms 'asc', 'desc', 'ASC', 'DESC' otherwise returns ASC
-  def upcasing_sort_direction(direction = @paginable_params[:sort_direction])
-    directions = ["asc", "desc", "ASC", "DESC"]
-    directions.include?(direction) ? direction.upcase : "ASC"
-  end
-
-  # Returns DESC when ASC is passed and vice versa, otherwise nil
-  def swap_sort_direction(direction = @paginable_params[:sort_direction])
-    direction_upcased = upcasing_sort_direction(direction)
-    "DESC" if direction_upcased == "ASC"
-    "ASC" if direction_upcased == "DESC"
   end
 
   # Refine a scope passed to this concern if any of the params (search,
@@ -143,7 +121,7 @@ module Paginable
       end
       # Can raise ActiveRecord::StatementInvalid (e.g. column does not
       # exist, ambiguity on column, etc)
-      scope = scope.order("#{@paginable_params[:sort_field]} #{upcasing_sort_direction}")
+      scope = scope.order("#{@paginable_params[:sort_field]} #{sort_direction}")
     end
     if @paginable_params[:page] != "ALL"
       # Can raise error if page is not a number
@@ -152,12 +130,16 @@ module Paginable
     scope
   end
 
+  def sort_direction
+    @sort_direction ||= SortDirection.new(@paginable_params[:sort_direction])
+  end
+
   # Returns the sort link name for a given sort_field. The link name includes
   # html prevented of being escaped
   def sort_link_name(sort_field)
     class_name = "fa-sort"
     if @paginable_params[:sort_field] == sort_field
-      class_name = upcasing_sort_direction == "ASC" ? "fa-sort-asc" : "fa-sort-desc"
+      class_name = "fa-sort-#{sort_direction.downcase}"
     end
     <<~HTML.html_safe
       <i class="fa #{class_name}"
@@ -165,54 +147,45 @@ module Paginable
          style="float: right; font-size: 1.2em;">
 
         <span class="screen-reader-text">
-
-        Sort by #{sort_field.split(".").first}
+          Sort by #{sort_field.split(".").first}
+        </span>
       </i>
     HTML
   end
 
   # Returns the sort url for a given sort_field.
   def sort_link_url(sort_field)
-    page = @paginable_params[:page] == "ALL" ? "ALL" : 1
+    query_params = {}
+    query_params[:page] = @paginable_params[:page] == "ALL" ? "ALL" : 1
+    query_params[:sort_field] = sort_field
     if @paginable_params[:sort_field] == sort_field
-      sort_url = paginable_base_url_with_query_params(
-        page: page,
-        sort_field: sort_field,
-        sort_direction: swap_sort_direction)
+      query_params[:sort_direction] = sort_direction.opposite
     else
-      sort_url = paginable_base_url_with_query_params(
-        page: page,
-        sort_field: sort_field)
+      query_params[:sort_direction] = sort_direction
     end
-    "#{sort_url}#{stringify_nonpagination_query_params}"
+    base_url = paginable_base_url(query_params[:page])
+    sort_url = URI(base_url)
+    sort_url.query = stringify_query_params(query_params)
+    sort_url.to_s
+    "#{sort_url}&#{stringify_nonpagination_query_params}"
   end
 
   # Retrieve any query params that are not a part of the paginable concern
   def stringify_nonpagination_query_params
-    other_params = @paginable_params.select do |param|
-      ![:page, :sort_field, :sort_direction,
-        :search, :controller, :action].include?(param)
-    end
-    if other_params.empty?
-      ""
-    else
-      "&#{other_params.collect { |k, v| "#{k}=#{v}" }.join('&')}"
-    end
+    @paginable_params.except(*PAGINATION_QUERY_PARAMS).to_param
   end
 
-  def stringify_query_params(
-    search: @paginable_params[:search],
+  def stringify_query_params(page: 1, search: @paginable_params[:search],
     sort_field: @paginable_params[:sort_field],
     sort_direction: nil)
 
-    query_string = []
-    query_string << "search=#{search}" if search.present?
+    query_string = {}
+    query_string["search"] = search if search.present?
     if sort_field.present?
-      query_string << "sort_field=#{sort_field}"
-      direction = sort_direction || upcasing_sort_direction
-      query_string << "sort_direction=#{direction}"
+      query_string["sort_field"] = sort_field
+      query_string["sort_direction"] = SortDirection.new(sort_direction)
     end
-    query_string.join("&")
+    query_string.to_param
   end
 
 end
