4a6837eaed307c181794fe0b8bb32f483b895c0d
Brian Riley <brian.riley@ucop.edu>
2018-09-27 03:14:25 -0700

    added default for feedback email subject. updated JS toggle to enable/disable feddback message appropriately (#1895)
    added default for feedback email subject. updated JS toggle to enable/disable feddback message appropriately

--- app/models/org.rb
+++ app/models/org.rb
@@ -31,6 +31,7 @@
 class Org < ActiveRecord::Base
   include ValidationMessages
   include ValidationValues
+  include FeedbacksHelper
   include GlobalHelpers
   include FlagShihTzu
   extend Dragonfly::Model::Validations
@@ -126,6 +127,7 @@ class Org < ActiveRecord::Base
     where("orgs.name LIKE ? OR orgs.contact_email LIKE ?", search_pattern, search_pattern)
   }
 
+  before_validation :set_default_feedback_email_subject
   after_create :create_guidance_group
 
   # EVALUATE CLASS AND INSTANCE METHODS BELOW
@@ -219,6 +221,12 @@ class Org < ActiveRecord::Base
       if logo.height != 100
         self.logo = logo.thumb('x100')  # resize height and maintain aspect ratio
       end
+    end
+  end
+
+  def set_default_feedback_email_subject
+    if self.feedback_enabled? && !self.feedback_email_subject.present?
+      self.feedback_email_subject = feedback_confirmation_default_subject
     end
   end
 
