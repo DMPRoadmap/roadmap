321dd204cabf5bea35d9050398199ede54a0c6c1
briri <brian.riley@ucop.edu>
2018-10-02 11:46:13 -0700

    refactored signin/create account forms to use updated Roadmap codebase

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -120,7 +120,6 @@ class RegistrationsController < Devise::RegistrationsController
         if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_navigational_format?
           sign_up(resource_name, resource)
-<<<<<<< HEAD
 
         # ------------------------------------
         # START DMPTool customization
