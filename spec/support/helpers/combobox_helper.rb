2824b53b101bb9c37db8d4a888979a21778555b5
Bodacious <gavin@katanacode.com>
2018-10-04 19:52:33 +0100

    Update tinyMCE helper to match capybara format

--- spec/support/helpers/combobox_helper.rb
+++ spec/support/helpers/combobox_helper.rb
@@ -2,7 +2,10 @@ module ComboboxHelper
 
   def choose_suggestion(suggestion_text)
     matching_element = all(:css, '.js-suggestion').detect do |element|
-      element.text == suggestion_text
+      element.text.strip == suggestion_text.strip
+    end
+    unless matching_element.present?
+      raise ArgumentError, "No such suggestion with text '#{suggestion_text}'"
     end
     matching_element.click
     # Wait for the JS to run
