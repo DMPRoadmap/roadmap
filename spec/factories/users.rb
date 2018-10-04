b77ab02082a9e1ced464aafdfbdb4f32dee4fda9
Bodacious <gavin@katanacode.com>
2018-10-04 13:09:29 +0100

    Remove uniqueness constraint from user factories

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -49,8 +49,8 @@ FactoryBot.define do
   factory :user do
     org
     language     { Language.first || create(:language) }
-    firstname    { Faker::Name.unique.first_name }
-    surname      { Faker::Name.unique.last_name }
+    firstname    { Faker::Name.first_name }
+    surname      { Faker::Name.last_name }
     email        { Faker::Internet.unique.safe_email }
     password     { "password" }
     accept_terms { true }
