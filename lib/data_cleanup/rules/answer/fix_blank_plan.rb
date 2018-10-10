01f72d3077ec5f5e5cd8d7cdf9b00203460e0561
briri <brian.riley@ucop.edu>
2018-10-10 10:01:54 -0700

    fixed issue with Org abbreviation yaml check

--- lib/data_cleanup/rules/answer/fix_blank_plan.rb
+++ lib/data_cleanup/rules/answer/fix_blank_plan.rb
@@ -3,14 +3,14 @@ module DataCleanup
   module Rules
     # Fix blank user on Answer
     module Answer
-      class FixBlankUser < Rules::Base
+      class FixBlankPlan < Rules::Base
 
         def description
           "Fix blank plan on Answer"
         end
 
         def call
-          ::Answer.where.not(plan_id: Plan.all.collect(&:id)).each do |answer|
+          ::Answer.where.not(plan_id: ::Plan.all.collect(&:id)).each do |answer|
             unless answer.plan.present?
               log("Destroying orphaned Answer##{answer.id}")
               answer.destroy
