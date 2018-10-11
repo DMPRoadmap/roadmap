7fd7edaa5425ef169506b496207dfc670ac6f198
xsrust <xsrust@gmail.com>
2018-10-11 11:11:00 +0100

    added annotations to models for stat objects

--- app/models/stat_joined_user.rb
+++ app/models/stat_joined_user.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: stats
+#
+#  id         :integer          not null, primary key
+#  count      :integer          default(0)
+#  date       :date             not null
+#  type       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  org_id     :integer
+#
+
 class StatJoinedUser < Stat 
   extend OrgDateRangeable
 
