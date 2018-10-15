42e6e87b325ed84cf1ee21bb44eed36802718c59
briri <brian.riley@ucop.edu>
2018-10-15 09:09:30 -0700

    removed unecessary validator from orgidentifier

--- app/models/org_identifier.rb
+++ app/models/org_identifier.rb
@@ -34,8 +34,7 @@ class OrgIdentifier < ActiveRecord::Base
   validates :identifier_scheme_id, uniqueness: { scope: :org_id,
                                                  message: UNIQUENESS_MESSAGE }
 
-  validates :identifier, presence: { message: PRESENCE_MESSAGE },
-                         uniqueness: { message: UNIQUENESS_MESSAGE, scope: org_id }
+  validates :identifier, presence: { message: PRESENCE_MESSAGE }
 
   validates :org, presence: { message: PRESENCE_MESSAGE }
 
