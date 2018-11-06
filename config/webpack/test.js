ddd00c72862b075be8479d1f181065ceff071bca
briri <brian.riley@ucop.edu>
2018-11-06 14:50:32 -0800

    updated karma config. commented out karma tests

--- config/webpack/test.js
+++ config/webpack/test.js
@@ -2,4 +2,16 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'development'
 
 const environment = require('./environment')
 
+environment.plugins.get('Manifest').opts.writeToFileEmit = process.env.NODE_ENV !== 'test'
+
+environment.loaders.append('istanbul-instrumenter', {
+  test: /\.ts$/,
+  enforce: "post",
+  loader: "istanbul-instrumenter-loader",
+  query: {
+    esModules: true
+  },
+  exclude: ["node_modules", /\.test\.ts$/]
+});
+
 module.exports = environment.toWebpackConfig()
