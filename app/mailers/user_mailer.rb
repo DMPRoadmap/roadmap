e972cdb18cf44b2a3e476bebab08899a4f3bcfe1
briri <brian.riley@ucop.edu>
2019-01-22 13:47:58 -0800

    stop sending current user an email when they comment on a plan

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -111,7 +111,15 @@ class UserMailer < ActionMailer::Base
   # commenter - User who wrote the comment
   # plan      - Plan for which the comment is associated to
   def new_comment(commenter, plan)
-    if commenter.is_a?(User) && plan.is_a?(Plan)
+    #------------------------------------------
+    # Start DMPTool Customization
+    #    Do not send the current user an email since they're the one leaving the comment!
+    #------------------------------------------
+    #if commenter.is_a?(User) && plan.is_a?(Plan)
+    if commenter.is_a?(User) && plan.is_a?(Plan) && commenter != current_user
+    #------------------------------------------
+    # End DMPTool Customization
+    #------------------------------------------
       owner = plan.owner
       if owner.present? && owner.active?
         @commenter = commenter
