f43ff9fede5321ab23279fd1fe535db6d5e35611
briri <brian.riley@ucop.edu>
2019-01-22 11:32:01 -0800

    fixed pagination issues
    fixed issue with public plans template sort

--- app/controllers/public_pages_controller.rb
+++ app/controllers/public_pages_controller.rb
@@ -75,7 +75,7 @@ class PublicPagesController < ApplicationController
   # GET /plans_index
   # ------------------------------------------------------------------------------------
   def plan_index
-    @plans = Plan.publicly_visible.page(1)
+    @plans = Plan.publicly_visible.includes(:template).page(1)
     render "plan_index", locals: {
       query_params: {
         sort_field: "plans.updated_at",
