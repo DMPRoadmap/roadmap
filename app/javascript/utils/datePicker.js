26c198c602ec838a4ea237fd956eeb22c12162cf
briri <brian.riley@ucop.edu>
2019-03-06 09:51:30 -0800

    removed comments

--- app/javascript/utils/datePicker.js
+++ app/javascript/utils/datePicker.js
@@ -5,14 +5,12 @@ import 'jquery-ui/ui/widgets/datepicker';
  * Wire up the JQuery UI DatePicker if the browser does not support the HTML5 date
  */
 export default () => {
-  console.log('Init DatePicker');
   if ($('[type="date"]').prop('type') !== 'date') {
-    console.log(`BOOM - WIRING #${$('[type="date"]').attr('id')}`);
-
     $('[type="date"]').datepicker({
       dateFormat: 'yy-mm-dd',
       constrainInput: true,
       navigationAsDateFormat: true,
+      goToCurrent: true
     });
   }
 };
