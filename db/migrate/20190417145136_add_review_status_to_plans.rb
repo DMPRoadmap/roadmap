fcb90056dd26c7333c7f5518d052b693ab726332
Sam Rust <xsrust@gmail.com>
2019-05-06 12:01:14 +0100

    removed unneeded migration

--- db/migrate/20190417145136_add_review_status_to_plans.rb
+++
@@ -1,9 +0,0 @@
-class AddReviewStatusToPlans < ActiveRecord::Migration
-  def up
-    add_column :plans, :review_status, :integer, default: 0, null: false
-  end
-
-  def down
-    remove_column :plans, :review_status
-  end
-end
