b6aff239fd6011bba3dfe5a515cad252dfa0337e
FAURE Benjamin <benjamin.faure@inist.fr>
2019-05-28 14:45:23 +0200

    Removed Private plans from org admin csv download

--- app/controllers/org_admin/plans_controller.rb
+++ app/controllers/org_admin/plans_controller.rb
@@ -43,46 +43,46 @@ class OrgAdmin::PlansController < ApplicationController
     end
   end
 
-  # GET /org_admin/download_plans
-  def download_plans
-    # Test auth directly and throw Pundit error sincePundit
-    # is unaware of namespacing
-    unless current_user.present? && current_user.can_org_admin?
-      raise Pundit::NotAuthorizedError
-    end
+  # # GET /org_admin/download_plans
+  # def download_plans
+  #   # Test auth directly and throw Pundit error sincePundit
+  #   # is unaware of namespacing
+  #   unless current_user.present? && current_user.can_org_admin?
+  #     raise Pundit::NotAuthorizedError
+  #   end
 
-    org = current_user.org
-    file_name = org.name.gsub(/ /, "_")
-                        .gsub(/[\.;,]/, "")
-    header_cols = [
-      "#{_('Project title')}",
-      "#{_('Template')}",
-      "#{_('Organisation')}",
-      "#{_('Owner name')}",
-      "#{_('Owner email')}",
-      "#{_('Updated')}",
-      "#{_('Visibility')}"
-    ]
+  #   org = current_user.org
+  #   file_name = org.name.gsub(/ /, "_")
+  #                       .gsub(/[\.;,]/, "")
+  #   header_cols = [
+  #     "#{_('Project title')}",
+  #     "#{_('Template')}",
+  #     "#{_('Organisation')}",
+  #     "#{_('Owner name')}",
+  #     "#{_('Owner email')}",
+  #     "#{_('Updated')}",
+  #     "#{_('Visibility')}"
+  #   ]
 
-    plans = CSV.generate do |csv|
-      csv << header_cols
-      org.plans.includes(template: :org).order(updated_at: :desc).each do |plan|
-        owner = plan.owner
-        csv << [
-          "#{plan.title}",
-          "#{plan.template.title}",
-          "#{plan.owner.org.present? ? plan.owner.org.name : ''}",
-          "#{plan.owner.name(false)}",
-          "#{plan.owner.email}",
-          "#{l(plan.latest_update.to_date, format: :csv)}",
-          "#{Plan::VISIBILITY_MESSAGE[plan.visibility.to_sym].capitalize}"
-        ]
-      end
-    end
+  #   plans = CSV.generate do |csv|
+  #     csv << header_cols
+  #     org.plans.includes(template: :org).order(updated_at: :desc).each do |plan|
+  #       owner = plan.owner
+  #       csv << [
+  #         "#{plan.title}",
+  #         "#{plan.template.title}",
+  #         "#{plan.owner.org.present? ? plan.owner.org.name : ''}",
+  #         "#{plan.owner.name(false)}",
+  #         "#{plan.owner.email}",
+  #         "#{l(plan.latest_update.to_date, format: :csv)}",
+  #         "#{Plan::VISIBILITY_MESSAGE[plan.visibility.to_sym].capitalize}"
+  #       ]
+  #     end
+  #   end
 
-    respond_to do |format|
-      format.csv  { send_data plans,  filename: "#{file_name}.csv" }
-    end
-  end
+  #   respond_to do |format|
+  #     format.csv  { send_data plans,  filename: "#{file_name}.csv" }
+  #   end
+  # end
 
 end
--- lib/dmpopidor/controllers/org_admin/plans.rb
+++ lib/dmpopidor/controllers/org_admin/plans.rb
@@ -14,6 +14,52 @@ module Dmpopidor
           @feedback_plans = Plan.where(id: feedback_ids)
           @plans = current_user.org.plans.where.not(visibility: [Plan.visibilities[:privately_private_visible], Plan.visibilities[:is_test]])
         end
+
+        # CHANGES
+        # Removed Private plans from download
+        # GET /org_admin/download_plans
+        def download_plans
+          # Test auth directly and throw Pundit error sincePundit
+          # is unaware of namespacing
+          unless current_user.present? && current_user.can_org_admin?
+            raise Pundit::NotAuthorizedError
+          end
+
+          org = current_user.org
+          file_name = org.name.gsub(/ /, "_")
+                              .gsub(/[\.;,]/, "")
+          header_cols = [
+            "#{_('Project title')}",
+            "#{_('Template')}",
+            "#{_('Organisation')}",
+            "#{_('Owner name')}",
+            "#{_('Owner email')}",
+            "#{_('Updated')}",
+            "#{_('Visibility')}"
+          ]
+
+          plans = CSV.generate do |csv|
+            csv << header_cols
+            org.plans
+                .where.not(visibility: Plan.visibilities[:privately_private_visible])
+                .includes(template: :org).order(updated_at: :desc).each do |plan|
+              owner = plan.owner
+              csv << [
+                "#{plan.title}",
+                "#{plan.template.title}",
+                "#{plan.owner.org.present? ? plan.owner.org.name : ''}",
+                "#{plan.owner.name(false)}",
+                "#{plan.owner.email}",
+                "#{l(plan.latest_update.to_date, format: :csv)}",
+                "#{Plan::VISIBILITY_MESSAGE[plan.visibility.to_sym].capitalize}"
+              ]
+            end
+          end
+
+          respond_to do |format|
+            format.csv  { send_data plans,  filename: "#{file_name}.csv" }
+          end
+        end
       end
     end
   end
