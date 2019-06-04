d72ab857a23eabc2c3630a3ab68a7aca8f3fa22e
John Pinto <john.pinto@ed.ac.uk>
2019-06-04 13:06:00 +0100

    Add functionality for School/Department for an organisation.
        Changes:
        - added a Department model whose instances belong to an org and require name.
        - added a DepartmentController and views for create, edit/update and a index view which is a tab
          of 'Organisational Details'.
        - added the optional attribute department_id to User model.
        - added Rspec tests.
        - updated app/controllers/registrations_controller.rb, app/models/org.rb and
          app/models/user.rb to get rid of outstanding Rubocop warnings
    
        Fix for issue #2088.

---
+++ app/models/department.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: departments
+#
+#  id         :integer          not null, primary key
+#  code       :string
+#  name       :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  org_id     :integer
+#
+# Indexes
+#
+#  index_departments_on_org_id  (org_id)
+#
+
+class Department < ActiveRecord::Base
+
+  include ValidationMessages
+
+  belongs_to :org
+
+  has_many :users
+
+  # ===============
+  # = Validations =
+  # ===============
+
+  validates :org, presence: { message: PRESENCE_MESSAGE }
+
+  validates :name, presence: { message: PRESENCE_MESSAGE },
+                      uniqueness: { message: UNIQUENESS_MESSAGE,
+                                    scope: :org_id }
+
+  validates :name, uniqueness: { message: UNIQUENESS_MESSAGE,
+                                 scope: :org_id }
+
+  # Retrieves every department associated to an org
+  scope :by_org, ->(org) { where(org_id: org.id) }
+
+end
