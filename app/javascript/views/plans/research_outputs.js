50d9ad09615ef344df255ccccd9a4d478a249366
FAURE Benjamin <benjamin.faure@inist.fr>
2019-06-25 14:48:52 +0200

    Added edition button on research ouput form

--- app/javascript/views/plans/research_outputs.js
+++ app/javascript/views/plans/research_outputs.js
@@ -12,7 +12,7 @@ $(() => {
   $('#add-research-output').click(() => {
     const lastResearchOutput = $('#research-outputs .research-output-element').last();
     const lastResearchOutputOrder = parseInt(lastResearchOutput.find('.research-output-order').val(), 10);
-    const duplicated = lastResearchOutput.clone(false, false);
+    const duplicated = lastResearchOutput.clone(true, true);
     const duplicatedId = `plan_research_outputs_attributes_${new Date().getTime()}`;
     const duplicatedName = `plan[research_outputs_attributes][${new Date().getTime()}]`;
 
@@ -36,6 +36,7 @@ $(() => {
     duplicated.find('.research-output-type select').attr('name', `${duplicatedName}[research_output_type_id]`);
     duplicated.find('.research-output-type label').attr('for', `${duplicatedId}_research_output_type_id`);
     duplicated.find('.research-output-type select').val(null);
+    duplicated.find('.research-output-type select').trigger('change');
     // Research Output order
     duplicated.find('.research-output-order').attr('id', `${duplicatedId}_order`);
     duplicated.find('.research-output-order').attr('name', `${duplicatedName}[order]`);
@@ -49,9 +50,41 @@ $(() => {
     const parentElement = selectElement.closest('.research-output-element');
     const otherTypeElement = parentElement.find('.research-output-other-type');
     if (selectElement.find('option:selected').data('other')) {
+      console.log('ici');
       otherTypeElement.show();
     } else {
+      console.log('ici');
       otherTypeElement.hide();
     }
+  });
+
+  $('#research-outputs').on('click', ' .research-output-actions .edit', (e) => {
+    const editElement = $(e.target);
+    const parentElement = $(e.target).closest('.research-output-element');
+    const cancelElement = parentElement.find('.cancel');
+    parentElement.find('.input').each((idx, inp) => {
+      $(inp).show();
+    });
+    parentElement.find('span').each((idx, val) => {
+      $(val).hide();
+    });
+
+    editElement.hide();
+    cancelElement.show();
+  });
+
+  $('#research-outputs').on('click', ' .research-output-actions .cancel', (e) => {
+    const cancelElement = $(e.target);
+    const parentElement = $(e.target).closest('.research-output-element');
+    const editElement = parentElement.find('.edit');
+    parentElement.find('.input').each((idx, inp) => {
+      $(inp).hide();
+    });
+    parentElement.find('span').each((idx, val) => {
+      $(val).show();
+    });
+
+    editElement.show();
+    cancelElement.hide();
   });
 });
