79a789418b2c905463d55f1c16ea82d13b1c66ef
Killian Dunne <killian.dunne97@gmail.com>
2019-07-29 14:50:45 +0100

    working on conditional questions, incomplete and contains error(s)

---
+++ app/models/condition.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: conditions
+#
+#  id                 :integer          not null, primary key
+#  action_type        :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  question_option_id :integer
+#  remove_question_id :integer
+#
+# Indexes
+#
+#  index_conditions_on_question_option_id  (question_option_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (question_option_id => question_options.id)
+#
+
+class Condition < ActiveRecord::Base
+  belongs_to :question_option
+  enum action_type: [:remove, :add_webhook]
+end
