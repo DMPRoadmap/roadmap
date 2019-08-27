0a079fd9bcc4793bdfd0d08f30a9c29884f02c74
Killian Dunne <killian.dunne97@gmail.com>
2019-08-27 11:46:38 +0100

    first iteration complete

---
+++ app/javascript/utils/sectionUpdate.js
@@ -0,0 +1,21 @@
+// update details in section progress panel
+export const updateSectionProgress = (id, numSecAnswers, numSecQuestions) => {
+  const progressDiv = $(`#section-panel-${id}`).find('.section-status');
+  progressDiv.html(`(${numSecAnswers} /  ${numSecQuestions})`);
+  const heading =  progressDiv.closest('.panel-heading');
+  const hideColor = 'rgb(211, 211, 211)';
+  if (numSecQuestions === 0) { // disable section if empty
+    if (heading.parent().attr('aria-expanded') === 'true') {
+      heading.parent().trigger('click');
+    }
+    heading.css({ 'background-color': hideColor, cursor: 'auto' });
+    heading.closest('.panel').find(`#collapse-${id}`).hide();
+    heading.closest('.panel').find('i.fa-plus, i.fa-minus').removeClass('fa-plus').removeClass('fa-minus');
+  } else if (heading.css('background-color') === hideColor) { // enable section if questions re-added
+    heading.css({ 'background-color': 'rgb(79,82,83)', cursor: 'pointer' });
+    heading.closest('.panel').find('i[aria-hidden="true"]').addClass('fa-plus');
+    heading.closest('.panel').find('#collapse-' + id).css('display', '');
+  }
+};
+
+export const getQuestionDiv = (id) => $(`#answer-form-${id}`).closest('.row');
