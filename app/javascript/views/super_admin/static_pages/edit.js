77de63b29c60608f13013cc73432d02e06cd6152
FAURE Benjamin <benjamin.faure@inist.fr>
2019-09-16 16:20:37 +0200

    Added code plugin to static page editor

--- app/javascript/views/super_admin/static_pages/edit.js
+++ app/javascript/views/super_admin/static_pages/edit.js
@@ -1,9 +1,12 @@
 import { Tinymce } from '../../../utils/tinymce';
+import 'tinymce/plugins/code';
 
 $(() => {
   Tinymce.init({
     selector: '.content-editor',
     forced_root_block: '',
-    toolbar: 'bold italic underline | formatselect | bullist numlist | link',
+    toolbar: 'bold italic underline | formatselect | bullist numlist | link | code',
+    plugins: 'table autoresize link paste advlist lists code',
+    menubar: 'toolbar',
   });
 });
