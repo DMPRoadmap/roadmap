2fe5645f693b667a3b7362d3fb9915f6db5498e7
John Pinto <john.pinto@ed.ac.uk>
2019-09-19 14:54:21 +0100

    Fix for Accessibility issues with Tables (highlighted by UCOP electronic accessibility review)
    Changes:
      - Added scope="col" to table header tags <th> that were missing them.
      - The Jquery dropdown menus in tables are closed when the table cell
    containing it is out of focus.
    
    (The issue the fontawesome sort icons are read as 'column # link'.
    Add some screen reader only text. This issue was sorted by someone else
    who added ".screen-reader-text" class to the sort_link_name() method in
    app/controllers/concerns/paginable.rb.)
    
    Fix for issue #1455.

---
+++ app/javascript/utils/outOfFocus.js
@@ -0,0 +1,24 @@
+$(() => {
+  // This bit of code currently only closes dropdown menus in a table when the focus is no longer in the containing cell.
+
+  // List of focusable Elements on page
+  const focusables = $('td, .dropdown, .dropdown-toggle, a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, [tabindex], [contenteditable]');
+
+  focusables.each((i, el) => {
+    const focusable = $(el); // JQuery object
+
+    // On entrying a new focusable element we respond to event
+    focusable.focusin(() => {
+
+      $('td').children('.dropdown.open').each((j, elj ) => {
+
+          const td = $(elj).parent(); // DOM Element
+          const dropdownBtn = $(elj).find('.dropdown-toggle');  // JQuery object
+          // Close dropdown menu if the focus is no the table cell containing the dropdown
+          if(!($.contains(td.get(0), focusable.get(0)) || focusable.is(td))) {
+              dropdownBtn.dropdown('toggle');
+          }
+      });
+    });
+  });
+});
