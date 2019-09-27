fc6da5beab50bdf27aa84c00842adb1e223ccec5
briri <brian.riley@ucop.edu>
2019-09-27 07:37:02 -0700

    fixed issue with progress bar totals
    updated logic to allow for phase sepcific progress or total progress
    
    updated plan
    
    Refactored num_answrered_questions

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -486,11 +486,13 @@ class Plan < ActiveRecord::Base
   # The number of answered questions from the entire plan
   #
   # Returns Integer
-  def num_answered_questions
-    Answer.where(id: answers.map(&:id))
-          .includes(:question_options, question: :question_format)
-          .to_a
-          .sum { |answer| answer.is_valid? ? 1 : 0 }
+  def num_answered_questions(phase = nil)
+    return answers.select { |answer| answer.answered? }.length unless phase.present?
+
+    answered = answers.select do |answer|
+      answer.answered? && phase.questions.include?(answer.question)
+    end
+    answered.length
   end
 
   # The number of questions for a plan.
@@ -526,7 +528,7 @@ class Plan < ActiveRecord::Base
                                           question: :question_format)
                                 .where(id: answer_ids)
     num_answers = pre_fetched_answers.reduce(0) do |m, a|
-      m += 1 if a.is_valid?
+      m += 1 if a.answered?
       m
     end
     num_questions == num_answers
