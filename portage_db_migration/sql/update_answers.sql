17a408e95a8c2a8028af2975a638ce49f540594e
piyapongch <charoenw@ualberta.ca>
2019-10-15 14:23:34 -0600

    Add update text field with random wordings sql script

---
+++ portage_db_migration/sql/update_answers.sql
@@ -0,0 +1,11 @@
+begin;
+-- update field text with random wordings and link
+update answers 
+	set text = concat('<p>', str_random_lipsum(5, null, null), ' <a href="http://google.com">'
+		, str_random_lipsum(3, null, null), '</a> ', str_random_lipsum(4, null, null), '</p>')
+	where id > 0;
+select * from answers 
+	limit 100;
+commit;
+
+    
