72d93c2f2bc464812b4ebf6da824daac1f2fa9f8
piyapongch <charoenw@ualberta.ca>
2019-10-16 15:25:06 -0600

    Fix to update only text that has data only

--- portage_db_migration/sql/update_answers.sql
+++ portage_db_migration/sql/update_answers.sql
@@ -3,9 +3,9 @@ begin;
 update answers 
 	set text = concat('<p>', str_random_lipsum(5, null, null), ' <a href="http://google.com">'
 		, str_random_lipsum(3, null, null), '</a> ', str_random_lipsum(4, null, null), '</p>')
-	where id > 0;
+	where id > 0 and not (text = '' or text is null);
 select * from answers 
 	limit 100;
-commit;
+rollback;
 
     
