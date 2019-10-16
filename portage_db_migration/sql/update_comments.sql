2f295358dcede6f0a2cca4a87926eacd9df07905
piyapongch <charoenw@ualberta.ca>
2019-10-16 15:26:07 -0600

    Add update field text on comments table

---
+++ portage_db_migration/sql/update_comments.sql
@@ -0,0 +1,12 @@
+begin;
+-- update field text with random wordings and link
+update comments 
+	set text = concat('<p>', str_random_lipsum(5, null, null), ' <a href="http://google.com">'
+		, str_random_lipsum(3, null, null), '</a> ', str_random_lipsum(4, null, null), '</p>')
+	where id > 0 and not (text = '' or text is null);
+select * from comments 
+	where not (text = '' or text is null)
+	limit 100;
+commit;
+
+    
