11f09b7cd553a4e34c364210a3e54f73cc51c567
FAURE Benjamin <benjamin.faure@inist.fr>
2019-10-30 11:47:17 +0100

    Added color picker & font size select to static pages editor

--- app/javascript/views/super_admin/static_pages/edit.js
+++ app/javascript/views/super_admin/static_pages/edit.js
@@ -1,12 +1,14 @@
 import { Tinymce } from '../../../utils/tinymce';
 import 'tinymce/plugins/code';
+import 'tinymce/plugins/textcolor';
+import 'tinymce/plugins/colorpicker';
 
 $(() => {
   Tinymce.init({
     selector: '.content-editor',
     forced_root_block: '',
-    toolbar: 'bold italic underline | formatselect | bullist numlist | link | code',
-    plugins: 'table autoresize link paste advlist lists code',
+    toolbar: 'bold italic underline | formatselect fontsizeselect forecolor backcolor | bullist numlist | link | code',
+    plugins: 'table autoresize link paste advlist lists code textcolor colorpicker',
     menubar: 'toolbar',
   });
 });
