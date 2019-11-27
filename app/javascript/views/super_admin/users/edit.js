0f846d5b59305a3191a4fec7458190e07235ecff
Sam Rust <xsrust@gmail.com>
2019-11-27 16:07:14 +0000

    update for eslint errors

--- app/javascript/views/super_admin/users/edit.js
+++ app/javascript/views/super_admin/users/edit.js
@@ -4,6 +4,17 @@ $(() => {
   const options = { selector: '#super_admin_user_edit' };
   initOrgSelection(options);
 
+  const updateMergeConfirmation = (userSelect) => {
+    // update the confirmation dialogue with the selected user's email address
+    const editingUserEmail = $('#superadmin_user_email').val();
+    const chosenUserEmail = userSelect.find('option:selected').text();
+    const submitButton = userSelect.closest('form').find(':submit');
+    submitButton.attr('data-confirm',
+      `Confirm Account Merge: The account for ${editingUserEmail} will be merged with ${chosenUserEmail}.
+      All plans and account information for ${chosenUserEmail} will now be accessible via ${editingUserEmail}.
+      The account for ${chosenUserEmail} will then be destroyed.`);
+  };
+
   $('#super_admin_user_edit').on('submit', (e) => {
     // Additional validation to force the user to choose an org or type something for other
     if (!validateOrgSelection(options)) {
@@ -18,17 +29,5 @@ $(() => {
     userSelect.on('change', () => updateMergeConfirmation(userSelect));
     userSelect.change();
   });
-
-  const updateMergeConfirmation = (userSelect) => {
-    // update the confirmation dialogue with the selected user's email address
-    const editingUserEmail = $('#superadmin_user_email').val();
-    const chosenUserEmail = userSelect.find("option:selected").text();
-    const submitButton = userSelect.closest('form').find(':submit');
-    submitButton.attr("data-confirm",
-    `Confirm Account Merge: The account for ${editingUserEmail} will be merged with ${chosenUserEmail}.
-    All plans and account information for ${chosenUserEmail} will now be accessible via ${editingUserEmail}.
-    The account for ${chosenUserEmail} will then be destroyed.`);
-  };
-
 
 });
