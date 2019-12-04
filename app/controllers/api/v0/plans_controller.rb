1cc0b2ee985f4273d109637de233f187a52f8a94
Sam Rust <xsrust@gmail.com>
2019-12-04 16:28:43 +0000

    added helper function to role, move pagination to after filtering

--- app/controllers/api/v0/plans_controller.rb
+++ app/controllers/api/v0/plans_controller.rb
@@ -61,13 +61,13 @@ class Api::V0::PlansController < Api::V0::BaseController
     if params[:per_page].present? && params[:per_page].to_i > Rails.configuration.branding[:application][:api_max_page_size]
       params[:per_page] = Rails.configuration.branding[:application][:api_max_page_size]
     end
-    @plans = paginate @user.org.plans.includes( [ {roles: :user}, {answers: :question_options} ,
+    @plans = @user.org.plans.includes( [ {roles: :user}, {answers: :question_options} ,
       template: [ { phases: { sections: { questions: [:question_format, :themes ] } } }, :org]
       ])
-    
+
     # Filter on list of users
     user_ids = extract_param_list(params, "user")
-    @plans = @plans.where(roles: {user_id: user_ids}) if user_ids.present?
+    @plans = @plans.where(roles: {user_id: user_ids, access: Role.bit_values(:editor)}) if user_ids.present?
     # filter on dates
     if params["created_after"].present? || params["created_before"].present?
       @plans = @plans.where(created_at: dates_to_range(params,"created_after","created_before"))
@@ -81,6 +81,8 @@ class Api::V0::PlansController < Api::V0::BaseController
     # filter on id(s)
     plan_ids = extract_param_list(params, "plan")
     @plans = @plans.where(id: plan_ids ) if plan_ids.present?
+    # apply pagination after filtering
+    @plans = paginate @plans
     respond_with @plans
   end
 
