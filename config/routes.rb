b4f2d32fdf84c449eeb0e26fe4cb606a3a6d325f
briri <brian.riley@ucop.edu>
2020-01-23 08:44:38 -0800

    added identifiers polymorphic table
    finished cleanup of old identifiers
    
    fixed bug
    
    fixed bug and some rubocop issues
    
    added managed lflag to Org
    
    updated code to work with new managed flag
    
    fixed issue with scope
    
    Added lib task for orgs.managed
    
    added a rake db:migrate step to github workflow tests
    
    fixed rubocop issues
    
    removed db:migrate from workflows
    
    fixed bug in upgrade task
    
    removed bad index from schema and replaced with good one
    
    fixed broken tests
    
    initial ROR service and abstract Base service with config and tests
    
    updated tests
    
    rubocop fixes
    
    added webmock exception for chromedriver
    
    Part 1 - maDMP ROR/Fundref - Identifier consolidation (#2361)
    
    * added identifiers polymorphic table
    
    * finished cleanup of old identifiers
    
    * fixed bug
    
    * fixed bug and some rubocop issues
    
    parent b6af5de80cdb5ba63280769a60c60c361c06a703
    author briri <brian.riley@ucop.edu> 1579797878 -0800
    committer briri <brian.riley@ucop.edu> 1580422823 -0800
    
    added identifiers polymorphic table
    
    finished cleanup of old identifiers
    
    fixed bug
    
    fixed bug and some rubocop issues
    
    added managed lflag to Org
    
    updated code to work with new managed flag
    
    fixed issue with scope
    
    Added lib task for orgs.managed
    
    added a rake db:migrate step to github workflow tests
    
    fixed rubocop issues
    
    removed db:migrate from workflows
    
    fixed bug in upgrade task
    
    removed bad index from schema and replaced with good one
    
    fixed broken tests
    
    initial ROR service and abstract Base service with config and tests
    
    updated tests
    
    rubocop fixes
    
    added webmock exception for chromedriver
    
    initial org selection service
    
    pulled in a bunch of changes from other branch
    
    added tests
    
    added controller to rails_helper
    
    fixed typos in upgrade rake
    
    fixed some issues with models and specs
    
    work on org selector
    
    further work on new selection boxes
    
    cleaned up org selection service and added to profile and create plan pages
    
    got all org selectors switched over to new autocomplete
    
    Added contextual flags to identifier_schemes and fixed tests for org model
    
    update scopes on identiifer_scheme for contextual access
    
    cleaned up schema
    
    ran annotate
    
    cleaned up annotations
    
    cleaned up annotationss
    
    added some tests and cleaned up presenters
    
    added tests for hash_to_org_service
    
    finished up refactor of org selection
    
    fixed failing tests
    
    fixed some rubocop issues in new objects
    
    added funder_id and org_id to the plans table
    
    added funder and org associatyions to Plan
    
    updated create plan and project details to use funder
    
    finished up issues with ROR/Fundref
    
    finished ror work
    
    fixed issues
    
    more spec issues
    
    fixed trypo in commented out test
    
    Removed debug statements
    
    fixed tests and added orgselectable to invitiations
    
    updated rake task to use new services
    
    fixed up remaining issues with JS and org selectors
    
    fixed weird bug when filtered_shib_discovery_service is enabled
    
    fixed weird bug when filtered_shib_discovery_service is enabled
    
    fixed some issues saving shib entityId
    
    fixed another shib entityID issue
    
    final fix for shib entityID
    
    updated identifier and identifiable and org_Selector
    
    finish up changes
    
    added doi stub service and moved openaire service
    
    final cleanup
    
    fixes for random failing tests in Travis
    
    api v1 phase 1
    
    added api_clients table and UI
    
    added some missing files
    
    forgot to do a git add before last commit
    
    fixed issue with display name
    
    removed unused paginable. made email required
    
    some fixes to try and help out travis failures
    
    fixed issue with test
    
    minor fixes after further api testing
    
    fixed two test issues
    
    tweaks to scheme names in a few tests to help travis

--- config/routes.rb
+++ config/routes.rb
@@ -163,6 +163,14 @@ Rails.application.routes.draw do
         end
       end
     end
+
+    namespace :v1 do
+      get :heartbeat, controller: "base_api"
+      post :authenticate, controller: "authentication"
+
+      resources :plans, only: [:create, :show]
+      resources :templates, only: [:index]
+    end
   end
 
   namespace :paginable do
@@ -214,6 +222,10 @@ Rails.application.routes.draw do
     resources :departments, only: [] do
       get 'index/:page', action: :index, on: :collection, as: :index
     end
+    # Paginable actions for api_clients
+     resources :api_clients, only: [] do
+       get 'index/:page', action: :index, on: :collection, as: :index
+     end
   end
 
   resources :template_options, only: [:index], constraints: { format: /json/ }
@@ -287,6 +299,12 @@ Rails.application.routes.draw do
       end
     end
     resources :notifications, except: [:show]
+    resources :api_clients do
+       member do
+         get :email_credentials
+         get :refresh_credentials
+       end
+     end
   end
 
   get "research_projects/search", action: "search",
