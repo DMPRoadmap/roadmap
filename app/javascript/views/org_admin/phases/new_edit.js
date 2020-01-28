43564907cb338f87f60f71af3537cde3abe83ff6
FAURE Benjamin <benjamin.faure@inist.fr>
2020-01-28 14:05:36 +0100

    Fixed default answer form

--- app/javascript/views/org_admin/phases/new_edit.js
+++ app/javascript/views/org_admin/phases/new_edit.js
@@ -37,7 +37,8 @@ $(() => {
       initQuestionOption(context);
       addAsterisks(`#${context}`);
       // Swap in the question_formats when the user selects an option based question type
-      $(`#${context} select.question_format`).change((e) => {
+      $(`#${context} .question_format select`).change((e) => {
+        console.log('ici');
         onChangeQuestionFormat(e);
       });
     }
