f3bba9d49600bd708bd371378fe7d66dd58a6fe3
Ray Carrick <ray.carrick@ed.ac.uk>
2020-02-19 10:18:06 +0000

    update factory bot for condition

--- spec/factories/conditions.rb
+++ spec/factories/conditions.rb
@@ -3,26 +3,28 @@
 # Table name: conditions
 #
 #  id                 :integer          not null, primary key
-#  action_type        :integer
+#  question_id        :integer
 #  number             :integer
-#  webhook_data       :string
+#  action_type        :integer
+#  option_list        :text
+#  remove_data        :text
+#  webhook_data       :text
 #  created_at         :datetime         not null
 #  updated_at         :datetime         not null
-#  question_option_id :integer
-#  remove_question_id :integer
 #
 # Indexes
 #
-#  index_conditions_on_question_option_id  (question_option_id)
+#  index_conditions_on_question_id  (question_id)
 #
 # Foreign Keys
 #
-#  fk_rails_...  (question_option_id => question_options.id)
+#  fk_rails_...  (question_id => question.id)
 #
+# 
 
 FactoryBot.define do
   factory :condition do
-    question_option { nil }
-    remove_question_id { 1 }
+    option_list { nil }
+    remove_data { nil }
   end
 end
