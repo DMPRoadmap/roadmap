187b9c6e840b7aafba8fe924798707e10ea813ea
Quentin SONREL <quentin.sonrel@inist.fr>
2020-02-20 17:28:52 +0100

    tmp

--- test.rb
+++ test.rb
@@ -1,7 +1,8 @@
 plan = Plan.find(4351)
-org = plan.templates.org
+org = plan.template.org
 question = plan.template.questions.first
 file = File.open('test.json')
 data = JSON.load(file)
 sds = StructuredDataSchema.create(label: 'test', name: 'test', version: 1, schema: data.to_json, org_id: org.id, object: 'foo')
-question.update(structured: true, structured_data_schema: sds)
+qf = QuestionFormat.create(title: "Structured", description: "foo", structured: true)
+question.update(question_format: qf, structured_data_schema: sds)
