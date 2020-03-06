7526098ffd794590cf9569ec09a8a2232048f373
Sam Rust <xsrust@gmail.com>
2020-03-06 08:36:45 +0000

    attempt at paralellizing stat generation

--- app/services/org/create_created_plan_service.rb
+++ app/services/org/create_created_plan_service.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+import Process
+# need to explicitly import to avoid circular dependancies
+import OrgDateRangeable
+import StatCreatedPlan::CreateOrUpdate
+
 class Org
 
   class CreateCreatedPlanService
@@ -8,16 +13,27 @@ class Org
 
       def call(org = nil)
         orgs = org.nil? ? Org.all : [org]
-
+        threads = []
         orgs.each do |org|
-          OrgDateRangeable.split_months_from_creation(org) do |start_date, end_date|
-            StatCreatedPlan::CreateOrUpdate.do(
-              start_date: start_date,
-              end_date: end_date,
-              org: org
-            )
+          pp "Running #{org.name}"
+          # ensure the number of running threads don't exceed our db connection pool
+          if threads.length == 4
+            # rejoin and remove the longest-running thread
+            threads.first.join
+            threads.shift
+          end
+          t = Process.fork do
+            OrgDateRangeable.split_months_from_creation(org) do |start_date, end_date|
+              StatCreatedPlan::CreateOrUpdate.do(
+                start_date: start_date,
+                end_date: end_date,
+                org: org
+              )
+            end
           end
         end
+        Process.waitall
+
       end
 
     end
