03bff62a126588158db0203dc1333c00e96d93c2
Benjamin FAURE <benjamin.faure@inist.fr>
2020-03-20 16:48:29 +0100

    Users without perms should receive a notification when granted

--- lib/dmpopidor/controllers/users.rb
+++ lib/dmpopidor/controllers/users.rb
@@ -26,6 +26,59 @@ module Dmpopidor
           end
         end
       end
+
+      ##
+      # CHANGES : USERS without perms should receive a perm when granted
+      # POST - updates the permissions for a user
+      # redirects to the admin_index action
+      # should add validation that the perms given are current perms of the current_user
+      def admin_update_permissions
+        @user = User.find(params[:id])
+        authorize @user
+        perms_ids = params[:perm_ids].blank? ? [] : params[:perm_ids].map(&:to_i)
+        perms = Perm.where(id: perms_ids)
+        privileges_changed = false
+        if @user.perms.empty? 
+          @user.perms << perms
+          privileges_changed = true
+        else
+          current_user.perms.each do |perm|
+            if @user.perms.include? perm
+              if ! perms.include? perm
+                @user.perms.delete(perm)
+                if perm.id == Perm.use_api.id
+                  @user.remove_token!
+                end
+                privileges_changed = true
+              end
+            else
+              if perms.include? perm
+                @user.perms << perm
+                if perm.id == Perm.use_api.id
+                  @user.keep_or_generate_token!
+                  privileges_changed = true
+                end
+              end
+            end
+          end
+        end 
+
+        if @user.save
+          if privileges_changed
+            deliver_if(recipients: @user, key: "users.admin_privileges") do |r|
+              UserMailer.admin_privileges(r).deliver_now
+            end
+          end
+          render(json: {
+            code: 1,
+            msg: success_message(perms.first_or_initialize, _("saved")),
+            current_privileges: render_to_string(partial: "users/current_privileges",
+                                                locals: { user: @user }, formats: [:html])
+            })
+        else
+          render(json: { code: 0, msg: failure_message(@user, _("updated")) })
+        end
+      end
     end
   end
 end
