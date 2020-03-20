ecd44a97def287ba30f638f1e23ed48b68ff9d95
Marta Ribeiro <m.ribeiro@live.co.uk>
2020-03-20 13:09:29 +0000

    Removed some spaces and commented out a variable that was not being used. This alloows the tests to run

--- app/javascript/views/super_admin/notifications/edit.js
+++ app/javascript/views/super_admin/notifications/edit.js
@@ -1,6 +1,5 @@
 import { Tinymce } from '../../../utils/tinymce';
-
-//add the info on selecting the check from notification suitable
+// add the info on selecting the check from notification suitable
 import { paginableSelector } from '../../../utils/paginable';
 import * as notifier from '../../../utils/notificationHelper';
 
@@ -16,23 +15,19 @@ $(() => {
 
 
   $(paginableSelector).on('ajax:success', '.set_active_notification', (e, data) => {
-    const form = $(e.target);
+    // const form = $(e.target);
     if (data.code === 1 && data.msg && data.msg !== '') {
       notifier.renderNotice(data.msg);
     } else {
       notifier.renderAlert(data.msg);
     }
-
-
-    //remove or display the div
-    //  if (form.find('input[type="checkbox"]').is(':checked')) {
-  //      form.parent().siblings('.plan-visibility').html(getConstant('PLAN_VISIBILITY_WHEN_TEST'))
-    //      .attr('title', '');
-  //    } else {
-    //    form.parent().siblings('.plan-visibility').html(getConstant('PLAN_VISIBILITY_WHEN_NOT_TEST'))
-    //      .attr('title', getConstant('PLAN_VISIBILITY_WHEN_NOT_TEST_TOOLTIP'));
-    //  }
-
+    /* remove or display the div
+    if (form.find('input[type="checkbox"]').is(':checked')) {
+      form.parent().siblings('.plan-visibility').html(getConstant('PLAN_VISIBILITY_WHEN_TEST'))
+        .attr('title', '');
+     } else {
+       form.parent().siblings('.plan-visibility').html(getConstant('PLAN_VISIBILITY_WHEN_NOT_TEST'))
+        .attr('title', getConstant('PLAN_VISIBILITY_WHEN_NOT_TEST_TOOLTIP'));
+    } */
   });
-
 });
