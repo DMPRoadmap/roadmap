46730b17250e8185ad2ffc91e94a5fc1daae58b0
briri <brian.riley@ucop.edu>
2020-04-09 14:32:57 -0700

    removed filter section from usage dashboard and made charts clickable
    added logic to pagionable versions of controller
    
    Reverted schema.rb

--- app/controllers/paginable/users_controller.rb
+++ app/controllers/paginable/users_controller.rb
@@ -7,11 +7,22 @@ class Paginable::UsersController < ApplicationController
   # /paginable/users/index/:page
   def index
     authorize User
-    if current_user.can_super_admin?
+    @filter = params[:month]
+
+    if current_user.can_super_admin? && !@filter.present?
       scope = User.includes(:roles)
+    elsif @filter.present?
+      # Convert an incoming month from the usage dashboard into a date range query
+      # the month is appended to the query string when a user clicks on a bar in
+      # the users joined chart
+      start_date = Date.parse("#{@filter}-01")
+      scope = current_user.org.users.includes(:roles)
+                          .where("users.created_at BETWEEN ? AND ?",
+                                 start_date.to_s, start_date.end_of_month.to_s)
     else
       scope = current_user.org.users.includes(:roles)
     end
+
     paginable_renderise(
       partial: "index",
       scope: scope,
