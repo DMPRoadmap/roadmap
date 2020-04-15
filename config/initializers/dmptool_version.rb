18663f7699384c5a17ece070667ab855e903b4e9
briri <brian.riley@ucop.edu>
2020-04-15 13:10:26 -0700

    removed version initializer. will place in config repo

--- config/initializers/dmptool_version.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# The capistrano deploy writes out a `.version` file into the root dir
-# If it exists, add it to the configuration so that it will be available
-# on the `views/branded/layout/_footer.html.erb`
-if File.exists?(".version")
-  Rails.configuration.x.dmptool.version = File.read(".version")
-end
