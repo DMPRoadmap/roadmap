82a1944d20abd2b7857ff13cbeabbce6872a848e
briri <brian.riley@ucop.edu>
2020-04-16 11:38:02 -0700

    tweak to webpack erb loader config

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -5,9 +5,7 @@ module.exports = {
   use: [{
     loader: 'rails-erb-loader',
     options: {
-      runner: 'bundle exec bin/rails runner',
+      runner: (/^win/.test(process.platform) ? 'ruby ' : '') + ' bundle exec bin/rails runner',
     }
   }]
 }
-
-// runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner'
