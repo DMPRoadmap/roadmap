e3b0f0403741a44485a89460b5ae61b3d482d694
ray <ray_carrick@yahoo.com>
2020-04-16 13:48:12 +0100

    when question_option deleted remove any condition dependent on it

--- app/controllers/org_admin/question_options_controller.rb
+++ app/controllers/org_admin/question_options_controller.rb
@@ -11,23 +11,26 @@ module OrgAdmin
 
     def destroy
       question_option = QuestionOption.find(params[:id])
+      option_id_to_remove = question_option.id.to_s
       authorize question_option
-#      begin
+      begin
         question_option = get_modifiable(question_option)
-        section = question_option.question.section
+        question = question_option.question
+        section = question.section
         if question_option.destroy!
+          # need to remove any conditions which refer to this question option
+          question.conditions.each do |cond|
+            if cond.option_list.include?(option_id_to_remove)
+              cond.destroy
+            end
+          end
           flash[:notice] = success_message(question_option, _("deleted"))
         else
           flash[:alert] = flash[:alert] = failure_message(question_option, _("delete"))
         end
-#      rescue StandardError => e
-#        flash[:alert] = _("Unable to create a new version of this template.")
-#      end
-#      redirect_to edit_org_admin_template_phase_section_path(
-#        template_id: question.section.phase.template.id,
-#        phase_id: question.section.phase.id,
-#        id: question.section.id
-#      )
+      rescue StandardError => e
+        flash[:alert] = _("Unable to create a new version of this template.")
+      end
       redirect_to edit_org_admin_template_phase_path(
         template_id: section.phase.template.id,
         id: section.phase.id,
