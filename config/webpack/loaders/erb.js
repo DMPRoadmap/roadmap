be0d172894aa20bf950962fa34009012205054b8
briri <brian.riley@ucop.edu>
2020-04-16 12:08:06 -0700

    tweak to rails-erb-loader config

--- config/webpack/loaders/erb.js
+++ config/webpack/loaders/erb.js
@@ -5,7 +5,10 @@ module.exports = {
   use: [{
     loader: 'rails-erb-loader',
     options: {
-      runner: (/^win/.test(process.platform) ? 'bundle exec bin/ruby ' : '') + 'bin/rails runner'
+      runner: (/^win/.test(process.platform) ? '/dmp/local/bin/ruby ' : '') + 'bin/rails runner'
     }
   }]
 }
+
+/* DMPTool Customization - had to be more specific with the location of ruby */
+/* runner: (/^win/.test(process.platform) ? 'ruby ' : '') + 'bin/rails runner', */
