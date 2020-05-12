7e1a7fb381cee2f1ccfbeeb8b620e27f5d9f2186
Benjamin FAURE <benjamin.faure@inist.fr>
2020-05-12 13:51:58 +0200

    Changes to the feedback feature, should display the current_user's org message & feedback plans should appear in all org

--- lib/dmpopidor/controllers/org_admin/plans.rb
+++ lib/dmpopidor/controllers/org_admin/plans.rb
@@ -9,8 +9,8 @@ module Dmpopidor
           unless current_user.present? && current_user.can_org_admin?
             raise Pundit::NotAuthorizedError
           end
-
-          feedback_ids = Role.creator.joins(:user,:plan)
+          
+          feedback_ids = Role.joins(:user,:plan)
             .where('users.org_id = ? AND plans.feedback_requested is TRUE AND roles.active is TRUE',
               current_user.org_id).pluck(:plan_id)
           @feedback_plans = Plan.where(id: feedback_ids).reject{|p| p.nil?}
