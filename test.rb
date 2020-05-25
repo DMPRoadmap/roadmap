b50e0a6ad1eeef58cd8c3042981c7aaae2b96747
Sudiukil <sudiukil@gmx.fr>
2020-05-25 02:53:44 +0200

    [TMP] Updated test script (needs to be moved)

--- test.rb
+++ test.rb
@@ -1,8 +1,11 @@
-plan = Plan.find(4351)
-org = plan.template.org
-question = plan.template.questions.first
-file = File.open('test.json')
-data = JSON.load(file)
-sds = StructuredDataSchema.create(label: 'test', name: 'test', version: 1, schema: data.to_json, org_id: org.id, object: 'foo')
-qf = QuestionFormat.create(title: "Structured", description: "foo", structured: true)
-question.update(question_format: qf, structured_data_schema: sds)
+qf = QuestionFormat.create(title: "Structured", description: 'Structured question', structured: true)
+schemas_dir = File.join(Rails.root, 'config', 'schemas')
+Dir.glob("#{schemas_dir}/*.json") do |path|
+  f = File.open(path)
+  d = JSON.load(f)
+  n = File.basename(path, '.json')
+  StructuredDataSchema.create(label: n, name: n, version: 1, schema: d.to_json, org_id: nil, classname: nil)
+end
+Question.find(19051).update(question_format: qf, structured_data_schema: StructuredDataSchema.find_by(name: 'simple'))
+Question.find(19052).update(question_format: qf, structured_data_schema: StructuredDataSchema.find_by(name: 'occ_simple'))
+Question.find(19053).update(question_format: qf, structured_data_schema: StructuredDataSchema.find_by(name: 'occ_struct'))
