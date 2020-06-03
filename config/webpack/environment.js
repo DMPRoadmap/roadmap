4bd1657af10c7bab54275c86f8df15eb54ac13e4
briri <brian.riley@ucop.edu>
2020-06-03 13:57:29 -0700

    added eslint back in and dropped backup files that were committed by accident

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,8 +1,10 @@
 const { environment } = require('@rails/webpacker');
 const erb = require('./loaders/erb');
 const webpack = require('webpack');
+const eslint = require('./loaders/eslint');
 
 environment.loaders.prepend('erb', erb);
+environment.loaders.prepend('ESLint', eslint);
 
 environment.plugins.append('Provide', new webpack.ProvidePlugin({
   $: 'jquery',
