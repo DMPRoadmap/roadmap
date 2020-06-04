b0f2797b29e8fe68019497273b5ec88e4df4cd61
Benjamin FAURE <benjamin.faure@inist.fr>
2020-06-04 10:16:36 +0200

    Implemented dynamic select field

--- app/helpers/dynamic_form_helper.rb
+++ app/helpers/dynamic_form_helper.rb
@@ -23,15 +23,15 @@ module DynamicFormHelper
   def create_text_field(form, value, name, label, html_class: nil, is_multiple: false, index: 0)
     render partial: 'shared/dynamic_form/fields/text_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label,
-    field_class: html_class,
-    input_type: nil
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      field_class: html_class,
+      input_type: nil
+    }
   end
 
 
@@ -39,15 +39,15 @@ module DynamicFormHelper
   def create_url_field(form, value, name, label, html_class: nil, is_multiple: false, index: 0)
     render partial: 'shared/dynamic_form/fields/text_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label,
-    field_class: html_class,
-    input_type: 'url'
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      field_class: html_class,
+      input_type: 'url'
+    }
   end
 
 
@@ -55,15 +55,15 @@ module DynamicFormHelper
   def create_email_field(form, value, name, label, html_class: nil, is_multiple: false, index: 0)
     render partial: 'shared/dynamic_form/fields/text_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label,
-    field_class: html_class,
-    input_type: 'email'
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      field_class: html_class,
+      input_type: 'email'
+    }
   end
 
 
@@ -71,29 +71,30 @@ module DynamicFormHelper
   def create_date_field(form, value, name, label, html_class: nil, is_multiple: false, index: 0)
     render partial: 'shared/dynamic_form/fields/text_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label,
-    field_class: html_class,
-    input_type: 'date'
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      field_class: html_class,
+      input_type: 'date'
+    }
   end
 
 
 
-  def create_number_field(form, value, name, label, html_class=nil, is_multiple=false, index=0)
+  def create_number_field(form, value, name, label, html_class: nil, is_multiple: false, index: 0)
     render partial: 'shared/dynamic_form/fields/number_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      field_class: html_class
+    }
   end
 
 
@@ -101,35 +102,37 @@ module DynamicFormHelper
   def create_checkbox_field(form, value, name, label, html_class: nil)
     render partial: 'shared/dynamic_form/fields/checkbox_field', 
     locals: {
-    f: form, 
-    field_value: value, 
-    field_name: name, 
-    field_label: label
-  }
+      f: form, 
+      field_value: value, 
+      field_name: name, 
+      field_label: label
+    }
   end
 
-  def create_select_field(form, value, name, properties, html_class: nil)
-    render partial: 'questions/fields/select_field', 
+  def create_select_field(form, value, name, label, select_values, html_class: nil)
+    render partial: 'shared/dynamic_form/fields/select_field', 
     locals: {
-    f: form, 
-    field_value: value, 
-    field_name: name, 
-    field_properties: properties
-  }
+      f: form, 
+      selected_value: value, 
+      field_name: name,
+      field_label: label,
+      select_values: select_values,
+      field_class: html_class
+    }
   end 
 
   def create_schema_field(form, value, name, label, is_multiple=false, index=0, schema_id, answer_id)
     render partial: 'questions/fields/schema_field', 
     locals: {
-    f: form, 
-    multiple: is_multiple,
-    index: index,
-    field_value: value, 
-    field_name: name, 
-    field_label: label,
-    schema_id: schema_id,
-    answer_id: answer_id,
-  }
+      f: form, 
+      multiple: is_multiple,
+      index: index,
+      field_value: value, 
+      field_name: name, 
+      field_label: label,
+      schema_id: schema_id,
+      answer_id: answer_id,
+    }
   end
 
 end
