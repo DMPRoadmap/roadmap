6163fece1be414de91f7f00eb7875c8c50afb695
briri <brian.riley@ucop.edu>
2020-06-09 14:14:35 -0700

    tweaks to config to use default webpacker uglifier/minifier

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -1,6 +1,5 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 
 const environment = require('./environment');
-environment.plugins.get("UglifyJs").options.uglifyOptions.ecma = 5;
 
 module.exports = environment.toWebpackConfig();
