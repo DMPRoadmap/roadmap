73ef9142cc4b0ffc0b38fdbbcc36106dca90a01d
Sam Rust <xsrust@gmail.com>
2020-06-11 20:31:51 +0100

    fix error in previous commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,30 +9,30 @@ class SessionsController < Devise::SessionsController
   # Capture the user's shibboleth id if they're coming in from an IDP
   # ---------------------------------------------------------------------
   def create
-    super
-    if success
-      flash[:notice] = success
-      existing_user = User.find_by(email: params[:user][:email])
-      if !existing_user.nil?
+    existing_user = User.find_by(email: params[:user][:email])
+    if !existing_user.nil?
 
-        # Until ORCID login is supported
-        if !session["devise.shibboleth_data"].nil?
-          args = {
-            identifier_scheme: IdentifierScheme.find_by(name: "shibboleth"),
-            identifier: session["devise.shibboleth_data"]["uid"],
-            user: existing_user
-          }
-          if UserIdentifier.create(args)
-            # rubocop:disable Metrics/LineLength
-            success = _("Your account has been successfully linked to your institutional credentials. You will now be able to sign in with them.")
-            # rubocop:enable Metrics/LineLength
-          end
-        end
-        unless existing_user.get_locale.nil?
-          session[:locale] = existing_user.get_locale
-        end
-        # Method defined at controllers/application_controller.rb
-        set_gettext_locale
+      # Until ORCID login is supported
+      if !session["devise.shibboleth_data"].nil?
+        args = {
+          identifier_scheme: IdentifierScheme.find_by(name: "shibboleth"),
+          identifier: session["devise.shibboleth_data"]["uid"],
+          user: existing_user
+        }
+        @ui = UserIdentifier.new(args)
+      end
+      unless existing_user.get_locale.nil?
+        session[:locale] = existing_user.get_locale
+      end
+      # Method defined at controllers/application_controller.rb
+      set_gettext_locale
+    end
+
+    super do
+      if @ui.save
+        # rubocop:disable Metrics/LineLength
+        flash[:notice] = _("Your account has been successfully linked to your institutional credentials. You will now be able to sign in with them.")
+        # rubocop:enable Metrics/LineLength
       end
     end
   end
