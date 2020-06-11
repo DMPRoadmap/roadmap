84527eb3eb998ccf495f701151e0c12749bf6fcf
Sam Rust <xsrust@gmail.com>
2020-06-11 19:24:17 +0100

    fix a bug where UserIdentifiers were saved with nil user_id

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,31 +9,31 @@ class SessionsController < Devise::SessionsController
   # Capture the user's shibboleth id if they're coming in from an IDP
   # ---------------------------------------------------------------------
   def create
-    existing_user = User.find_by(email: params[:user][:email])
-    if !existing_user.nil?
-
-      # Until ORCID login is supported
-      if !session["devise.shibboleth_data"].nil?
-        args = {
-          identifier_scheme: IdentifierScheme.find_by(name: "shibboleth"),
-          identifier: session["devise.shibboleth_data"]["uid"],
-          user: existing_user
-        }
-        if UserIdentifier.create(args)
-          # rubocop:disable Metrics/LineLength
-          success = _("Your account has been successfully linked to your institutional credentials. You will now be able to sign in with them.")
-          # rubocop:enable Metrics/LineLength
-        end
-      end
-      unless existing_user.get_locale.nil?
-        session[:locale] = existing_user.get_locale
-      end
-      # Method defined at controllers/application_controller.rb
-      set_gettext_locale
-    end
     super
     if success
       flash[:notice] = success
+      existing_user = User.find_by(email: params[:user][:email])
+      if !existing_user.nil?
+
+        # Until ORCID login is supported
+        if !session["devise.shibboleth_data"].nil?
+          args = {
+            identifier_scheme: IdentifierScheme.find_by(name: "shibboleth"),
+            identifier: session["devise.shibboleth_data"]["uid"],
+            user: existing_user
+          }
+          if UserIdentifier.create(args)
+            # rubocop:disable Metrics/LineLength
+            success = _("Your account has been successfully linked to your institutional credentials. You will now be able to sign in with them.")
+            # rubocop:enable Metrics/LineLength
+          end
+        end
+        unless existing_user.get_locale.nil?
+          session[:locale] = existing_user.get_locale
+        end
+        # Method defined at controllers/application_controller.rb
+        set_gettext_locale
+      end
     end
   end
 
