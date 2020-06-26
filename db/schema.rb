95cbf4e103f59840c4deae9df9134152adbd911b
briri <brian.riley@ucop.edu>
2020-06-26 15:02:59 -0700

    fixed date in db.schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200601121822) do
+ActiveRecord::Schema.define(version: 20200625092600) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
