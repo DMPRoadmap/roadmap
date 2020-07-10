1630286370b5e98566c442869110c66ef66ec9d4
briri <brian.riley@ucop.edu>
2020-07-10 08:58:45 -0700

    ran rubocop auto-cleanup on validators

--- app/validators/answer_for_correct_template_validator.rb
+++ app/validators/answer_for_correct_template_validator.rb
@@ -1,10 +1,14 @@
+# frozen_string_literal: true
+
 class AnswerForCorrectTemplateValidator < ActiveModel::Validator
+
   def validate(record)
     # Make sure that the question and plan belong to the same template!
     unless record.plan.nil? || record.question.nil?
       unless record.plan.template == record.question.section.phase.template
-        record.errors[:question] << I18n.t('helpers.answer.question_must_belong_to_correct_template')
+        record.errors[:question] << I18n.t("helpers.answer.question_must_belong_to_correct_template")
       end
     end
   end
+
 end
