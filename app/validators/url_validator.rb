3a1d8c0833e03e41d45157f4b4ab537afb8f0e39
briri <brian.riley@ucop.edu>
2020-07-10 08:58:45 -0700

    ran rubocop auto-cleanup on validators

--- app/validators/url_validator.rb
+++ app/validators/url_validator.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class UrlValidator < ActiveModel::EachValidator
+
   def validate_each(record, attribute, value)
-    unless value =~ /https?:\/\/[-a-zA-Z0-9@:%_\+.~#?&\/=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&\/=]*)?/
+    unless value =~ %r{https?://[-a-zA-Z0-9@:%_\+.~#?&/=]{2,256}\.[a-z]{2,4}\b(/[-a-zA-Z0-9@:%_\+.~#?&/=]*)?}
       record.errors[attribute] << (options[:message] || "is not a valid URL")
     end
   end
+
 end
