afd7b817c8d3db971cae970f1d24afa98c8ea25b
briri <brian.riley@ucop.edu>
2020-07-10 08:57:45 -0700

    ran rubocop auto-cleanup on scrubbers

--- app/scrubbers/table_free_scrubber.rb
+++ app/scrubbers/table_free_scrubber.rb
@@ -1,6 +1,8 @@
+# frozen_string_literal: true
+
 class TableFreeScrubber < Rails::Html::PermitScrubber
 
-  TABLE_TAGS = %w[table thead tbody tr td th tfoot caption]
+  TABLE_TAGS = %w[table thead tbody tr td th tfoot caption].freeze
 
   ALLOWED_TAGS = Rails.application.config.action_view.sanitized_allowed_tags - TABLE_TAGS
 
