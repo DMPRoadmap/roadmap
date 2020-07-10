75c36049f4561c8e530aa3210b0abbaba77f29d7
briri <brian.riley@ucop.edu>
2020-07-10 08:56:55 -0700

    ran rubocop auto-cleanup on presenters

--- app/presenters/guidance_presenter.rb
+++ app/presenters/guidance_presenter.rb
@@ -52,12 +52,12 @@ class GuidancePresenter
       if annotations.present? || main_groups.present? # annotations and main group
         # Tab with org.abbreviation
         display_tabs << { name: org.abbreviation, groups: main_groups,
-                        annotations: annotations }
+                          annotations: annotations }
       end
-      if subsets.present?
-        subsets.each_pair do |group, theme|
-          display_tabs << { name: group.name.truncate(15), groups: { group => theme } }
-        end
+      next unless subsets.present?
+
+      subsets.each_pair do |group, theme|
+        display_tabs << { name: group.name.truncate(15), groups: { group => theme } }
       end
     end
     display_tabs
@@ -72,6 +72,7 @@ class GuidancePresenter
   # The orgs from every guidance group selected for this plan
   def orgs
     return @orgs if defined?(@orgs)
+
     @orgs = []
     orgs_from_annotations.each { |org| @orgs << org unless org_found(@orgs, org) }
     orgs_from_guidance_groups.each { |org| @orgs << org unless org_found(@orgs, org) }
@@ -87,11 +88,12 @@ class GuidancePresenter
   # Returns true if exists any guidance_group applicable to the org and question passed
   def guidance_groups_by_theme?(org: nil, question: nil)
     return false unless question.respond_to?(:themes)
-    return false unless hashified_guidance_groups.has_key?(org)
+    return false unless hashified_guidance_groups.key?(org)
+
     result = guidance_groups_by_theme(org: org, question: question)
-               .detect do |gg, theme_hash|
+             .detect do |_gg, theme_hash|
       if theme_hash.present?
-        theme_hash.detect { |theme, guidances| guidances.present? }
+        theme_hash.detect { |_theme, guidances| guidances.present? }
       else
         false
       end
@@ -102,7 +104,8 @@ class GuidancePresenter
   # Returns true if exists any annotation applicable to the org and question passed
   def guidance_annotations?(org: nil, question: nil)
     return false unless question.respond_to?(:id)
-    return false unless hashified_annotations.has_key?(org)
+    return false unless hashified_annotations.key?(org)
+
     hashified_annotations[org].find do |annotation|
       (annotation.question_id == question.id) && (annotation.type == "guidance")
     end.present?
@@ -113,24 +116,23 @@ class GuidancePresenter
   # { guidance_group: { theme: [guidance, ...], ... }, ... }
   def guidance_groups_by_theme(org: nil, question: nil)
     raise ArgumentError unless question.respond_to?(:themes)
+
     question = Question.includes(:themes).find(question.id)
-    return {} unless hashified_guidance_groups.has_key?(org)
-    hashified_guidance_groups[org].each_key.reduce({}) do |acc, gg|
-      filtered_gg = hashified_guidance_groups[org][gg].each_key.reduce({}) do |acc, theme|
-        if question.themes.include?(theme)
-          acc[theme] = hashified_guidance_groups[org][gg][theme]
-        end
-        acc
+    return {} unless hashified_guidance_groups.key?(org)
+
+    hashified_guidance_groups[org].each_key.each_with_object({}) do |gg, acc|
+      filtered_gg = hashified_guidance_groups[org][gg].each_key.each_with_object({}) do |theme, acc|
+        acc[theme] = hashified_guidance_groups[org][gg][theme] if question.themes.include?(theme)
       end
       acc[gg] = filtered_gg if filtered_gg.present?
-      acc
     end
   end
 
   # Returns a collection of annotations (type guidance) for an org and question passed
   def guidance_annotations(org: nil, question: nil)
     raise ArgumentError unless question.respond_to?(:id)
-    return [] unless hashified_annotations.has_key?(org)
+    return [] unless hashified_annotations.key?(org)
+
     hashified_annotations[org].select do |annotation|
       (annotation.question_id == question.id) && (annotation.type == "guidance")
     end
@@ -138,6 +140,7 @@ class GuidancePresenter
 
   def orgs_from_guidance_groups
     return @orgs_from_guidance_groups if defined?(@orgs_from_guidance_groups)
+
     @orgs_from_guidance_groups = Org.joins(:guidance_groups)
                                     .where(guidance_groups: { id: guidance_groups.ids })
                                     .distinct("orgs.id")
@@ -146,6 +149,7 @@ class GuidancePresenter
 
   def orgs_from_annotations
     return @orgs_from_annotations if defined?(@orgs_from_annotations)
+
     @orgs_from_annotations = []
     if plan.template.customization_of.present?
       family_id = plan.template.customization_of
@@ -160,11 +164,11 @@ class GuidancePresenter
   end
 
   def hashified_guidance_groups?
-    result = hashified_guidance_groups.detect do |org, gg_hash|
+    result = hashified_guidance_groups.detect do |_org, gg_hash|
       if gg_hash.present?
-        gg_hash.detect do |gg, theme_hash|
+        gg_hash.detect do |_gg, theme_hash|
           if theme_hash.present?
-            theme_hash.detect { |theme, guidances| guidances.present? }
+            theme_hash.detect { |_theme, guidances| guidances.present? }
           else
             false
           end
@@ -181,7 +185,7 @@ class GuidancePresenter
   end
 
   def hashified_annotations?
-    hashified_annotations.detect { |org, annotations| annotations.present? }.present?
+    hashified_annotations.detect { |_org, annotations| annotations.present? }.present?
   end
 
   # Hashifies guidance groups for a plan according to the distinct orgs into the
@@ -189,15 +193,13 @@ class GuidancePresenter
   # { org: { guidance_group: { theme: [guidance, ...], ... }, ... }, ... }
   def hashify_guidance_groups
     hashified_guidances = hashify_guidances
-    orgs_from_guidance_groups.reduce({}) do |acc, org|
+    orgs_from_guidance_groups.each_with_object({}) do |org, acc|
       org_guidance_groups = hashified_guidances.each_key.select do |gg|
         gg.org_id == org.id
       end
-      acc[org] = org_guidance_groups.reduce({}) do |acc, gg|
+      acc[org] = org_guidance_groups.each_with_object({}) do |gg, acc|
         acc[gg] = hashified_guidances[gg]
-        acc
       end
-      acc
     end
   end
 
@@ -205,24 +207,21 @@ class GuidancePresenter
   # structure:
   # { guidance_group: { theme: [guidance, ...], ... }, ... }
   def hashify_guidances
-    guidance_groups.reduce({}) do |acc, gg|
+    guidance_groups.each_with_object({}) do |gg, acc|
       themes = Theme.includes(:guidances)
                     .joins(:guidances)
                     .merge(Guidance.where(guidance_group_id: gg.id, published: true))
-      acc[gg] = themes.reduce({}) do |acc, theme|
+      acc[gg] = themes.each_with_object({}) do |theme, acc|
         acc[theme] = theme.guidances
-        acc
       end
-      acc
     end
   end
 
   def hashify_annotations
-    orgs_from_annotations.reduce({}) do |acc, org|
+    orgs_from_annotations.each_with_object({}) do |org, acc|
       annotations = Annotation.where(org_id: org.id,
                                      question_id: plan.template.question_ids)
       acc[org] = annotations.select { |annotation| annotation.org_id = org.id }
-      acc
     end
   end
 
