585fed25cdae8007a2552417570989f73342b42a
Sam Rust <xsrust@gmail.com>
2020-07-17 14:53:03 +0100

    fix failing tests on answer model that were causing failure on sections/phases

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -97,7 +97,7 @@ class Answer < ApplicationRecord
     # If the question is option based then see if any options were selected
     return question_options.any? if question.question_format.option_based?
     # Strip out any white space and see if the text is empty
-    return text.gsub(%r{</?p>}, "").gsub(%r{<br\s?/?>}, "").chomp.blank? if text.present?
+    return !text.gsub(%r{</?p>}, "").gsub(%r{<br\s?/?>}, "").chomp.blank? if text.present?
 
     false
   end
