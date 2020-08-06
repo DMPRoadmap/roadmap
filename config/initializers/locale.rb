07167dc353831707a599aa91d07eb09bc56f0253
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-08-06 14:14:02 -0600

    Add missing files

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,6 +2,8 @@
 
 # This initilializer should not be removed unless all internationalisation is handled by
 # gettext_rails
+require_relative('../application.rb')
+
 DMPRoadmap::Application.config.i18n.load_path += Dir[
   Rails.root.join('config', 'locales', '**', '*.yml').to_s
 ]
