4bbbe766dae91aba997e854e406a9ca8d93b781d
Marta Ribeiro <m.ribeiro@live.co.uk>
2020-08-13 14:47:07 +0100

    Rubocop fixes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,7 +41,6 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = { address: "mailcatcher", port: 1025 }
 
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
