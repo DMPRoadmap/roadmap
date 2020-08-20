aad03a2e216bd6dbb065d35c43dfda2568962c27
Ray Carrick <ray.carrick@ed.ac.uk>
2020-08-20 12:35:39 +0100

    fix template table AJAX html extraction for filters

--- app/javascript/src/orgAdmin/templates/index.js
+++ app/javascript/src/orgAdmin/templates/index.js
@@ -4,7 +4,7 @@ $(() => {
   // Update the contents of the table when user clicks on a scope link
   $('.template-scope').on('ajax:success', 'a[data-remote="true"]', (e) => {
     const data = e.detail[0];
-    $(e.target).closest('.template-scope').find('.paginable').html(data);
+    $(e.target).closest('.template-scope').find('.paginable').html(data.html);
   });
 
   if ($('#super-admin-switch-org').length > 0) {
