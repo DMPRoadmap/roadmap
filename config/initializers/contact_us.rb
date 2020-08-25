63fbef8db0ac808f0931ada102435e474efd1a04
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-08-25 11:49:37 -0600

    Add mailer information

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -6,10 +6,10 @@ ContactUs.setup do |config|
   # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
-  config.mailer_from = nil
+  config.mailer_from = Rails.application.secrets.mailer_from
 
   # Configure the e-mail address which should receive the contact form email notifications.
-  config.mailer_to = "roadmap-l@listserv.ucop.edu"
+  config.mailer_to = Rails.application.secrets.mailer_to
 
   # ==> Form Configuration
 
