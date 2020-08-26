1f8bdf685bb96e47837c71797ff48a5ada65621d
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-08-26 12:26:43 -0600

    Add configuration files

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,7 @@
 module DMPRoadmap
   class Application < Rails::Application
     WickedPdf.config = {
-      exe_path: Rails.application.secrets.wicked_pdf_path || '/usr/local/bin/wkhtmltopdf'
+      exe_path: Rails.application.secrets.wicked_pdf_path || '/usr/bin/wkhtmltopdf'
     }
   end
 end
