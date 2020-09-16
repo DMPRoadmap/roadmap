b5e192ecce4778e41faef0ba862256856fd141ec
Sam Rust <xsrust@gmail.com>
2020-09-16 12:43:15 +0100

    update spec localization to set/get from I18n

--- spec/support/locales.rb
+++ spec/support/locales.rb
@@ -9,15 +9,9 @@ RSpec.configure do |config|
   config.before(:suite) do
     # This is required for the Faker gem. See this issue here:
     # https://github.com/stympy/faker/issues/266
-    I18n.available_locales = AVAILABLE_TEST_LOCALES.map do |locale|
-      LocaleService.to_i18n(locale: locale)
-    end
-    I18n.default_available_locales = AVAILABLE_TEST_LOCALES.map do |locale|
-      LocaleService.to_gettext(locale: locale)
-    end
+    I18n.available_locales = AVAILABLE_TEST_LOCALES
     default_locale = AVAILABLE_TEST_LOCALES.first
-    I18n.default_locale = LocaleService.to_i18n(locale: default_locale)
-    I18n.default_locale = LocaleService.to_gettext(locale: default_locale)
+    I18n.default_locale = default_locale
 
     unless Language.default.present?
       Language.create(name: default_locale, abbreviation: default_locale,
@@ -28,8 +22,7 @@ RSpec.configure do |config|
   config.before(:each, type: :feature) do
     I18n.config.enforce_available_locales = true
     default_locale = AVAILABLE_TEST_LOCALES.first
-    I18n.config.locale = LocaleService.to_i18n(locale: default_locale)
-    I18n.locale = LocaleService.to_gettext(locale: default_locale)
+    I18n.config.locale = default_locale
   end
 
 end
