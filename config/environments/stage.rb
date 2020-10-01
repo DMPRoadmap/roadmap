1cf35831b9666e667b44abb1033da1c26a2d0d8a
briri <brian.riley@ucop.edu>
2020-10-01 08:25:42 -0700

    updated stage env config

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -25,11 +25,11 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  # config.assets.compile = false
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to
   # config/initializers/assets.rb
@@ -96,8 +96,8 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Assets pipeline
-  config.assets.enabled = false
-  config.assets.debug = false
+  config.assets.enabled = true
+  config.assets.debug = true
   config.assets.compile = false
   config.assets.quiet = true
 end
