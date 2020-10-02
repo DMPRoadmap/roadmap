658831ea6eb90fc69ed1502525508dace291f685
briri <brian.riley@ucop.edu>
2020-10-02 10:50:37 -0700

    minor bug fixes

--- app/javascript/src/shared/createAccountForm.js
+++ app/javascript/src/shared/createAccountForm.js
@@ -2,10 +2,9 @@ import { initAutocomplete, scrubOrgSelectionParamsOnSubmit } from '../utils/auto
 import { togglisePasswords } from '../utils/passwordHelper';
 
 $(() => {
-  const options = { selector: '#create-account-form' };
   initAutocomplete('#create-account-org-controls .autocomplete');
   // Scrub out the large arrays of data used for the Org Selector JS so that they
   // are not a part of the form submissiomn
   scrubOrgSelectionParamsOnSubmit('#create_account_form');
-  togglisePasswords(options);
+  togglisePasswords({ selector: '#create_account_form' });
 });
