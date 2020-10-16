76178a15c4618864737cc19268e5a212a93fb51a
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-10-16 11:03:30 -0600

    Fix support for nulldb adapter

--- config/application.rb
+++ config/application.rb
@@ -3,6 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'recaptcha/rails'
 require 'csv'
+require 'nulldb/rails'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
