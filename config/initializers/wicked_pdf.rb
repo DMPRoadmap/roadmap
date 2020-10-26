4becdf2eb2484ba4556ff6fff701e76f45e43d20
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-10-26 11:20:22 -0600

    Add configuration for wicked_pdf proxy

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,7 +1,8 @@
 module DMPRoadmap
   class Application < Rails::Application
     WickedPdf.config = {
-      exe_path: Rails.application.secrets.wicked_pdf_path || '/usr/bin/wkhtmltopdf'
+      exe_path: Rails.application.secrets.wicked_pdf_path || '/usr/bin/wkhtmltopdf',
+      proxy: Rails.application.secrets.wicked_pdf_proxy || ''
     }
   end
 end
