db4686ee679efb31c1ab7fbd5e26038f2b905bd7
Benjamin FAURE <benjamin.faure@inist.fr>
2020-10-27 10:20:06 +0100

    Solved issue with missing save event after dynamic form save

--- app/javascript/views/answers/edit.js
+++ app/javascript/views/answers/edit.js
@@ -55,8 +55,10 @@ $(() => {
             // Refresh optimistic locking view with the form that caused the locking
             $(`#answer-locking-${data.question.id}-research-output-${data.research_output.id}`).html(data.question.locking);
           } else { // When answer is NOT stale...
+            const newForm = $(`#answer-form-${data.question.id}-research-output-${data.research_output.id}`).find('form');
             $(`#answer-locking-${data.question.id}-research-output-${data.research_output.id}`).html('');
-            $(`#answer-form-${data.question.id}-research-output-${data.research_output.id}`).html(data.question.form);
+            newForm.html(data.question.form);
+            attachEventHandlers(newForm); // eslint-disable-line no-use-before-define
             if (isNumber(data.question.answer_lock_version)) {
               form.find('#answer_lock_version').val(data.question.answer_lock_version);
             }
