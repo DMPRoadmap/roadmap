b496ce1772264f13efc0b58de8c0bdd6224e9a06
Benjamin FAURE <benjamin.faure@inist.fr>
2020-11-10 10:33:04 +0100

    MadmpSchema are now searchable in superadmin

--- app/policies/madmp_schema_policy.rb
+++ app/policies/madmp_schema_policy.rb
@@ -1,24 +1,35 @@
+# frozen_string_literal: true
+
 class MadmpSchemaPolicy < ApplicationPolicy
+
   def initialize(user, *args)
     raise Pundit::NotAuthorizedError, _("must be logged in") unless user
+
     @user = user
   end
+
   def index?
     @user.can_super_admin?
   end
+
   def new?
     @user.can_super_admin?
   end
+
   def create?
     @user.can_super_admin?
   end
+
   def edit?
     @user.can_super_admin?
   end
+
   def update?
     @user.can_super_admin?
   end
+
   def destroy?
     @user.can_super_admin?
   end
+
 end
