b77bf89fcd347ccb7ee2d7bb971e0515cb870a1d
briri <brian.riley@ucop.edu>
2020-11-17 09:44:41 -0800

    added configs and update gitignore

--- config/initializers/contact_us.rb
+++ config/initializers/contact_us.rb
@@ -2,10 +2,10 @@
 
 # Use this hook to configure contact mailer.
 ContactUs.setup do |config|
-
   # ==> Mailer Configuration
 
-  # Configure the e-mail address which email notifications should be sent from.  If emails must be sent from a verified email address you may set it here.
+  # Configure the e-mail address which email notifications should be sent from.
+  # If emails must be sent from a verified email address you may set it here.
   # Example:
   # config.mailer_from = "contact@please-change-me.com"
   config.mailer_from = Rails.configuration.x.organisation.do_not_reply_email
@@ -29,5 +29,4 @@ ContactUs.setup do |config|
   # Set the following variable to true if you are using localized paths
   # e.g. /en/contact-us OR /fr/contact-us
   config.localize_routes = true
-
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,1 +1,58 @@
-/Users/briley/Documents/workspace/dmptool_config/config/initializers/devise.rb
+# frozen_string_literal: true
+
+require "custom_failure"
+
+# rubocop:disable Metrics/BlockLength
+Devise.setup do |config|
+  require "devise/orm/active_record"
+
+  config.mailer_sender = Rails.configuration.x.organisation.do_not_reply_email
+
+  config.case_insensitive_keys = [:email]
+  config.strip_whitespace_keys = [:email]
+
+  config.skip_session_storage = [:http_auth]
+
+  config.stretches = Rails.env.test? ? 1 : 10
+
+  config.secret_key = Rails.application.credentials.devise_secret
+  config.pepper = Rails.application.credentials.devise_key
+
+  config.reconfirmable = false
+
+  config.password_length = 8..72
+
+  config.timeout_in = 3.hours
+
+  config.reset_password_within = 6.hours
+
+  config.sign_out_via = :delete
+
+  # Omniauth Providers
+  config.omniauth :orcid,
+                  Rails.application.credentials.orcid[:client_id],
+                  Rails.application.credentials.orcid[:client_secret],
+                  member: true,
+                  sandbox: Rails.application.credentials.orcid[:sandbox]
+
+  config.omniauth :shibboleth, {
+    request_type: :header,
+    uid_field: "eppn",
+    info_fields: {
+      email: "mail",
+      name: "displayName",
+      last_name: "sn",
+      first_name: "givenName",
+      identity_provider: "shib_identity_provider"
+    },
+    extra_fields: [:schacHomeOrganization],
+    debug: false
+  }
+
+  config.omniauth_path_prefix = "/users/auth"
+
+  config.warden do |manager|
+    manager.failure_app = CustomFailure
+  end
+end
+# rubocop:enable Metrics/BlockLength
