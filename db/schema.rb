3f4502f0cd9e3eafe32c1bf940d43d13e3dab9e4
briri <brian.riley@ucop.edu>
2020-11-23 13:51:48 -0800

    updated schema date in schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_174910) do
+ActiveRecord::Schema.define(version: 2020_11_19_210343) do
 
   create_table "annotations", id: :integer, force: :cascade do |t|
     t.integer "question_id"
