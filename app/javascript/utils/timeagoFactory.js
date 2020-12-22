77b059734bd85a0b900787f361d075d5a4d7bf2c
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2020-12-22 12:29:08 -0700

    Add timeago localization

--- app/javascript/utils/timeagoFactory.js
+++ app/javascript/utils/timeagoFactory.js
@@ -1,15 +1,26 @@
-import enGB from '../locale/en_GB/timeago';
+import fr_CA from '../locale/fr_CA/timeago';
+import en_CA from '../locale/en_CA/timeago';
+import en_GB from '../locale/en_GB/timeago';
 
 /* global timeago */
+
 const TimeagoFactory = (() => {
-  timeago.register('en_GB', enGB);
+
+  timeago.register('fr_CA', fr_CA);
+  timeago.register('en_CA', en_CA);
+  timeago.register('en_GB', en_GB);
   /*
     @param el - DOM element
     @returns
   */
+
   return {
     render: (el) => {
-      timeago().render(el, 'en_GB');
+      // The global variable i18nLocale is being used to fetch rails locale.
+      // This variable is defined on application.html.erb
+      // We are using a global variable since fetching from $('body').dataset
+      // was not working.
+      timeago().render(el, i18nLocale);
     },
   };
 })();
