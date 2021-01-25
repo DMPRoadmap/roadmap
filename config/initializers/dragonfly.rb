5bcd331f3a253a1fac8870b9dc595cf7edbb0cd1
briri <brian.riley@ucop.edu>
2021-01-25 08:12:34 -0800

    added S3 to dragonfly config

--- config/initializers/dragonfly.rb
+++ config/initializers/dragonfly.rb
@@ -1,24 +1,28 @@
-require 'dragonfly'
-# require 'dragonfly/s3_data_store'
+# frozen_string_literal: true
+
+require "dragonfly"
+require "dragonfly/s3_data_store"
 
 # Configure
 Dragonfly.app.configure do
-
   plugin :imagemagick
 
   secret Rails.application.credentials.dragonfly_secret
 
   url_format "/media/:job/:name"
 
-  datastore :file,
-    root_path: Rails.root.join('public/system/dragonfly', Rails.env),
-    server_root: Rails.root.join('public')
-
-  #datastore :s3,
-  #  url_scheme: 's3',
-  #  url_host: 'uc3-s3dmp-stg',
-  #  bucket_name: 'uc3-s3dmp-stg',
-  #  use_iam_profile: true
+  if Rails.env.development?
+    datastore :file,
+              root_path: Rails.root.join("public/system/dragonfly", Rails.env),
+              server_root: Rails.root.join("public")
+  else
+    datastore :s3,
+              url_scheme: "s3",
+              # url_host: 'uc3-s3dmp-stg',
+              root_path: "logos",
+              bucket_name: "uc3-s3dmp-stg",
+              use_iam_profile: true
+  end
 end
 
 # Logger
