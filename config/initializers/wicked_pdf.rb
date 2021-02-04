14aa4160f6a4f8c66f6132bb8f1690626dd1cd49
briri <brian.riley@ucop.edu>
2021-02-04 15:17:32 -0800

    reverted change to wkhtmltopdf executable

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -4,16 +4,16 @@ module DMPRoadmap
 
   class Application < Rails::Application
 
-    WickedPdf.config = {
-      exe_path: "./bin/wkhtmltopdf"
-    }
-    # if Rails.env.development?
-    #   WickedPdf.config = { exe_path: "/Users/briley/.rbenv/shims/wkhtmltopdf" }
-    # else
-    #   WickedPdf.config = {
-    #     exe_path: "/dmp/local/bin/wkhtmltopdf"
-    #   }
-    # end
+    # WickedPdf.config = {
+    #   exe_path: Rails.root.join("bin", "wkhtmltopdf")
+    # }
+    if Rails.env.development?
+      WickedPdf.config = { exe_path: "/Users/briley/.rbenv/shims/wkhtmltopdf" }
+    else
+      WickedPdf.config = {
+        exe_path: "/dmp/local/bin/wkhtmltopdf"
+      }
+    end
   end
 
 end
