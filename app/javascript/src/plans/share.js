406188a8a6efcbb82ea4b1a0421a4eb7eb282997
briri <brian.riley@ucop.edu>
2021-02-10 08:25:37 -0800

    fixed issue with setting plan visibility

--- app/javascript/src/plans/share.js
+++ app/javascript/src/plans/share.js
@@ -1,22 +1,7 @@
 import * as notifier from '../utils/notificationHelper';
-import { isObject, isString } from '../utils/isType';
+import { isObject } from '../utils/isType';
 
 $(() => {
-  $('#set_visibility').on('ajax:success', (e) => {
-    const data = e.detail[0];
-    if (isObject(data) && isString(data.msg)) {
-      notifier.renderNotice(data.msg);
-    }
-  });
-  $('#set_visibility').on('ajax:error', (e) => {
-    const xhr = e.detail[2];
-    if (isObject(xhr.responseJSON)) {
-      notifier.renderAlert(xhr.responseJSON.msg);
-    } else {
-      notifier.renderAlert(`${xhr.statusCode} - ${xhr.statusText}`);
-    }
-  });
-
   $('.toggle-existing-user-access')
     .on('ajax:success', (e) => {
       const data = e.detail[0];
