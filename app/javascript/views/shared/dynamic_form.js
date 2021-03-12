ee28014d63ede08278358c72c1d19090b4fa67e3
Benjamin FAURE <benjamin.faure@inist.fr>
2021-03-12 16:53:16 +0100

    Custom value is not available for multiple select

--- app/javascript/views/shared/dynamic_form.js
+++ app/javascript/views/shared/dynamic_form.js
@@ -1,90 +1,93 @@
-$(document).on('click', '.madmp-fragment .actions .add-record', (e) => {
-  const currentField = $(e.target.closest('.dynamic-field'));
-  const clonedField = currentField.clone(true, true);
 
-  clonedField.find('input').val(null);
-  clonedField.find('.remove-record').show();
+$(() => {
+  $(document).on('click', '.madmp-fragment .actions .add-record', (e) => {
+    const currentField = $(e.target.closest('.dynamic-field'));
+    const clonedField = currentField.clone(true, true);
 
-  currentField.after(clonedField);
-});
+    clonedField.find('input').val(null);
+    clonedField.find('.remove-record').show();
 
-$(document).on('click', '.madmp-fragment .actions .remove-record', (e) => {
-  const currentField = $(e.target.closest('.dynamic-field'));
-  currentField.remove();
-});
-
-// When the sub fragment modal opens
-$('#sub-fragment-modal').on('show.bs.modal', (e) => {
-  // Set the modal content (loads the form)
-  const link = $(e.relatedTarget).data('open');
-  const parent = $(e.relatedTarget).parent();
-  $.ajax({
-    url: link,
-    method: 'get',
-    success: (data) => {
-      $('#sub-fragment-modal-body').html(data);
-      $('#sub-fragment-modal-body').find('#parent_form_id').val(parent.attr('for'));
-      $('#sub-fragment-modal-body').find('#parent_form_index').val(parent.attr('index'));
-    },
-    error: (err) => {
-      console.log(err);
-    },
+    currentField.after(clonedField);
   });
-});
 
-$(document).on('click', '.linked-fragments-list .actions .delete', (e) => {
-  const target = $(e.target);
-  // TODO : replace confirm()
-  // eslint-disable-next-line
-  const confirmed = confirm(target.data('confirm-message'));
-  if (confirmed) {
+  $(document).on('click', '.madmp-fragment .actions .remove-record', (e) => {
+    const currentField = $(e.target.closest('.dynamic-field'));
+    currentField.remove();
+  });
+
+  // When the sub fragment modal opens
+  $('#sub-fragment-modal').on('show.bs.modal', (e) => {
+    // Set the modal content (loads the form)
+    const link = $(e.relatedTarget).data('open');
+    const parent = $(e.relatedTarget).parent();
     $.ajax({
-      url: target.data('url'),
-      method: 'delete',
-    }).done((data) => {
-      $(`table.list-${data.query_id} tbody`).html(data.html);
+      url: link,
+      method: 'get',
+      success: (data) => {
+        $('#sub-fragment-modal-body').html(data);
+        $('#sub-fragment-modal-body').find('#parent_form_id').val(parent.attr('for'));
+        $('#sub-fragment-modal-body').find('#parent_form_index').val(parent.attr('index'));
+      },
+      error: (err) => {
+        console.log(err);
+      },
     });
-  }
-});
+  });
 
-/*
- * Changes the url of the "View" link according to the selected value in the fragment select
-*/
-$(document).on('change', '.linked-fragments-select', (e) => {
-  const value = e.target.value;
-  const viewLink = $(e.target).parent().find('a');
-  viewLink.attr('href', viewLink.attr('href').replace(/fragment_id=([^&]+)/, `fragment_id=${value}`));
-});
+  $(document).on('click', '.linked-fragments-list .actions .delete', (e) => {
+    const target = $(e.target);
+    // TODO : replace confirm()
+    // eslint-disable-next-line
+    const confirmed = confirm(target.data('confirm-message'));
+    if (confirmed) {
+      $.ajax({
+        url: target.data('url'),
+        method: 'delete',
+      }).done((data) => {
+        $(`table.list-${data.query_id} tbody`).html(data.html);
+      });
+    }
+  });
 
-$(document).on('change', '.schema_picker', (e) => {
-  const target = $(e.target);
-  const form = target.parents('.question').find('.form-answer');
-  form.find('.schema_id').val(target.val());
-  form.trigger('submit');
-});
+  /*
+   * Changes the url of the "View" link according to the selected value in the fragment select
+  */
+  $(document).on('change', '.linked-fragments-select', (e) => {
+    const value = e.target.value;
+    const viewLink = $(e.target).parent().find('a');
+    viewLink.attr('href', viewLink.attr('href').replace(/fragment_id=([^&]+)/, `fragment_id=${value}`));
+  });
 
-$(document).on('click', '.toggle-guidance-section', (e) => {
-  const target = $(e.currentTarget);
-  target.parents('.question-body').find('.guidance-section').toggle();
-  target.find('span.fa-chevron-right, span.fa-chevron-left')
-    .toggleClass('fa-chevron-right')
-    .toggleClass('fa-chevron-left');
-});
+  $(document).on('change', '.schema_picker', (e) => {
+    const target = $(e.target);
+    const form = target.parents('.question').find('.form-answer');
+    form.find('.schema_id').val(target.val());
+    form.trigger('submit');
+  });
 
-$(document).on('click', '.select-field .overridable-link', (e) => {
-  e.preventDefault();
-  const target = $(e.target);
-  const selectField = target.parent();
+  $(document).on('click', '.toggle-guidance-section', (e) => {
+    const target = $(e.currentTarget);
+    target.parents('.question-body').find('.guidance-section').toggle();
+    target.find('span.fa-chevron-right, span.fa-chevron-left')
+      .toggleClass('fa-chevron-right')
+      .toggleClass('fa-chevron-left');
+  });
 
-  selectField.find('.custom-value').show();
-  selectField.find('.custom-value input').val('');
-  selectField.find('select').val('');
-});
+  $(document).on('click', '.select-field .overridable-link', (e) => {
+    e.preventDefault();
+    const target = $(e.target);
+    const selectField = target.parent();
 
-$(document).on('change', '.select-field select', (e) => {
-  const target = $(e.target);
-  const selectField = target.parent();
+    selectField.find('.custom-value').show();
+    selectField.find('.custom-value input').val('');
+    selectField.find('select').val('');
+  });
 
-  selectField.find('.custom-value').hide();
-  selectField.find('.custom-value input').val('');
+  $(document).on('change', '.select-field select', (e) => {
+    const target = $(e.target);
+    const selectField = target.parent();
+
+    selectField.find('.custom-value').hide();
+    selectField.find('.custom-value input').val('');
+  });
 });
