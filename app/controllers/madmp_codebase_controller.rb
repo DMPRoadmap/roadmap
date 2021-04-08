2d49912b4fd47dbcd3d79553bbbb352ddc49a5ed
Benjamin FAURE <benjamin.faure@inist.fr>
2021-04-08 16:32:33 +0200

    Some refactoring in MadmpCodebase controller

--- app/controllers/madmp_codebase_controller.rb
+++ app/controllers/madmp_codebase_controller.rb
@@ -5,24 +5,31 @@ class MadmpCodebaseController < ApplicationController
   after_action :verify_authorized
 
   def run
-    @fragment = MadmpFragment.find(params[:fragment_id])
+    fragment = MadmpFragment.find(params[:fragment_id])
     script_id = params[:script_id]
-
-    response = ExternalApis::MadmpCodebaseService.run(script_id, body:
-      {
-        "data": @fragment.data,
-        "schema": {},
-        "dmp_id": @fragment.dmp_id
-      })
-    @fragment.save_codebase_fragment(response.data, @fragment.madmp_schema)
+    response = fetch_run_data(fragment, script_id)
+    # fragment.save_codebase_fragment(response.data, fragment.madmp_schema)
 
     # EXAMPLE DATA : CODEBASE NEEDS FIXES
     # file_path = Rails.root.join("config/schemas/codebase_example_data.json")
     # response = JSON.load(File.open(file_path))
     # @fragment.save_codebase_fragment(response, @fragment.madmp_schema)
 
-    authorize @fragment
+    authorize fragment
     render json: response.to_json
+  end
+
+  private
+
+  def fetch_run_data(fragment, script_id)
+    return {} unless fragment.present? && script_id.present?
+
+    ExternalApis::MadmpCodebaseService.run(script_id, body:
+      {
+        "data": fragment.data,
+        "schema": {},
+        "dmp_id": fragment.dmp_id
+      })
   end
 
 end
