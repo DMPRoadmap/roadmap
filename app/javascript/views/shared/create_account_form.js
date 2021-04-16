ee2d287440f51e2889c03e7a10643271dd55aced
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2021-04-16 12:41:06 -0600

    Change url redirection after failed sing up

--- app/javascript/views/shared/create_account_form.js
+++ app/javascript/views/shared/create_account_form.js
@@ -5,11 +5,4 @@ $(() => {
   const options = { selector: '#create-account-form' };
   togglisePasswords(options);
   initOrgSelection(options);
-
-  $('#create_account_form').on('submit', (e) => {
-    // Additional validation to force the user to choose an org or type something for other
-    if (!validateOrgSelection(options)) {
-      e.preventDefault();
-    }
-  });
 });
