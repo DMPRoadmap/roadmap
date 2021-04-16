ff12cc0018c0831f2ce541088362bb103d658d39
Omar Rodriguez Arenas <orodrigu@ualberta.ca>
2021-04-16 12:41:06 -0600

    Change url redirection after failed sing up

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -90,7 +90,7 @@ class RegistrationsController < Devise::RegistrationsController
       end
 
       build_resource(sign_up_params)
-      
+
       if verify_recaptcha(model: resource) && resource.save
         if resource.active_for_authentication?
           set_flash_message :notice, :signed_up if is_navigational_format?
