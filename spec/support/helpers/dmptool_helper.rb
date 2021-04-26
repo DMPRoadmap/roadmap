9dce0f5d2c79abf2da27ac291db3c3de1c833e43
briri <brian.riley@ucop.edu>
2021-04-26 15:13:41 -0700

    added blog to the example _dmproadmap initializer

--- spec/support/helpers/dmptool_helper.rb
+++ spec/support/helpers/dmptool_helper.rb
@@ -74,6 +74,9 @@ module DmptoolHelper
     stub_request(:get, "https://blog.dmptool.org/feed").to_return(
       status: 200, body: xml.to_s, headers: {}
     )
+    stub_request(:get, "https://blog.example.org/feed").to_return(
+      status: 200, body: xml.to_s, headers: {}
+    )
   end
 
 end
