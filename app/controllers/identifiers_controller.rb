9644341426aa3f162160beb857b74e57657689a2
briri <brian.riley@ucop.edu>
2021-05-04 12:49:04 -0700

    fixed typo in code triggered by disconnecting orcid or shib

--- app/controllers/identifiers_controller.rb
+++ app/controllers/identifiers_controller.rb
@@ -27,7 +27,7 @@ class IdentifiersController < ApplicationController
     # TODO: While this works for ORCID it might not for future integrations. We should consider
     #       moving it to a different place on the Edit Profile page
     # Revoke any OAuth access tokens for the identifier
-    tokens = user.external_api_tokens.select do |token|
+    tokens = user.external_api_access_tokens.select do |token|
       token.external_service_name == identifier.identifier_scheme.downcase
     end
     tokens.each(&:revoke!)
