bb03fabc91aefc63bce56661375ff54362e13ac9
briri <brian.riley@ucop.edu>
2021-05-17 10:31:48 -0700

    updated doorkeeper config to allow localhost redirect_uris from stage

--- config/initializers/doorkeeper.rb
+++ config/initializers/doorkeeper.rb
@@ -316,6 +316,11 @@ Doorkeeper.configure do
   # force_ssl_in_redirect_uri !Rails.env.development?
   #
   # force_ssl_in_redirect_uri { |uri| uri.host != 'localhost' }
+  #
+  # DMPROADMAP NOTES:
+  # -----------------------------------------------------------------------
+  # We use our stage env for testing intgerations, so allow localhost and http
+  #
   force_ssl_in_redirect_uri Rails.env.production?
 
   # Specify what redirect URI's you want to block during Application creation.
