36fc67990c651d778cecabb529e8bc247d29a799
briri <brian.riley@ucop.edu>
2021-06-01 09:53:41 -0700

    fix for db migration and update so that it properly saves metadata standard selections

--- db/migrate/20210317202247_create_metadata_standard_categories.rb
+++ db/migrate/20210317202247_create_metadata_standard_categories.rb
@@ -22,8 +22,7 @@ class CreateMetadataStandardCategories < ActiveRecord::Migration[5.2]
 
     create_table :metadata_standards_research_outputs do |t|
       t.references :metadata_standard, null: true,  index: { name: "metadata_research_outputs_on_metadata" }
-      t.references :repository, null: true,  index: { name: "metadata_research_outputs_on_repo" }
-      t.timestamps
+      t.references :research_output, null: true,  index: { name: "metadata_research_outputs_on_ro" }
     end
 
   end
