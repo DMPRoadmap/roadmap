3923bffff8898781cddbfa68a41d684bbb797f90
briri <brian.riley@ucop.edu>
2021-06-07 10:20:31 -0700

    fixed up issues with db schema

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -315,7 +315,7 @@ class RegistrationsController < Devise::RegistrationsController
 
   # Finds or creates the selected org and then returns it's id
   def handle_org(attrs:)
-    return [] unless attrs.present?
+    return nil unless attrs.present?
 
     # DMPTool hack to deal with Org via IdP
     if attrs[:default_org_id].present?
