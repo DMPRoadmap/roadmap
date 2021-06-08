9fd6a80e7d2ebd32b7b3152f3c9e7eee8971023a
briri <brian.riley@ucop.edu>
2021-06-08 15:35:02 -0700

    added callback_method and callback?_uri to oauth_applications

--- db/migrate/20210521183235_add_logo_to_oauth_applications.rb
+++ db/migrate/20210521183235_add_logo_to_oauth_applications.rb
@@ -2,5 +2,7 @@ class AddLogoToOauthApplications < ActiveRecord::Migration[5.2]
   def change
     add_column :oauth_applications, :logo_uid, :string
     add_column :oauth_applications, :logo_name, :string
+    add_column :oauth_applications, :callback_uri, :string
+    add_column :oauth_applications, :callback_method, :integer
   end
 end
