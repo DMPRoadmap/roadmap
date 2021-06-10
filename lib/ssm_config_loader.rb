5d01bf85738a7ef3d2a62b67d3307aec87b55330
briri <brian.riley@ucop.edu>
2021-06-10 11:05:27 -0700

    removed debug from ssm config loader

--- lib/ssm_config_loader.rb
+++ lib/ssm_config_loader.rb
@@ -13,32 +13,17 @@ class SsmConfigLoader < Anyway::Loaders::Base
   def call(name:, **_opts)
     logger = Logger.new($stdout)
     ssm = Uc3Ssm::ConfigResolver.new
-
-logger.warn "SSM Loader for #{name}"
-logger.warn "  FOR ORCID_API_BASE_URL: #{ssm.parameter_for_key('dmproadmap/orcid_api_base_url')}"
-
     parameters = ssm.parameters_for_path(path: name, recursive: true)
-
-logger.warn "  PARAMS FROM SSM:"
-logger.warn parameters.inspect
-
-logger.warn "  PROCESSING PARAMS:"
-
     config = {}
     # reverse processing order to ensure correct precidence based on ssm_root_path
     parameters.reverse_each do |param|
       # strip off ssm_root_path
       sub_path = name + param[:name].partition(name)[-1]
-
-logger.warn "    #{sub_path}"
-
       new_hash = hashify_param_path({}, sub_path, param[:value])
       Anyway::Utils.deep_merge!(config, new_hash)
     end
     # require "pp"
     # pp config
-
-logger.warn config.inspect
 
     trace!(:ssm_parameter_store, ssm_root_path: ENV["SSM_ROOT_PATH"].to_s) do
       config[name].to_h || {}
