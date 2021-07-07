d5a059742b276490f1ab37af594de052c67db7c3
Benjamin FAURE <benjamin.faure@inist.fr>
2021-07-07 10:29:02 +0200

    User should be able to search for registry value using up or lowercase

--- app/controllers/registries_controller.rb
+++ app/controllers/registries_controller.rb
@@ -11,7 +11,7 @@ class RegistriesController < ApplicationController
     locale = plan.template.locale
     search_term = params[:term] || ""
     values_list = registry.registry_values
-    formatted_list = values_list.select { |v| v.to_s(locale).downcase.include?(search_term) }
+    formatted_list = values_list.select { |v| v.to_s(locale).downcase.include?(search_term.downcase) }
                                 .map    { |v| { "id" => select_value(v, locale), "text" => v.to_s(locale) } }
     authorize plan
     render json: {
