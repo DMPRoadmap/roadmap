47beaa92cbcb0a6521c889186488bc2f156b2d59
briri <brian.riley@ucop.edu>
2021-07-27 14:49:52 -0700

    added pally

--- app/javascript/src/dmptool/breakpoints.js
+++
@@ -1,9 +0,0 @@
-// SCSS Breakpoints to JS //
-
-// Get list of breakpoints from --breakpoints variable defined in _breakpoints-js.scss, make into array, destructure into "minwidth#" variables, then export them for js module use:
-
-const breakpoints = window.getComputedStyle(document.documentElement).getPropertyValue('--breakpoints');
-
-const breakpointsArr = breakpoints.split(',');
-
-export const [minwidth1, minwidth2] = breakpointsArr;
--- app/javascript/src/dmptool/tests.js
+++
@@ -1,7 +0,0 @@
-// Initial Tests //
-
-if (document.querySelector('#js-tests2')) {
-  const tests2 = document.querySelector('#js-tests2 output');
-
-  tests2.innerText = 'Yes';
-}
