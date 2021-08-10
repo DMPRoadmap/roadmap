915fe40db4817ecc8e9b8b4ea8e5a3c1262bb5c8
briri <brian.riley@ucop.edu>
2021-08-10 10:10:16 -0700

    fixing issue with create for api v2

--- app/services/api/v2/json_validation_service.rb
+++ app/services/api/v2/json_validation_service.rb
@@ -14,6 +14,7 @@ module Api
       BAD_CONTRIB_MSG = _(":role and either the :name or :email are required for each :contributor").freeze
       BAD_FUNDING_MSG = _(":name, :funder_id or :grant_id are required for each funding").freeze
       BAD_DATASET_MSSG = _(":title is required for each :dataset").freeze
+      BAD_HOST_MSG = _(":host must include either a :url or :dmproadmap_host_id").freeze
       # rubocop:enable Layout/LineLength
 
       class << self
