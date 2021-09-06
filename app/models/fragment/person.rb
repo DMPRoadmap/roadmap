d97e5aac6c70ad20671fe594be667ebdea0e01e4
Benjamin FAURE <benjamin.faure@inist.fr>
2021-09-06 15:05:57 +0200

    Data contact role is now displayed once with the associated research outputs in parenthesis

--- app/models/fragment/person.rb
+++ app/models/fragment/person.rb
@@ -20,22 +20,32 @@
 class Fragment::Person < MadmpFragment
 
   def roles(selected_research_outputs = nil)
-    if selected_research_outputs.nil?
-      contributors_list = Fragment::Contributor.where("(data->'person'->>'dbid')::int = ?", id)
-    else
-      contributors_list = Fragment::Contributor.where("(data->'person'->>'dbid')::int = ?", id)
-                                               .select do |c|
-                                                 c.research_output_id.nil? || selected_research_outputs.include?(c.research_output_id)
-                                               end
+    contributors_list = self.contributors
+    roles_list = []
+    ro_contact_role = nil
+    if selected_research_outputs.present?
+      contributors_list = contributors_list
+                          .select do |c|
+                            c.research_output_id.nil? || selected_research_outputs.include?(c.research_output_id)
+                          end
     end
-    contributors_list.map do |c|
+    # This part of the code whecks if the contributor is a data contact for a research output
+    # if so, the role will be displayed once as a concatenation of the research output abbreviation
+    # Ex: Data contact (RO1, RO2)
+    contributors_list.each_with_index do |c, index|
       if c.parent&.classname.eql?("research_output_description")
         ro = ::ResearchOutput.find(c.research_output_id)
-        "#{c.data['role']} (#{ro.abbreviation})"
+        ro_contact_role = "#{c.data['role']} (" if ro_contact_role.nil?
+        ro_contact_role += "#{ro.abbreviation}"
+        unless index.eql?(contributors_list.size - 1)
+          ro_contact_role += ", "
+        end
       else
-        c.data["role"]
+        roles_list.push(c.data["role"])
       end
-    end.compact
+    end
+    roles_list.push(ro_contact_role + ")")
+    return roles_list.compact.sort
   end
 
   def contributors
