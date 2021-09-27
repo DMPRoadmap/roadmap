97e0e49c57eab2889b22accc60eb87be30db54c6
briri <brian.riley@ucop.edu>
2021-09-27 08:26:29 -0700

    added handler for Pundit errors on ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,6 +24,8 @@ class ApplicationController < ActionController::Base
 
   rescue_from StandardError, with: :handle_server_error
 
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
   private
 
   def current_org
