494931c51754fc6673a29fd3562f98366a54daa6
briri <brian.riley@ucop.edu>
2021-10-07 15:20:12 -0700

    updated PDF coversheet to always show the creator of the DMP

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -33,12 +33,6 @@ module ExportsHelper
     get_margin_value_for_side(:right)
   end
 
-  def plan_attribution(attribution)
-    attribution = Array(attribution)
-    prefix = attribution.many? ? _("Creators:") : _("Creator:")
-    "<strong>#{prefix}</strong> #{attribution.join(', ')}"
-  end
-
   private
 
   def get_margin_value_for_side(side)
