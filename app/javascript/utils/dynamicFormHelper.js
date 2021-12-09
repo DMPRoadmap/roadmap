f23eca7ba6a8984c8678471cc5eedb8a2f6095d2
Benjamin FAURE <benjamin.faure@inist.fr>
2021-12-09 13:46:50 +0100

    Added Helper for autoNumeric config

--- app/javascript/utils/dynamicFormHelper.js
+++ app/javascript/utils/dynamicFormHelper.js
@@ -1,7 +1,7 @@
 
-import AutoNumeric from 'autonumeric';
 import { Tinymce } from './tinymce.js.erb';
 import { Select2 } from './select2';
+import { AutoNumericHelper } from './autoNumericHelper';
 
 const toolbar = 'bold italic underline | fontsizeselect forecolor | bullist numlist | link | table';
 
@@ -11,21 +11,13 @@ export const formLoadingCallback = (data, target, source) => {
       toolbar,
     });
     Select2.init('.plan-details');
-    // eslint-disable-next-line no-new
-    new AutoNumeric(
-      '.plan-details .number-field',
-      { digitGroupSeparator: ' ', decimalPlaces: '0' },
-    );
+    AutoNumericHelper.init('.plan-details .number-field');
   } else if (source === 'modal_form') {
     Tinymce.init({
       toolbar,
     });
     Select2.init('#modal-window');
-    // eslint-disable-next-line no-new
-    new AutoNumeric(
-      '#modal-window .number-field',
-      { digitGroupSeparator: ' ', decimalPlaces: '0' },
-    );
+    AutoNumericHelper.init('#modal-window .number-field');
   } else {
     Tinymce.init({
       selector: `#research_output_${data.research_output.id}_section_${data.section.id} .note`,
@@ -33,11 +25,7 @@ export const formLoadingCallback = (data, target, source) => {
     });
     Select2.init(`#answer-form-${data.question.id}-research-output-${data.research_output.id}`);
     target.find('.toggle-guidance-section').removeClass('disabled');
-    // eslint-disable-next-line no-new
-    new AutoNumeric(
-      `#answer-form-${data.question.id}-research-output-${data.research_output.id} .number-field`,
-      { digitGroupSeparator: ' ', decimalPlaces: '0' },
-    );
+    AutoNumericHelper.init(`#answer-form-${data.question.id}-research-output-${data.research_output.id} .number-field`);
   }
   $('[data-toggle="tooltip"]:not([data-placement])').tooltip({
     animated: 'fade',
