06835ddbde5dbbda530f7fb4c96aaafff8e503e1
briri <brian.riley@ucop.edu>
2022-01-07 15:09:24 -0800

    added tests

--- spec/features/authentication/initial_email_entry_spec.rb
+++ spec/features/authentication/initial_email_entry_spec.rb
@@ -8,44 +8,52 @@ RSpec.describe 'Sign in/up via email entry', type: :feature do
 
   before(:each) do
     mock_blog
-    @org = create(:org)
+    @email_domain = "@#{Faker::Lorem.unique.word.downcase}.edu"
+    @org = create(:org, contact_email: "#{Faker::Lorem.unique.word}#{@email_domain}")
     visit root_path
   end
 
-  it 'Displays an error if no email is provided' do
+  it 'Displays an error if no email is provided', js: true do
     within("form[action=\"#{user_session_path}\"]") do
-      expect { find('.is-invalid[id="sign-up-email"]') }.to raise_error(Capybara::ElementNotFound)
+      expect { find('.is-invalid[id="sign-in-sign-up-email"]') }.to raise_error(Capybara::ElementNotFound)
       click_button 'Continue'
-      expect(find('.is-invalid[id="sign-up-email"]').present?).to eql(true)
+      expect(find('.is-invalid[id="sign-in-sign-up-email"]').present?).to eql(true)
     end
   end
 
-  it 'sends known user with an unshibbolized org to the sign in form' do
+  it 'sends known user with an unshibbolized org', js: true do
 
   end
 
-  it 'sends known user with an shibbolized org to the SSO sign in form' do
+  it 'sends known user with an shibbolized org', js: true do
 
   end
 
-  it 'sends unknown user with an unknown email domain to the sign up form' do
+  it 'sends unknown user with an unknown email', js: true do
     fill_in 'Email address', with: Faker::Internet.unique.email
     click_on 'Continue'
 
     expect(page).to have_text('New Account Sign Up')
   end
 
-  it 'sends unknown user with a known email domain for an unshibbolized org get the sign up form' do
-    fill_in 'Email address', with: Faker::Internet.unique.email
+  it 'sends unknown user with a known email for an unshibbolized org', js: true do
+    fill_in 'Email address', with: "#{Faker::Lorem.unique.word.downcase}#{@email_domain}"
     click_on 'Continue'
 
     expect(page).to have_text('New Account Sign Up')
+
+pp page.body
+
+    expect(find('#org_autocomplete_name')).to eql(@org.name)
   end
 
-  it 'sends unknown user with a known email domain for an shibbolized org get the SSO sign up form' do
+  it 'sends unknown user with a known email for an shibbolized org', js: true do
     fill_in 'Email address', with: Faker::Internet.unique.email
     click_on 'Continue'
 
     expect(page).to have_text('New Account Sign Up')
+    #'Your address is associated with:'
+    #org.name
+    #'Sign in with Institution to Continue'
   end
 end
