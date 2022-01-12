d517061fa63c8a4b59e6d068075997f85b782598
Ray Carrick <ray.carrick@ed.ac.uk>
2022-01-12 12:25:58 +0000

    Update org_selectable.rb
    remove trailing space

--- app/controllers/concerns/org_selectable.rb
+++ app/controllers/concerns/org_selectable.rb
@@ -65,7 +65,7 @@ module OrgSelectable
     # Converts the incoming params_into an Org by either locating it
     # via its id, identifier and/or name, or initializing a new one
     # the default allow_create is based off restrict_orgs
-    def org_from_params(params_in:, 
+    def org_from_params(params_in:,
                         allow_create: !Rails.configuration.x.application.restrict_orgs)
       # params_in = params_in.with_indifferent_access
       return nil unless params_in[:org_id].present? &&
