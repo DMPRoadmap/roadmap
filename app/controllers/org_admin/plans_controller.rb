0c17d14bdc42892629fcba349f25597392c34227
Ray Carrick <ray.carrick@ed.ac.uk>
2022-01-28 11:43:26 +0000

    Rubocop fixes
    disable complexity warnings and change quotes to get rubocop happy.

--- app/controllers/org_admin/plans_controller.rb
+++ app/controllers/org_admin/plans_controller.rb
@@ -43,7 +43,7 @@ module OrgAdmin
     # rubocop:enable Metrics/AbcSize
 
     # GET /org_admin/download_plans
-    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+    # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity
     def download_plans
       # Test auth directly and throw Pundit error sincePundit
       # is unaware of namespacing
@@ -68,7 +68,7 @@ module OrgAdmin
           csv << [
             plan.title.to_s,
             plan.template.title.to_s,
-            (plan.owner&.org&.present? ? plan.owner.org.name : "").to_s,
+            (plan.owner&.org&.present? ? plan.owner.org.name : '').to_s,
             plan.owner&.name(false)&.to_s,
             plan.owner&.email&.to_s,
             l(plan.latest_update.to_date, format: :csv).to_s,
