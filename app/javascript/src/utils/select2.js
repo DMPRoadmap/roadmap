273cf25e96f7a76cb2a7f9ed4717c8cc63992148
Benjamin FAURE <benjamin.faure@inist.fr>
2022-03-03 10:55:10 +0100

    Solved issue where user couldn't access the search field in select2 selector in modals

--- app/javascript/src/utils/select2.js
+++ app/javascript/src/utils/select2.js
@@ -1,7 +1,8 @@
 export const Select2 = {
-  init(questionId = null) {
+  init(questionId = null, inModal = false) {
     $(`${questionId} .select-field select, .linked-fragments-select select, .schema-picker-zone select`).select2({
       theme: 'bootstrap4',
+      dropdownParent: inModal ? $(`${questionId}`) : $('body'),
     });
   },
 };
