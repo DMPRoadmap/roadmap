db7c44532481e835ae3e76804ab1128d04e5ee3c
briri <brian.riley@ucop.edu>
2022-03-24 07:43:07 -0700

    add stat generation back to monthly maintenance job but limit it to managed orgs

--- app/services/org/create_last_month_shared_plan_service.rb
+++ app/services/org/create_last_month_shared_plan_service.rb
@@ -15,7 +15,7 @@ class Org
   class CreateLastMonthSharedPlanService
     class << self
       def call(org = nil, threads: 0)
-        orgs = org.nil? ? Org.all : [org]
+        orgs = org.nil? ? Org.where(managed: true) : [org]
 
         Parallel.each(orgs, in_threads: threads) do |org_obj|
           months = OrgDateRangeable.split_months_from_creation(org_obj)
