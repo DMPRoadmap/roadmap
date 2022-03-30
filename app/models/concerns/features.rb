7fa9dc5bc4c6ccf18fce2cab211081485afc28a9
pengyin-shan <wendy.shan@hotmail.com>
2022-03-30 13:15:30 -0400

    change to secret

--- app/models/concerns/features.rb
+++ app/models/concerns/features.rb
@@ -1,9 +1,14 @@
 class Features
     def self.configuration
-        Rails.configuration.features
+        # Rails.configuration.features
+        # p Rails.configuration.features
+        # p "######################"
+        Rails.application.secrets.feature_flag
     end
 
     def self.enabled?(feature)
-        !!configuration[feature.to_s]
+        p feature
+        p Rails.application.secrets
+        configuration[feature.to_s].present?
     end
 end
