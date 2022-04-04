8b698cc72d7e0bb016446b2e838cc21abf766500
pengyin-shan <wendy.shan@hotmail.com>
2022-04-04 13:04:10 -0400

    change seed format to create seed for different environment

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,18 +3,5 @@
 # frozen_string_literal: true
 # warn_indent: true
 
-########## This file serves for the sandbox testing after 3.0 release
-# Steps:
-# 1) change default database to production database, then run `rails export:build_sandbox_data` to generate production-related data
-# 2) switch database to the sandbox database
-# 3) run rake db:seed or created alongside the db with db:setup to seed data
-##########
 
-# Forcing load seed file in sequence by last number
-# seeds_1 to seeds_3 are rake-generated. Other seeds file are manually edited
-puts 'run seeds.rb file now...'
-Dir[File.join(Rails.root, 'db', 'seeds', '*.rb')].sort.each_with_index do |seed, index|
-  if seed.include? index.to_s
-    load seed
-  end
-end
+load(Rails.root.join( 'db', 'seeds', "#{Rails.env.downcase}.rb")) # load seed file based on different environment
