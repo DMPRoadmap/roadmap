9a68a1972b587229572aa8a7ac6ee57cf8815f81
Nicolas Franck <nicolas.franck@ugent.be>
2022-04-05 18:20:26 +0200

    proposal to avoid used unpublished templates

--- app/controllers/org_admin/templates_controller.rb
+++ app/controllers/org_admin/templates_controller.rb
@@ -292,11 +292,13 @@ module OrgAdmin
     def unpublish
       template = Template.find(params[:id])
       authorize template
-      versions = Template.where(family_id: template.family_id)
-      versions.each do |version|
-        unless version.update_attributes!(published: false)
-          flash[:alert] = _("Unable to unpublish your #{template_type(template)}.")
+      Template.transaction do
+        # expected: template is latest
+        if template.published? && template.plans.any?
+          template.generate_version!
         end
+        Template.where(family_id: template.family_id)
+                .update_all(published: false)
       end
       flash[:notice] = _("Successfully unpublished your #{template_type(template)}") unless flash[:alert].present?
       redirect_to request.referrer.present? ? request.referrer : org_admin_templates_path
