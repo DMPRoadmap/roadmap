935d3de20cda10d051c54101c2d515735d3ef190
Brian Riley <brian.riley@ucop.edu>
2022-04-06 07:33:20 -0700

    Update schema.rb
    Updated schema.rb to have the timestamp of the new migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_160319) do
+ActiveRecord::Schema.define(version: 2022_03_15_104737) do
 
   create_table "annotations", id: :integer, force: :cascade do |t|
     t.integer "question_id"
