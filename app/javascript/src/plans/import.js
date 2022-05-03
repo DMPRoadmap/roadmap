2b7f71bf70a84aa4505d69157b35100959be2e31
Benjamin FAURE <benjamin.faure@inist.fr>
2022-05-03 14:21:26 +0200

    Added message for RDA import

--- app/javascript/src/plans/import.js
+++ app/javascript/src/plans/import.js
@@ -4,4 +4,12 @@ $(() => {
   $('body').on('click', '.plan-import', () => {
     toggleSpinner(true);
   });
+  $('body').on('change', '#import-format input:radio[name="import[format]"]', (e) => {
+    const $rdaMessage = $('.rda-message');
+    if ($(e.target).val() === 'rda') {
+      $rdaMessage.show();
+    } else {
+      $rdaMessage.hide();
+    }
+  });
 });
