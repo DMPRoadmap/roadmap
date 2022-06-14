2085e7d84ca3c3f095f56df3365c06ae05a8ddc2
briri <brian.riley@ucop.edu>
2022-06-14 15:37:20 -0700

    rubocop fixes and fix for index names in migration

--- db/migrate/20220614181654_add_uniqueness_indexes.rb
+++ db/migrate/20220614181654_add_uniqueness_indexes.rb
@@ -1,9 +1,9 @@
 class AddUniquenessIndexes < ActiveRecord::Migration[6.1]
   def change
-    add_index :departments, %i[name org_id], unique: true
-    add_index :orgs, :name, unique: true
-    add_index :perms, :name, unique: true
-    add_index :question_formats, :title, unique: true
-    add_index :regions, :abbreviation, unique: true
+    add_index :departments, %i[name org_id], unique: true, name: 'unique_departments'
+    add_index :orgs, :name, unique: true, name: 'unique_orgs'
+    add_index :perms, :name, unique: true, name: 'unique_perms'
+    add_index :question_formats, :title, unique: true, name: 'unique_question_formats'
+    add_index :regions, :abbreviation, unique: true, name: 'unique_regions'
   end
 end
