31f4190c0c800e3e72b2c070a8fe2e4ca56482fb
briri <brian.riley@ucop.edu>
2022-10-25 08:08:21 -0700

    removed duplicate require statement for js.erb

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,9 @@
 const { environment } = require('@rails/webpacker');
-const erb = require('./loaders/erb')
 // const ESLintPlugin = require('eslint-webpack-plugin');
 const erb = require('./loaders/erb');
 const webpack = require('webpack');
 
-environment.loaders.prepend('erb', erb);
+// environment.loaders.prepend('erb', erb);
 
 environment.plugins.append(
   'Provide',
