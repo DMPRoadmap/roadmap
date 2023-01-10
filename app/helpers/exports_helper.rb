c5378e350517f0332fa5eb4ab53bbfa6746b546f
briri <brian.riley@ucop.edu>
2023-01-10 11:08:03 -0800

    update font selector on download page to use Roboto

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -10,7 +10,7 @@ module ExportsHelper
   }.freeze
 
   def font_face
-    @formatting[:font_face].presence || 'Arial, Helvetica, Sans-Serif'
+    @formatting[:font_face].presence || 'Roboto, Helvetica, Sans-Serif'
   end
 
   def font_size
