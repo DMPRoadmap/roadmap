8e074e0b40beec403b2f71ff5fa8b71c9907033d
Benjamin FAURE <benjamin.faure@inist.fr>
2023-01-16 11:38:33 +0100

    Rubocop fixes

--- config/initializers/asset_url_processor.rb
+++ config/initializers/asset_url_processor.rb
@@ -5,7 +5,7 @@ class AssetUrlProcessor
   def self.call(input)
     context = input[:environment].context_class.new(input)
     data = input[:data].gsub(/asset-url\(["']?(.+?)["']?\)/) do |_match|
-      "url(#{context.asset_path($1)})"
+      "url(#{context.asset_path(::Regexp.last_match(1))})"
     end
     { data: data }
   end
