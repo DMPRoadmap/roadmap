d6ee0ff6b0cf23655b136c489762c9aa4fcfaded
briri <brian.riley@ucop.edu>
2023-01-23 07:47:41 -0800

    updated api v0 to use max_pages config value for api

--- app/controllers/api/v0/base_controller.rb
+++ app/controllers/api/v0/base_controller.rb
@@ -6,6 +6,7 @@ module Api
     class BaseController < ApplicationController
       protect_from_forgery with: :null_session
       before_action :define_resource, only: %i[destroy show update]
+      before_action :pagination_params, only: %i[index]
       respond_to :json
 
       # POST /api/{plural_resource_name}
