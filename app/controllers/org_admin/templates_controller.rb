9388d175fe584fd7f530693588c97a6724b3e528
briri <brian.riley@ucop.edu>
2023-01-24 13:32:01 -0800

    fixed typo

--- app/controllers/org_admin/templates_controller.rb
+++ app/controllers/org_admin/templates_controller.rb
@@ -389,7 +389,7 @@ module OrgAdmin
       # If present, we parse to retrieve the value
       if args[:visibility].nil?
         org.funder? ? Template.visibilities[:publicly_visible] : Template.visibilities[:organisationally_visible]
-      elsif args.fetch(:visibility, '0') == '1'
+      elsif ['0', 'organisationally_visible'].include?(args.fetch(:visibility, 'publicly_visible'))
         Template.visibilities[:organisationally_visible]
       else
         Template.visibilities[:publicly_visible]
