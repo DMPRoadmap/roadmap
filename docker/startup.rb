c48ef1dec155e558277ef43f94573be5b7118487
briri <brian.riley@ucop.edu>
2023-03-27 13:37:35 -0700

    fixed issue with docker startup script

--- docker/startup.rb
+++ docker/startup.rb
@@ -12,7 +12,7 @@ def system!(*args)
   system(*args) || abort("\n== Command #{args} failed ==")
 end
 
-chdir "#{APP_ROOT}" do
+chdir "#{APP_ROOT}/application" do
   prefix = "RAILS_ENV=#{ENV['RAILS_ENV']}"
   system! "bin/rails db:environment:set RAILS_ENV=#{ENV['RAILS_ENV']}"
 
