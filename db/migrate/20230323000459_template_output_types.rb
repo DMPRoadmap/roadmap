a1ee5e8a6142e34d3433da178b90bc32532a1615
Terry Brady <terrywbrady@gmail.com>
2023-03-28 11:02:55 -0700

    fix foreign keys

--- db/migrate/20230323000459_template_output_types.rb
+++ db/migrate/20230323000459_template_output_types.rb
@@ -1,7 +1,7 @@
 class TemplateOutputTypes < ActiveRecord::Migration[6.1]
   def change
     create_table :template_output_types do |t|
-      t.belongs_to :template, foreign_key: true, index: true
+      t.belongs_to :template, type: :integer, foreign_key: true, index: true
       t.string "research_output_type"
     end
   end
