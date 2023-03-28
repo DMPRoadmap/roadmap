021740278bc5f41303e87214b20088f0453a26ec
Terry Brady <terrywbrady@gmail.com>
2023-03-27 17:12:47 -0700

    license customize

--- app/controllers/org_admin/templates_controller.rb
+++ app/controllers/org_admin/templates_controller.rb
@@ -108,7 +108,10 @@ module OrgAdmin
       render 'preferences', locals: {
         partial_path: 'edit',
         template: template,
-        output_types: ResearchOutput.output_types
+        output_types: ResearchOutput.output_types,
+        preferred_licenses: License.preferred.map { |license| [license.identifier, license.id] },
+        licenses: License.selectable.map{ |license| [license.identifier, license.id] },
+        my_licenses: []
       }
     end
 
--- app/javascript/src/dmptool/orgAdmin/templates/preferences.js
+++ app/javascript/src/dmptool/orgAdmin/templates/preferences.js
@@ -1,5 +1,3 @@
-import { Tinymce } from '../../../utils/tinymce';
-
 $(() => {
   $('a.output_type_remove').on('click', (e) => {
     e.stopPropagation();
@@ -40,7 +38,7 @@ $(() => {
     const vclass = checkOutputType('#default-output-types', vnorm) ? 'standard' : 'custom';
     const li = $('<li/>').addClass('selectable_item').addClass('output_type').addClass(vclass)
       .appendTo('#my-output-types ul');
-    const a = $('<a href="#" aria-label="Remove this output type"/>').addClass('output_type_remove').appendTo(li);
+    const a = $('<a aria-label="Remove this output type"/>').addClass('output_type_remove').appendTo(li);
     a.on('click', (e) => {
       e.stopPropagation();
       $(e.currentTarget).parents('li.output_type').remove();
@@ -78,6 +76,83 @@ $(() => {
       addOutputType(v);
     }
     $('#new_output_type').val('');
+    return false;
+  });
+});
+
+$(() => {
+  $('a.license_remove').on('click', (e) => {
+    e.stopPropagation();
+    $(e.currentTarget).parents('li.license').remove();
+  });
+
+  function showLicenseSelections() {
+    if ($('#customize_licenses_sel').val() === '0') {
+      $('#default-licenses').show();
+      $('#my-licenses').hide();
+      $('#my-licenses input.license').attr('disabled', true);
+    } else {
+      $('#default-licenses').hide();
+      $('#my-licenses').show();
+      $('#my-licenses input.license').attr('disabled', false);
+    }
+  }
+  showLicenseSelections();
+
+  function checkLicense(sel, v) {
+    let res = false;
+    const ns = $(sel).find('ul li.license');
+    ns.each((n) => {
+      const node = $(ns.get(n));
+      if (v === node.find('input.license').val()) {
+        res = true;
+      }
+    });
+    return res;
+  }
+
+  function addLicense(id, v) {
+    if (checkLicense('#my-licenses', id)) {
+      return;
+    }
+    const li = $('<li/>').addClass('selectable_item').addClass('license')
+      .appendTo('#my-licenses ul');
+    const a = $('<a aria-label="Remove this license"/>').addClass('license_remove').appendTo(li);
+    a.on('click', (e) => {
+      e.stopPropagation();
+      $(e.currentTarget).parents('li.license').remove();
+    });
+    const span = $('<span/>').addClass('selectable_item_label').appendTo(a);
+    span.text(v);
+    const index = $('#my-licenses ul li').length;
+    $('<i class="fas fa-times-circle fa-reverse remove-license" aria-hidden="true"/>').appendTo(a);
+    const name = `template[template_licenses_attributes[${index}][research_license]]`;
+    $('<input class="license" type="hidden" autocomplete="off"/>').attr('data', name).val(id).appendTo(li);
+  }
+
+  $('input.license_init').each((n) => {
+    const node = $($('input.license_init').get(n));
+    addLicense(node.val(), node.attr('name'));
+  }).remove();
+
+  $('#customize_licenses_sel').on('change', (e) => {
+    e.stopPropagation();
+    if ($('#customize_licenses_sel').val() === '1') {
+      if ($('#my-licenses ul li').length === 0) {
+        $('#default-licenses ul li.license').each((n) => {
+          const node = $($('#default-licenses ul li.license').get(n));
+          addLicense(node.find('input.license').val(), node.find('input.license').attr('data'));
+        });
+      }
+    }
+    showLicenseSelections();
+  });
+
+  $('#add_license').on('click', () => {
+    const v = $('#new_license').val();
+    if (v !== '') {
+      addLicense(v, $('#new_license option:selected').text());
+    }
     return false;
   });
 });
