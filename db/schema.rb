cf82d85e4acad9079fd2f714c905f544ffb7af5b
Terry Brady <terrywbrady@gmail.com>
2023-04-07 12:01:24 -0700

    commit label in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_29_235100) do
+ActiveRecord::Schema.define(version: 2023_04_04_231434) do
 
   create_table "annotations", id: :integer, force: :cascade do |t|
     t.integer "question_id"
