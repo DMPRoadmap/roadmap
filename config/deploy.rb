eb087b4e63f7c46fe49c36762cb803e00445a9ae
briri <brian.riley@ucop.edu>
2023-04-14 12:35:17 -0700

    trying to segregate development gems out of deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -11,7 +11,7 @@ set :branch,           ENV.fetch('BRANCH', nil)          || 'master'
 # Gets the current Git tag and revision
 set :version_number, `git describe --tags`
 # Default environments to skip
-set :bundle_without, %w[pgsql thin rollbar test].join(' ')
+set :bundle_without, %w[pgsql thin rollbar test development].join(' ')
 # We only need to keep 3 releases
 set :keep_releases, 3
 
