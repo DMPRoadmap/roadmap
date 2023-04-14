468e7ea7722be31b3ab418e43af068b81e0a4d0f
briri <brian.riley@ucop.edu>
2023-04-14 14:18:53 -0700

    make rubocop happy

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require_relative "boot"
 
 require "rails/all"
@@ -25,6 +27,7 @@ if ENV.key?('SSM_ROOT_PATH')
 end
 
 module DMPRoadmap
+  # The DMPRoadmap Rails application
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
@@ -38,40 +41,40 @@ module DMPRoadmap
     # config.eager_load_paths << Rails.root.join("extras")
 
     # --------------------------------#
-   # OVERRIDES TO DEFAULT RAILS CONFIG #
-   # --------------------------------#
-   # Ensure that Zeitwerk knows to load our classes in the lib directory
-   config.eager_load_paths << config.root.join('lib')
+    # OVERRIDES TO DEFAULT RAILS CONFIG #
+    # --------------------------------#
+    # Ensure that Zeitwerk knows to load our classes in the lib directory
+    config.eager_load_paths << config.root.join('lib')
 
-   # CVE-2022-32224: add some compatibility with YAML.safe_load
-   # Rails 5,6,7 are using YAML.safe_load as the default YAML deserializer
-   config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess, Symbol, Date, Time]
+    # CVE-2022-32224: add some compatibility with YAML.safe_load
+    # Rails 5,6,7 are using YAML.safe_load as the default YAML deserializer
+    config.active_record.yaml_column_permitted_classes = [ActiveSupport::HashWithIndifferentAccess, Symbol, Date, Time]
 
-   # Have Zeitwerk skip generators because the generator templates are
-   # incompatible with the Rails module/class naming conventions
-   Rails.autoloaders.main.ignore(config.root.join('lib/generators'))
+    # Have Zeitwerk skip generators because the generator templates are
+    # incompatible with the Rails module/class naming conventions
+    Rails.autoloaders.main.ignore(config.root.join('lib/generators'))
 
-   # HTML tags that are allowed to pass through `sanitize`.
-   config.action_view.sanitized_allowed_tags = %w[
-     p br strong em a table thead tbody tr td th tfoot caption ul ol li
-   ]
+    # HTML tags that are allowed to pass through `sanitize`.
+    config.action_view.sanitized_allowed_tags = %w[
+      p br strong em a table thead tbody tr td th tfoot caption ul ol li
+    ]
 
-   # Configure sensitive parameters which will be filtered from the log file.
-   config.filter_parameters += [:password]
+    # Configure sensitive parameters which will be filtered from the log file.
+    config.filter_parameters += [:password]
 
-   # Enable escaping HTML in JSON.
-   config.active_support.escape_html_entities_in_json = true
+    # Enable escaping HTML in JSON.
+    config.active_support.escape_html_entities_in_json = true
 
-   # Allow controllers to access view helpers
-   # TODO: We should see what methods specifically are used by the controllers
-   #       and include them specifically in the controllers. We should also consider
-   #       moving our helper methods into Presenters if it makes sense
-   config.action_controller.include_all_helpers = true
+    # Allow controllers to access view helpers
+    # TODO: We should see what methods specifically are used by the controllers
+    #       and include them specifically in the controllers. We should also consider
+    #       moving our helper methods into Presenters if it makes sense
+    config.action_controller.include_all_helpers = true
 
-   # Load AnywayConfig class, but not if running `rails credentials:edit`
-   config.x.dmproadmap = DmproadmapConfig.new unless defined?(::Rails::Command::CredentialsCommand)
+    # Load AnywayConfig class, but not if running `rails credentials:edit`
+    config.x.dmproadmap = DmproadmapConfig.new unless defined?(::Rails::Command::CredentialsCommand)
 
-   # Set the default host for mailer URLs
-   config.action_mailer.default_url_options = { host: config.x.dmproadmap.server_host }
+    # Set the default host for mailer URLs
+    config.action_mailer.default_url_options = { host: config.x.dmproadmap.server_host }
   end
 end
