c7c10e372a8f7263172d3b1d09c9487f184dc8d0
briri <brian.riley@ucop.edu>
2023-06-12 08:32:16 -0700

    updates to cors rules

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,17 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  host = Rails.configuration.x.dmproadmap.server_host
+  host = "https://#{host}" unless host.start_with?('http')
+  localhost = 'http://localhost:3000'
+
   allow do
     if Rails.env.production?
-      origins ENV['DMPROADMAP_HOST']
+      origins host
+    elsif Rails.env.stage?
+      origins localhost, host
     else
-      origins 'http://localhost:3000', ENV['DMPROADMAP_HOST']
+      origins localhost
     end
 
     resource '*', headers: :any, methods: %i[get put post delete]
