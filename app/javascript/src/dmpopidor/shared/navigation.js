2225fc3d8e4849a15ecc97c8688332a6b046cb0c
WZidane <wail.zidane@hotmail.com>
2023-06-15 11:13:11 +0200

    refactor: update login and register forms behavior

--- app/javascript/src/dmpopidor/shared/navigation.js
+++ app/javascript/src/dmpopidor/shared/navigation.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('scroll', (event) => {
+
         let scrollY = window.scrollY;
         let width = screen.width;
         let body = document.querySelector('body');
@@ -9,19 +10,34 @@ document.addEventListener('DOMContentLoaded', function () {
         } else {
             body.classList.remove('is-scrolled');
         }
+
     });
 
     const button = document.getElementById('create-account-over');
-    const form = document.getElementById('create-account-form');
+    const formRegister = document.getElementById('create-account-form');
+    const formLogin = document.getElementById('sign-in-form');
+    let div = document.querySelector('div.col-md-12 div.sign-in div.tab-content');
 
     button.addEventListener('click', () => {
+
         button.style.display = "none";
-        form.style.opacity = "0";
-        form.style.transition = "visibility 0s 2s, opacity 2s linear;";
-        form.style.display = "block";
+        div.style.alignItems = "flex-start";
+        formRegister.style.opacity = "0";
+        formRegister.style.transition = "visibility 0s 2s, opacity 2s linear;";
+        formRegister.style.display = "block";
+        
+        
+        console.log(div);
+
         setTimeout(() => {
-            form.style.opacity = "1";
+            formRegister.style.opacity = "1";
+            formLogin.style.transition = "visibility 0s 2s, opacity 2s linear;";
+            formLogin.style.opacity = "0";
         }, 100)
+
+        setTimeout(() => {
+            formLogin.style.display = "none";
+        }, 2000)
     });
 
 });
