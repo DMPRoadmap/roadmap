93b4307689122d9af3fd7fb7ad19f4599831840a
WZidane <wail.zidane@hotmail.com>
2023-06-19 15:59:18 +0200

    refactor: update scroll behavior

--- app/javascript/src/dmpopidor/shared/navigation.js
+++ app/javascript/src/dmpopidor/shared/navigation.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
         let width = screen.width;
         let body = document.querySelector('body');
 
-        if (scrollY > 100 && width > 767) {
+        if (scrollY > 50 && width > 767) {
             body.classList.add('is-scrolled');
         } else {
             body.classList.remove('is-scrolled');
