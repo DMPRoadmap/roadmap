68f893df999af7fdd1073fffe92afc5eb4c78a79
Benjamin FAURE <benjamin.faure@inist.fr>
2023-06-23 08:42:06 +0200

    Updated static pages TinyMCE config for TinyMCE 6

--- app/javascript/src/superAdmin/staticPages/edit.js
+++ app/javascript/src/superAdmin/staticPages/edit.js
@@ -4,9 +4,7 @@ import 'tinymce/plugins/code';
 $(() => {
   Tinymce.init({
     selector: '.content-editor',
-    forced_root_block: '',
     toolbar: 'bold italic underline | formatselect fontsizeselect forecolor backcolor | bullist numlist | link | code',
-    plugins: 'table autoresize link paste advlist lists code',
-    menubar: 'toolbar',
+    plugins: 'table autoresize link advlist lists code',
   });
 });
