d8bb39e6f4a20e5d3c5fb874c41c8184f917c450
Fraser Clark <me@fraserclark.com>
2023-07-12 14:36:28 +0200

    fix redundant spaces

--- react-client/src/pages/dashboard/dashboard.js
+++ react-client/src/pages/dashboard/dashboard.js
@@ -1,21 +1,15 @@
-import {
-  useEffect,
-  useState,
-  Fragment,
-} from 'react';
+import { useEffect, useState, Fragment } from "react";
 
-import {
-  useNavigate,
-  Link
-} from 'react-router-dom';
+import { useNavigate, Link } from "react-router-dom";
 
-import { DmpApi } from '../../api.js';
+import { DmpApi } from "../../api.js";
+import "./dashboard.scss";
 
 function Dashboard() {
   const [projects, setProjects] = useState([]);
   const [user, setUser] = useState({
-    givenname: '',
-    surname: '',
+    givenname: "",
+    surname: "",
   });
 
   let navigate = useNavigate();
@@ -23,23 +17,26 @@ function Dashboard() {
   useEffect(() => {
     let api = new DmpApi();
 
-    fetch(api.getPath('/me'), api.getOptions()).then((resp) => {
-      api.handleResponse(resp)
-      return resp.json();
-    }).then((data) => {
-      setUser(data.items[0]);
-    });
+    fetch(api.getPath("/me"), api.getOptions())
+      .then((resp) => {
+        api.handleResponse(resp);
+        return resp.json();
+      })
+      .then((data) => {
+        setUser(data.items[0]);
+      });
 
     // Fetch the work in progress DMPs for the currently logged in user
-    fetch(api.getPath('/dmps'), api.getOptions()).then((resp) => {
-      api.handleResponse(resp);
-      return resp.json();
-    }).then((data) => {
-      console.log(data.items);
-      setProjects(data.items);
-    });
+    fetch(api.getPath("/dmps"), api.getOptions())
+      .then((resp) => {
+        api.handleResponse(resp);
+        return resp.json();
+      })
+      .then((data) => {
+        console.log(data.items);
+        setProjects(data.items);
+      });
   }, []);
-
 
   function dmp_id_for(dmp) {
     return dmp.dmphub_wip_id.identifier;
@@ -47,48 +44,60 @@ function Dashboard() {
 
   return (
     <div id="Dashboard">
-
-      <p>Welcome back {user.givenname} {user.surname}</p>
-      <p><a href="/plans" className="exit-prototype">Back to standard Dashboard</a></p>
-
-
+      <p>
+        Welcome back {user.givenname} {user.surname}
+      </p>
+      <p>
+        <a href="/plans" className="exit-prototype">
+          Back to standard Dashboard
+        </a>
+      </p>
 
       <div className="dmpui-heading with-action-button">
         <div>
           <h1>Dashboard</h1>
         </div>
         <div>
-          <button className="primary" onClick={() => navigate("/dashboard/dmp/new")}>
+          <button
+            className="primary"
+            onClick={() => navigate("/dashboard/dmp/new")}
+          >
             Add Plan
           </button>
         </div>
       </div>
 
-
-
       <div className="plan-steps">
         <div className="project-list todo">
-          <div className="data-heading" data-colname="title">Project Name</div>
-          <div className="data-heading" data-colname="funder">Funder</div>
-          <div className="data-heading" data-colname="grantId">Grant ID</div>
-          <div className="data-heading" data-colname="dmpId">DMP ID</div>
-          <div className="data-heading" data-colname="status">Status</div>
+          <div className="data-heading" data-colname="title">
+            Project Name
+          </div>
+          <div className="data-heading" data-colname="funder">
+            Funder
+          </div>
+          <div className="data-heading" data-colname="grantId">
+            Grant ID
+          </div>
+          <div className="data-heading" data-colname="dmpId">
+            DMP ID
+          </div>
+          <div className="data-heading" data-colname="status">
+            Status
+          </div>
           <div className="data-heading" data-colname="actions"></div>
 
-          {projects.map(item => (
+          {projects.map((item) => (
             <Fragment key={item.dmp.wip_id.identifier}>
               <div data-colname="title">{item.dmp?.title}</div>
               <div data-colname="funder">{item?.funder}</div>
               <div data-colname="grantId">tbd…</div>
-              <div data-colname="dmpId">
-                {item.dmp.wip_id.identifier}
-              </div>
+              <div data-colname="dmpId">{item.dmp.wip_id.identifier}</div>
               <div data-colname="status">
                 Incomplete <br />
                 <progress max="10" value="3" />
               </div>
               <div data-colname="actions">
-                <Link to={`/dashboard/dmp/${item.dmp.wip_id.identifier}`} >
+                <Link to={`/dashboard/dmp/${item.dmp.wip_id.identifier}`}>
                   Complete
                 </Link>
               </div>
@@ -100,5 +109,4 @@ function Dashboard() {
   );
 }
 
-
-export default Dashboard
+export default Dashboard;
--- react-client/src/pages/plan/new-plan/new.js
+++ react-client/src/pages/plan/new-plan/new.js
@@ -1,9 +1,7 @@
-import useContext from 'react';
-import { useNavigate } from 'react-router-dom';
-import { DmpApi } from '../../../api';
-import TextInput from '../../../components/text-input/textInput';
-
-
+import useContext from "react";
+import { useNavigate } from "react-router-dom";
+import { DmpApi } from "../../../api";
+import TextInput from "../../../components/text-input/textInput";
 
 function PlanNew() {
   let navigate = useNavigate();
@@ -18,43 +16,39 @@ function PlanNew() {
     const form = ev.target;
     const formData = new FormData(form);
 
-    formData.forEach((value, key) => stepData[key] = value);
+    formData.forEach((value, key) => (stepData[key] = value));
 
-    const fileResult = await api.getFileDataURL(stepData['project_pdf'])
+    const fileResult = await api.getFileDataURL(stepData["project_pdf"]);
 
     let options = api.getOptions({
       method: "post",
       body: JSON.stringify({
-        "dmp": {
-          "title": stepData['project_name'],
-          "narrative": fileResult,
-        }
+        dmp: {
+          title: stepData["project_name"],
+          narrative: fileResult,
+        },
       }),
     });
 
-    fetch(api.getPath('/dmps'), options).then((resp) => {
-      api.handleResponse(resp.status);
-      return resp.json();
-    }).then((data) => {
-      let dmp = data.items[0].dmp;
-      navigate(`/dashboard/dmp/${dmp.wip_id.identifier}`);
-    });
+    fetch(api.getPath("/dmps"), options)
+      .then((resp) => {
+        api.handleResponse(resp.status);
+        return resp.json();
+      })
+      .then((data) => {
+        let dmp = data.items[0].dmp;
+        navigate(`/dashboard/dmp/${dmp.wip_id.identifier}`);
+      });
   }
 
   return (
     <div id="planNew">
-
       <div className="dmpui-heading">
         <h1>New Plan</h1>
       </div>
 
-
-
-
       <form method="post" enctype="multipart/form-data" onSubmit={handleSubmit}>
         <div className="form-wrapper">
-
-
           <div className="dmpui-form-cols">
             <div className="dmpui-form-col">
               <TextInput
@@ -72,17 +66,12 @@ function PlanNew() {
 
           <div className="dmpui-form-cols">
             <div className="dmpui-form-col">
-              <div
-                className={'dmpui-field-group'}
-              >
-                <label className="dmpui-field-label">
-                  Upload DMP
-                </label>
+              <div className={"dmpui-field-group"}>
+                <label className="dmpui-field-label">Upload DMP</label>
                 <p className="dmpui-field-help">
                   Only PDFs may be uploaded, and files should be no more than
                   250kb.
                 </p>
-
 
                 <div className="dmpui-field-fileinput-group  ">
                   <div className="">
@@ -92,21 +81,19 @@ function PlanNew() {
               </div>
             </div>
           </div>
-
-
         </div>
 
         <div className="form-actions ">
-          <button type="button" onClick={() => navigate(-1)}>Cancel</button>
+          <button type="button" onClick={() => navigate(-1)}>
+            Cancel
+          </button>
           <button type="submit" className="primary">
             Save &amp; Continue
           </button>
         </div>
       </form>
-
-
     </div>
-  )
+  );
 }
 
 export default PlanNew;
