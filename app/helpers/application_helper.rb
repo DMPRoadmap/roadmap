905539608286078ed58bb78a22bb5f1ffdc79c19
yashu <ydk31137@gmail.com>
2023-07-13 15:36:08 -0400

    resolved rubocop issues

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -86,7 +86,7 @@ module ApplicationHelper
 
   def contacts_at_your_instutution_path
     if I18n.locale == 'fr_CA'
-      # 'https://portagenetwork.ca/fr/outils-et-ressources/personnes-ressources-pour-la-gdr-dans-les-etablissements/' #Old URL
+      # 'https://portagenetwork.ca/fr/outils-et-ressources/personnes-ressources-pour-la-gdr-dans-les-etablissements/'
       'https://alliancecan.ca/fr/services/gestion-des-donnees-de-recherche/apprentissage-et-ressources/personnes-ressources-dans-les-etablissements'
     else
       # Handling "en_CA" locale
