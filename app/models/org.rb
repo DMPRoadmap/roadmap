2b02759784b2f58a7931ef60a5d71a6a829c2c17
Benjamin FAURE <benjamin.faure@inist.fr>
2023-09-29 14:04:06 +0200

    Enable org search without accent

--- app/models/org.rb
+++ app/models/org.rb
@@ -207,8 +207,8 @@ class Org < ApplicationRecord
   scope :unmanaged, -> { where(managed: false) }
 
   scope :search, lambda { |term|
-    search_pattern = "%#{term}%"
-    where('lower(orgs.name) LIKE lower(?) OR ' \
+    search_pattern = I18n.transliterate "%#{term}%"
+    where('lower(UNACCENT(orgs.name)) LIKE lower(UNACCENT(?)) OR ' \
           'lower(orgs.contact_email) LIKE lower(?)',
           search_pattern, search_pattern)
   }
