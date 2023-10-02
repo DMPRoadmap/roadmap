9efb33c3965a023d003363f835d47f105911bd6b
briri <brian.riley@ucop.edu>
2023-10-02 11:50:10 -0700

    update plan model's notify_subscribers method to stop publishing pdf if no dmp_id. Fixed bug in PdfPublisherJob

--- app/jobs/pdf_publisher_job.rb
+++ app/jobs/pdf_publisher_job.rb
@@ -48,9 +48,6 @@ class PdfPublisherJob < ApplicationJob
 
     # Send it to DMPHub if it has a DMP ID otherwise store it in local ActiveStorage
     has_dmp_id = plan.dmp_id.present?
-
-p "Plan #{plan.id} has a DMP ID? #{has_dmp_id} --> #{plan.dmp_id}"
-
     _publish_to_dmphub(plan: plan, pdf_file_name: pdf_file_name) if has_dmp_id
     _publish_locally(plan: plan, pdf_file: pdf_file) unless has_dmp_id
 
@@ -59,7 +56,7 @@ p "Plan #{plan.id} has a DMP ID? #{has_dmp_id} --> #{plan.dmp_id}"
   end
 
   # Publish the PDF to local ActiveStorage
-  def _publish_locally(plan:, pdf_file_name:)
+  def _publish_locally(plan:, pdf_file:)
     plan.narrative = pdf_file
     if plan.save
       Rails.logger.info "PdfPublisherJob._publish_locally successfully published PDF for #{plan.dmp_id} at #{hash[:narrative_url]}"
