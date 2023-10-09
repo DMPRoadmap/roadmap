53a466d253de9d8306a99ecbe8ecd4434c482a82
Andre Engelbrecht <andre@teh-node.co.za>
2023-10-09 10:59:48 +0100

    ReactUI: WIP - Plan setup should be editable

--- react-client/src/pages/plan/setup/setup.js
+++ react-client/src/pages/plan/setup/setup.js
@@ -3,7 +3,10 @@ import { useNavigate, useParams } from "react-router-dom";
 
 import { DmpModel, getDraftDmp } from '../../../models.js';
 import { DmpApi } from "../../../api.js";
+
 import TextInput from "../../../components/text-input/textInput.js";
+import Checkbox from "../../../components/checkbox/checkbox.js";
+
 import "./setup.scss";
 
 
@@ -12,6 +15,7 @@ function DmpSetup() {
 
   const {dmpId} = useParams();
   const [dmp, setDmp] = useState(new DmpModel({}));
+  const [fileError, setFileError] = useState(null);
 
   useEffect(() => {
     if (typeof dmpId !== "undefined") {
@@ -21,33 +25,58 @@ function DmpSetup() {
     }
   }, [dmpId]);
 
+
+  function isValid(fdata) {
+    setFileError(null);
+    if (fdata.get("narrative").size > 256000) {
+      setFileError("File size cannot exceed 250 kb");
+      return false;
+    }
+    return true;
+  }
+
+
   async function handleSubmit(ev) {
     ev.preventDefault();
-    let api = new DmpApi();
 
+    let api = new DmpApi();
     let formData = new FormData(ev.target);
 
-    // NOTE: Remove the content-type header so that rails and browser will
-    // figure it out If we don't do this, then the request always fail.
-    var headers = api.getHeaders();
-    headers.delete('Content-Type');
+    console.log("form data?");
+    for (const pair of formData.entries()) {
+      console.log(`${pair[0]}, ${pair[1]}`);
+    }
 
-    let options = api.getOptions({
-      headers: headers,
-      method: "post",
-      body: formData,
-    });
+    if (isValid(formData)) {
+      if (!dmpId) {
 
-    fetch(api.getPath("/drafts"), options)
-      .then((resp) => {
-        api.handleResponse(resp);
-        return resp.json();
-      })
-      .then((data) => {
-        let dmp = data.items[0].dmp;
-        navigate(`/dashboard/dmp/${dmp.draft_id.identifier}/funders`);
-      });
+        // NOTE: Remove the content-type header so that rails and browser will
+        // figure it out If we don't do this, then the request always fail.
+        var headers = api.getHeaders();
+        headers.delete('Content-Type');
+
+        let options = api.getOptions({
+          headers: headers,
+          method: "post",
+          body: formData,
+        });
+
+        fetch(api.getPath("/drafts"), options)
+          .then((resp) => {
+            api.handleResponse(resp);
+            return resp.json();
+          })
+          .then((data) => {
+            let dmp = data.items[0].dmp;
+            navigate(`/dashboard/dmp/${dmp.draft_id.identifier}/funders`);
+          });
+      } else {
+        console.log("Hello existing DMP!");
+        console.log(formData.get("remove_narrative"));
+      }
+    }
   }
+
 
   return (
     <div id="planNew">
@@ -81,9 +110,19 @@ function DmpSetup() {
                   Only PDFs may be uploaded, and files should be no more than
                   250kb.
                 </p>
+                {fileError && <p className="dmpui-field-error"> {fileError} </p>}
 
-                <div className="dmpui-field-fileinput-group  ">
-                  <div className="">
+                <div className="dmpui-field-fileinput-group">
+                  <div>
+                    <Checkbox
+                      label="Remove PDF"
+                      name="remove_narrative"
+                      id="primaryContact"
+                      inputValue="yes"
+                    />
+
+                    <br />
+
                     <input
                       name="narrative"
                       type="file"
@@ -100,15 +139,10 @@ function DmpSetup() {
           <button type="button" onClick={() => navigate('/dashboard')}>
             Cancel
           </button>
-          {dmpId ? (
-              // TODO: This will render the button to "UPDATE" the dmp title
-              // and PDF
-              ""
-            ) : (
-              <button type="submit" className="primary">
-                Save &amp; Continue
-              </button>
-            )}
+
+          <button type="submit" className="primary">
+            Save &amp; Continue
+          </button>
         </div>
       </form>
     </div>
