f1ec3d5e607b5cf9e0260ced2ff6fa0041c0f74f
wilmouths <wilmouthsteven@gmail.com>
2023-10-10 16:26:35 +0200

    refactor: create dmp-comment tag

--- app/javascript/remount.js
+++ app/javascript/remount.js
@@ -5,6 +5,7 @@ import NewsPage from './dmp_opidor_react/src/components/news/NewsPage.jsx';
 import GeneralInfoLayout from './dmp_opidor_react/src/components/GeneralInfo/GeneralInfoLayout.jsx';
 import PlanCreationLayout from './dmp_opidor_react/src/components/PlanCreation/PlanCreationLayout.jsx';
 import WritePlanLayout from './dmp_opidor_react/src/components/WritePlan/WritePlanLayout.jsx';
+import Comment from './dmp_opidor_react/src/components/Shared/Comment.jsx';
 
 define({
   'dmp-homepage-news-page': HomepageNews,
@@ -12,4 +13,5 @@ define({
   'dmp-general-info': GeneralInfoLayout,
   'dmp-plan-creation': PlanCreationLayout,
   'dmp-write-plan': WritePlanLayout,
+  'dmp-comment': Comment,
 });
