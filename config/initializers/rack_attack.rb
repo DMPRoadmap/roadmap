6d90f5a10d8f2b424961f5ac93ad3dd8f992c843
briri <brian.riley@ucop.edu>
2023-10-13 07:46:03 -0700

    tweak rack_attack response

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -12,8 +12,14 @@ Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new # defaults to R
 
 # Throttle should send a 429 Error responsec code and display public/429.html
 Rack::Attack.throttled_responder = lambda do |_env|
-  html = ActionView::Base.empty.render(file: Rails.root.join('public/429.html'))
-  [429, { 'Content-Type' => 'text/html' }, [html]]
+  # html = ActionView::Base.empty.render(file: Rails.root.join('public/429.html'))
+  # [429, { 'Content-Type' => 'text/html' }, [html]]
+  details = request.env
+  Rails.logger.warn("RackAttack throttled: Matched: #{details['rack.attack.matched']}, \
+                     Type: #{details['rack.attack.match_type']}, Data: #{details['rack.attack.match_data']}, \
+                     Discriminator: #{['rack.attack.match_discriminator']}"
+
+  [ 429, {}, ["Too Many Requests.\n"] ]
 end
 
 # Throttle attempts to a particular path. 2 POSTs to /users/password every 30 seconds
