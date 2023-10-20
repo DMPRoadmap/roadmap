5991094c7ce8f74a1ce5ec0622979a7aae978d7e
gjacob24 <gjacob@ed.ac.uk>
2023-10-20 15:08:53 +0100

    Issue #3144 - more styling fixes Changes in this PR: - fixed copy link modal styling - fixed show password checkbox - added padding for accordion in themed guidance - removed round modal edges

--- app/javascript/src/publicTemplates/show.js
+++ app/javascript/src/publicTemplates/show.js
@@ -1,10 +1,33 @@
-$(() => {
-  $('body').on('click', '.copy-link', (e) => {
-    const link = $(e.currentTarget).siblings('.direct-link');
+// $(() => {
+//   $('body').on('click', '#copy-link', (e) => {
+//     const link = $(e.currentTarget).siblings('.direct-link');
+//     link.click();
 
-    $('#link-modal').on('show.bs.modal', () => {
-      $('#link').val(link.attr('href'));
-    });
+//     $('#link-modal').on('show.bs.modal', (e) => {
+//       const modal = $(e.currentTarget);
+//       modal.click();
+//       $('#link').val(link.attr('href'));
+//     });
+//   });
+
+//   $('body').on('click', '#copy-link-btn', () => {
+//     $('#link').select();
+//     // eslint-disable-next-line
+//     document.execCommand('copy');
+//   });
+// });
+
+$(() => {
+  let linkValue = '';
+
+  $('body').on('click', '#copy-link', (e) => {
+    const link = $(e.currentTarget).siblings('.direct-link');
+    link.click();
+    linkValue = link.attr('href');
+  });
+
+  $('#link-modal').on('show.bs.modal', () => {
+    $('#link').val(linkValue);
   });
 
   $('body').on('click', '#copy-link-btn', () => {
