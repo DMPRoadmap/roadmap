da4033b2be1a6ac4d4e59761104bbd63b84526db
gjacob24 <gjacob@ed.ac.uk>
2023-10-27 16:37:41 +0100

    Issue #3144 - Upgrade to Bootstrap 5 This commit consists of the file changes required for the upgrade from Bootstrap 3 to 5.  A more detailed version is in CHANGELOG.md (and in wiki: https://github.com/DMPRoadmap/roadmap/wiki/Release-notes-for-Bootstrap-5-upgrade)

--- app/helpers/customizable_template_link_helper.rb
+++ app/helpers/customizable_template_link_helper.rb
@@ -5,24 +5,28 @@ module CustomizableTemplateLinkHelper
   # Link to the appropriate customizable template.
   # Default link name set if name not set which can be overwritten.
   # rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity
-  def link_to_customizable_template(name, customization, template)
+  def link_to_customizable_template(name, customization, template, dropdown=true)
     name = nil unless name.present?
+    link_css =  dropdown ? 'dropdown-item px-3' : 'px-3'
 
-    if customization.present?
+    if customization.present? 
+
       if customization.created_at < template.created_at
-        name = _('Transfer customisation') if name.blank?
+        name = name.blank? ? _('Transfer customisation') : name
         link_to name,
                 org_admin_template_customization_transfers_path(customization.id),
-                data: { method: 'post' }
+                data: { method: 'post' },
+                class: link_css
       else
-        name = _('Edit customisation') if name.blank?
-        link_to name, org_admin_template_path(id: customization.id)
+        name = name.blank? ? _('Edit customisation') : name
+        link_to name, org_admin_template_path(id: customization.id), class: link_css
       end
     else
-      name = _('Customise') if name.blank?
+      name = name.blank? ? _('Customise') : name
       link_to name,
               org_admin_template_customizations_path(template.id),
-              'data-method': 'post'
+              'data-method': 'post',
+              class: link_css
     end
   end
   # rubocop:enable Metrics/AbcSize, Metrics/PerceivedComplexity
--- app/javascript/src/utils/sectionUpdate.js
+++ app/javascript/src/utils/sectionUpdate.js
@@ -2,19 +2,24 @@
 export const updateSectionProgress = (id, numSecAnswers, numSecQuestions) => {
   const progressDiv = $(`#section-panel-${id}`).find('.section-status');
   progressDiv.html(`(${numSecAnswers} /  ${numSecQuestions})`);
-  const heading = progressDiv.closest('.panel-heading');
+
+  /**
+   // THIS CODE MAY BE OBSOLETE.
+   // RETAINING IT TILL SURE.
+  const heading = progressDiv.closest('.card-heading');
   if (numSecQuestions === 0) { // disable section if empty
     if (heading.parent().attr('aria-expanded') === 'true') {
       heading.parent().trigger('click');
     }
     heading.addClass('empty-section');
-    heading.closest('.panel').find(`#collapse-${id}`).hide();
-    heading.closest('.panel').find('i.fa-plus, i.fa-minus').removeClass('fa-plus').removeClass('fa-minus');
+    heading.closest('.card').find(`#collapse-${id}`).hide();
+    heading.closest('.card').find('i.fa-plus, i.fa-minus').removeClass('fa-plus').removeClass('fa-minus');
   } else if (heading.hasClass('empty-section')) { // enable section if questions re-added
     heading.removeClass('empty-section');
-    heading.closest('.panel').find('i[aria-hidden="true"]').addClass('fa-plus');
-    heading.closest('.panel').find(`#collapse-${id}`).css('display', '');
+    heading.closest('.card').find('i[aria-hidden="true"]').addClass('fa-plus');
+    heading.closest('.card').find(`#collapse-${id}`).css('display', '');
   }
+  */
 };
 
 // given a question id find the containing div
