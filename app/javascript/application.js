c9a641331f8da94e898bc17927ebdbe813c38451
Benjamin FAURE <benjamin.faure@inist.fr>
2023-11-24 11:17:50 +0100

    fix: minor issues following merge

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -17,8 +17,6 @@ import 'bootstrap-select';
 
 import Rails from '@rails/ujs';
 
-import Rails from '@rails/ujs';
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
