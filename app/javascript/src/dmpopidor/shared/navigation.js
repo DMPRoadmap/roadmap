9950bef863a55f3aa51ae9481e39b6d101d58e92
wilmouths <wilmouthsteven@gmail.com>
2024-01-22 15:45:38 +0100

    refactor: changing singin & signup button behaviors

--- app/javascript/src/dmpopidor/shared/navigation.js
+++ app/javascript/src/dmpopidor/shared/navigation.js
@@ -1,38 +1,13 @@
-document.addEventListener('DOMContentLoaded', function () {
-    window.addEventListener('scroll', (event) => {
+document.addEventListener('DOMContentLoaded', () => {
+  window.addEventListener('scroll', (event) => {
+    const scrollY = window.scrollY;
+    const width = screen.width;
+    const body = document.querySelector('body');
 
-        let scrollY = window.scrollY;
-        let width = screen.width;
-        let body = document.querySelector('body');
-
-        if (scrollY > 50 && width > 767) {
-            body.classList.add('is-scrolled');
-        } else {
-            body.classList.remove('is-scrolled');
-        }
-
-    });
-
-    try {
-        const button = document.getElementById('create-account-over');
-        const formRegister = document.getElementById('create-account-form');
-        const div = document.querySelector('div.col-md-12 div.sign-in div.tab-content');
-
-
-        button.addEventListener('click', () => {
-
-            button.style.display = "none";
-            div.style.alignItems = "flex-start";
-            formRegister.style.opacity = "0";
-            formRegister.style.transition = "visibility 0s 2s, opacity 2s linear;";
-            formRegister.style.display = "block";
-
-            setTimeout(() => {
-                formRegister.style.opacity = "1";
-
-            }, 100)
-        });
-    } catch (e) {
-
+    if (scrollY > 50 && width > 767) {
+      body.classList.add('is-scrolled');
+    } else {
+      body.classList.remove('is-scrolled');
     }
+  });
 });
