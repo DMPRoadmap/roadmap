35eeac5ca9381a44ae875309dddabde4840d86dd
wilmouths <wilmouthsteven@gmail.com>
2024-02-27 16:02:50 +0100

    fix: plan creation, replace schemas names

--- app/models/dmpopidor/plan.rb
+++ app/models/dmpopidor/plan.rb
@@ -93,7 +93,7 @@ module Dmpopidor
           },
           dmp_id: dmp_fragment.id,
           parent_id: nil,
-          madmp_schema: MadmpSchema.find_by(name: 'CoordinatorStardard'),
+          madmp_schema: MadmpSchema.find_by(name: 'ContributorStandard'),
           additional_info: { property_name: 'contact' }
         )
 
@@ -139,7 +139,7 @@ module Dmpopidor
         },
         dmp_id: dmp_id,
         parent_id: nil,
-        madmp_schema: MadmpSchema.find_by(name: 'CoordinatorStardard'),
+        madmp_schema: MadmpSchema.find_by(name: 'ContributorStandard'),
         additional_info: { property_name: 'principalInvestigator' }
       )
 
