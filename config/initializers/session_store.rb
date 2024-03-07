fcce26d66573a53d575a8ba0c8982a11a5f3ab24
Benjamin FAURE <benjamin.faure@inist.fr>
2024-03-07 10:37:12 +0100

    feat: added redis as a session store

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 # Be sure to restart your server when you modify this file.
+secure = Rails.env.production?
+domain = ENV.fetch('DMPROADMAP_HOST', 'dmp.opidor.fr')
 
-Rails.application.config.session_store :cookie_store, key: '_dmp_roadmap_session',
-                                                      same_site: :lax
+Rails.application.config.session_store :redis_store, 
+                                       servers: ENV['REDIS_URL'],
+                                       expire_after: 90.minutes,
+                                       key: '_dmp_opidor_session',
+                                       threadsafe: false,
+                                       secure: secure
