6908ecb732616b30634a855b8e5a9bd76e3f8e6f
Benjamin FAURE <benjamin.faure@inist.fr>
2024-03-26 13:22:06 +0100

    fix: codebase messages are now in plan locale

--- engines/madmp_opidor/app/controllers/madmp_codebase_controller.rb
+++ engines/madmp_opidor/app/controllers/madmp_codebase_controller.rb
@@ -8,6 +8,7 @@ class MadmpCodebaseController < ApplicationController
   # rubocop:disable Metrics/PerceivedComplexity
   def run
     fragment = MadmpFragment.find(params[:fragment_id])
+    plan = fragment.plan
     script_name = params[:script_name]
     schema_run = fragment.madmp_schema.extract_run_parameters(script_name:)
     script_params = schema_run['params'] || {}
@@ -15,57 +16,59 @@ class MadmpCodebaseController < ApplicationController
 
     authorize fragment
 
-    # EXAMPLE DATA
-    # file_path = Rails.root.join("engines/madmp_opidor/config/example_data/codebase_example_data.json")
-    # response = JSON.load(File.open(file_path))
-    # fragment.raw_import(response, fragment.madmp_schema)
-    # render json: {
-    #   'fragment' => fragment.get_full_fragment(with_ids: true),
-    #   'needs_reload' => true
-    # }, status: 200
-    # return
-    # render json: {
-    #   'message' => _('Notification has been sent'),
-    #   'needs_reload' => false
-    # }, status: 200
-    # return
+    I18n.with_locale plan.template.locale do 
+      # EXAMPLE DATA
+      # file_path = Rails.root.join("engines/madmp_opidor/config/example_data/codebase_example_data.json")
+      # response = JSON.load(File.open(file_path))
+      # fragment.raw_import(response, fragment.madmp_schema)
+      # render json: {
+      #   'fragment' => fragment.get_full_fragment(with_ids: true),
+      #   'needs_reload' => true
+      # }, status: 200
+      # return
+      # render json: {
+      #   'message' => _('Notification has been sent'),
+      #   'needs_reload' => false
+      # }, status: 200
+      # return
 
-    fragment.plan.add_api_client!(fragment.madmp_schema.api_client) if script_name.downcase.include?('notifyer')
-    begin
-      response = fetch_run_data(fragment, script_name, script_owner, body: {
-        data: fragment.data,
-        schema: fragment.madmp_schema.schema,
-        dmp_language: fragment.dmp.locale,
-        dmp_id: fragment.dmp_id,
-        research_output_id: fragment.research_output_fragment&.id,
-        params: script_params.merge({ ro_uuid: fragment.research_output&.uuid })
-      })
+      fragment.plan.add_api_client!(fragment.madmp_schema.api_client) if script_name.downcase.include?('notifyer')
+      begin
+        response = fetch_run_data(fragment, script_name, script_owner, body: {
+          data: fragment.data,
+          schema: fragment.madmp_schema.schema,
+          dmp_language: fragment.dmp.locale,
+          dmp_id: fragment.dmp_id,
+          research_output_id: fragment.research_output_fragment&.id,
+          params: script_params.merge({ ro_uuid: fragment.research_output&.uuid })
+        })
 
-      if response['return_code'].eql?(0)
-        if response['data'].empty?
-          render json: {
-            'message' => _('Notification has been sent'),
-            'needs_reload' => false
-          }, status: 200
+        if response['return_code'].eql?(0)
+          if response['data'].empty?
+            render json: {
+              'message' => _('Notification has been sent'),
+              'needs_reload' => false
+            }, status: 200
+          else
+            fragment.import_with_instructions(response['data'], fragment.madmp_schema)
+            render json: {
+              'fragment' => fragment.get_full_fragment(with_ids: true),
+              'needs_reload' => true
+            }, status: 200
+          end
+          update_run_log(fragment, script_name)
         else
-          fragment.import_with_instructions(response['data'], fragment.madmp_schema)
+          # Rails.cache.delete(["codebase_run", fragment.id])
           render json: {
-            'fragment' => fragment.get_full_fragment(with_ids: true),
-            'needs_reload' => true
-          }, status: 200
+            'error' => "#{_('An error has occured: ')} #{response['result_message']}"
+          }, status: 500
         end
-        update_run_log(fragment, script_name)
-      else
+      rescue StandardError => e
         # Rails.cache.delete(["codebase_run", fragment.id])
         render json: {
-          'error' => "#{_('An error has occured: ')} #{response['result_message']}"
+          'error' => e.message
         }, status: 500
       end
-    rescue StandardError => e
-      # Rails.cache.delete(["codebase_run", fragment.id])
-      render json: {
-        'error' => e.message
-      }, status: 500
     end
   end
   # rubocop:enable Metrics/PerceivedComplexity
@@ -75,71 +78,75 @@ class MadmpCodebaseController < ApplicationController
   def project_search
     project_id = params[:project_id]
     fragment = MadmpFragment.find(params[:fragment_id])
+    plan = fragment.plan
     dmp_fragment = fragment.dmp
     script_name = params[:script_name]
 
     authorize fragment
-    # EXAMPLE DATA
-    # begin
-    #   file_path = Rails.root.join("engines/madmp_opidor/config/example_data/anr_example_data.json")
-    #   response = JSON.load(File.open(file_path))
-    #   dmp_fragment.raw_import(response, dmp_fragment.madmp_schema)
-    #   dmp_fragment.update_meta_fragment
+    
+    I18n.with_locale plan.template.locale do 
+      # EXAMPLE DATA
+      # begin
+      #   file_path = Rails.root.join("engines/madmp_opidor/config/example_data/anr_example_data.json")
+      #   response = JSON.load(File.open(file_path))
+      #   dmp_fragment.raw_import(response, dmp_fragment.madmp_schema)
+      #   dmp_fragment.update_meta_fragment
 
-    #   render json: {
-    #     'fragment' => dmp_fragment.get_full_fragment,
-    #     'persons' => dmp_fragment.persons.map do |f|
-    #       {
-    #         **f.get_full_fragment(with_ids: true),
-    #         'to_string' => f.to_s,
-    #       }
-    #     end,
-    #     "message" => _('New data have been added to your plan, please click on the "Reload" button.')
-    #   }, status: 200
-    # rescue StandardError => e
-    #   # Rails.cache.delete(["codebase_run", fragment.id])
-    #   render json: {
-    #     'error' => "Internal Server error: #{e.message}"
-    #   }, status: 500
-    # end
-    # return
+      #   render json: {
+      #     'fragment' => dmp_fragment.get_full_fragment,
+      #     'persons' => dmp_fragment.persons.map do |f|
+      #       {
+      #         **f.get_full_fragment(with_ids: true),
+      #         'to_string' => f.to_s,
+      #       }
+      #     end,
+      #     "message" => _('New data have been added to your plan, please click on the "Reload" button.')
+      #   }, status: 200
+      # rescue StandardError => e
+      #   # Rails.cache.delete(["codebase_run", fragment.id])
+      #   render json: {
+      #     'error' => "Internal Server error: #{e.message}"
+      #   }, status: 500
+      # end
+      # return
 
-    begin
-      response = fetch_run_data(fragment, script_name, 'superadmin', body: {
-        data: { grantId: project_id },
-        dmp_language: fragment.dmp.locale,
-        # schema: fragment.madmp_schema.schema,
-        # dmp_id: fragment.dmp_id,
-        # research_output_id: fragment.research_output_fragment&.id,
-        # params: params.merge({ ro_uuid: fragment.research_output&.uuid })
-      }, params: {})
+      begin
+        response = fetch_run_data(fragment, script_name, 'superadmin', body: {
+          data: { grantId: project_id },
+          dmp_language: fragment.dmp.locale,
+          # schema: fragment.madmp_schema.schema,
+          # dmp_id: fragment.dmp_id,
+          # research_output_id: fragment.research_output_fragment&.id,
+          # params: params.merge({ ro_uuid: fragment.research_output&.uuid })
+        }, params: {})
 
-      if response['return_code'].eql?(0)
-        dmp_fragment.raw_import(response['data'], dmp_fragment.madmp_schema)
-        dmp_fragment.update_meta_fragment
-        render json: {
-          'fragment' => dmp_fragment.get_full_fragment(with_ids: true),
-          'persons' => dmp_fragment.persons.map do |f|
-            {
-              **f.get_full_fragment(with_ids: true),
-              'to_string' => f.to_s,
-            }
-          end,
-          'plan_title' => dmp_fragment.meta.data['title'],
-          'message' => _('Project data have successfully been imported.')
-        }, status: 200
-        update_run_log(dmp_fragment, script_name)
-      else
+        if response['return_code'].eql?(0)
+          dmp_fragment.raw_import(response['data'], dmp_fragment.madmp_schema)
+          dmp_fragment.update_meta_fragment
+          render json: {
+            'fragment' => dmp_fragment.get_full_fragment(with_ids: true),
+            'persons' => dmp_fragment.persons.map do |f|
+              {
+                **f.get_full_fragment(with_ids: true),
+                'to_string' => f.to_s,
+              }
+            end,
+            'plan_title' => dmp_fragment.meta.data['title'],
+            'message' => _('Project data have successfully been imported.')
+          }, status: 200
+          update_run_log(dmp_fragment, script_name)
+        else
+          # Rails.cache.delete(["codebase_run", fragment.id])
+          render json: {
+            'error' => "#{_('An error has occured: ')} #{response['result_message']}"
+          }, status: 500
+        end
+      rescue StandardError => e
         # Rails.cache.delete(["codebase_run", fragment.id])
         render json: {
-          'error' => "#{_('An error has occured: ')} #{response['result_message']}"
+          'error' => "Internal Server error: #{e.message}"
         }, status: 500
       end
-    rescue StandardError => e
-      # Rails.cache.delete(["codebase_run", fragment.id])
-      render json: {
-        'error' => "Internal Server error: #{e.message}"
-      }, status: 500
     end
   end
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
