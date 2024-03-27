90cf3b1eaae8396e32cfbf32d6b03a2e9483d0b4
briri <brian.riley@ucop.edu>
2024-03-26 17:21:40 -0700

    fixes for the typeaheads that feed the react pages

--- app/controllers/api/v3/typeaheads_controller.rb
+++ app/controllers/api/v3/typeaheads_controller.rb
@@ -29,6 +29,10 @@ module Api
         end
 
         @items = process_results(term: term, matches: matches)
+        term_matched = @items.select { |it| it.name&.split(' (')&.first&.downcase&.strip == term.downcase.strip }.any?
+
+        # Add the search term if it was not included in the results already
+        @items.unshift(Org.new(name: term)) unless term_matched
         @use_funder_context = true
         render json: render_to_string(template: '/api/v3/typeaheads/index'), status: :ok
       rescue StandardError => e
@@ -55,6 +59,11 @@ module Api
 
         # Prepare the results
         @items = process_results(term: term, matches: matches)
+        term_matched = @items.select { |it| it.name&.split(' (')&.first&.downcase&.strip == term.downcase.strip }.any?
+
+        # Add the search term if it was not included in the results already
+        @items.unshift(Org.new(name: term)) unless term_matched
+
         render json: render_to_string(template: '/api/v3/typeaheads/index'), status: :ok
       rescue StandardError => e
         Rails.logger.error "Failure in Api::V3::TypeaheadsController.orgs #{e.message}"
@@ -73,6 +82,11 @@ module Api
         matches = matches.sort { |a, b| b.research_outputs&.length <=> a.research_outputs&.length }
 
         @items = process_results(term: term, matches: matches)
+        term_matched = @items.select { |it| it.name&.split(' (')&.first&.downcase&.strip == term.downcase.strip }.any?
+
+        # Add the search term if it was not included in the results already
+        @items.unshift(Org.new(name: term)) unless term_matched
+
         render json: render_to_string(template: '/api/v3/typeaheads/index'), status: :ok
       rescue StandardError => e
         Rails.logger.error "Failure in Api::V3::TypeaheadsController.repositories #{e.message}"
@@ -114,7 +128,6 @@ module Api
       def process_results(term:, matches: [])
         results = deduplicate(term: term, list: matches)
         results.map(&:name).flatten.compact.uniq
-
         paginate_response(results: results)
       end
 
