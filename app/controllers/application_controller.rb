185e8aa0c01216547620d974555adfb69efdc0b2
aaronskiba <askiba@ualberta.ca>
2024-04-30 11:35:07 -0600

    Remove duplicate code in application_controller.rb
    Removing `rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized` from line 25, because the exact statement also exists on line 20 of this same file.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -22,8 +22,6 @@ class ApplicationController < ActionController::Base
   # When we are in production reroute Record Not Found errors to the branded 404 page
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
 
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
-
   private
 
   def current_org
