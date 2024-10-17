a0b3e0cd53ded494d63a491e181b382f9e8e90dc
aaronskiba <askiba@ualberta.ca>
2024-10-17 16:49:39 -0600

    Test custom I18n strings / override default_locale
    In order to properly test the values of our customised `I18n.t` values (specifically, `I18n.t('devise.failure.unconfirmed')` and `I18n.t('devise.registrations.signed_up_but_unconfirmed')`), we need to set `I18n.default_locale` to match our app's default locale.
    
    NOTE: These changes only set `I18n.default_locale = :'en-CA'` for the duration of the email confirmation tests. However, it may be best to apply this update globally throughout the tests.

--- spec/integration/email_confirmation_spec.rb
+++ spec/integration/email_confirmation_spec.rb
@@ -6,6 +6,15 @@ require 'rails_helper'
 # (See `app/controllers/concerns/email_confirmation_handler.rb`)
 # Here, we define "unconfirmable" as a user that is both unconfirmed and has no outstanding confirmation token
 RSpec.describe 'Email Confirmation', type: :feature do
+  before(:each) do
+    @default_locale = I18n.default_locale
+    I18n.default_locale = :'en-CA'
+  end
+
+  after(:each) do
+    I18n.default_locale = @default_locale
+  end
+
   scenario 'A user attempts to sign in via the "Sign In" button. However, they are unconfirmable.', :js do
     # Setup
     user = create(:user, :unconfirmable)
@@ -16,6 +25,12 @@ RSpec.describe 'Email Confirmation', type: :feature do
 
     # Expectations
     expectations_for_unconfirmable_user_after_sign_in_attempt(user)
+
+    # Actions
+    sign_in(user)
+
+    # Expectations
+    expect_request_new_confirmation_link_message
   end
 
   scenario 'A user attempts to sign in via the "Sign in with institutional or social ID"
@@ -35,6 +50,12 @@ RSpec.describe 'Email Confirmation', type: :feature do
     expectations_for_unconfirmable_user_after_sign_in_attempt(user)
     # An Identifier entry was not created
     expect(Identifier.count).to be_zero
+
+    # Actions
+    click_link 'Sign in with institutional or social ID'
+
+    # Expectations
+    expect_request_new_confirmation_link_message
   end
 
   private
@@ -46,5 +67,20 @@ RSpec.describe 'Email Confirmation', type: :feature do
     expect(user.confirmation_token).to be_present
     # The user is not signed in
     expect(current_path).to eq(root_path)
+    # The correct flash message was rendered
+    expect_confirmation_link_has_been_sent_message
+  end
+
+  def expect_confirmation_link_has_been_sent_message
+    msg = 'A message with a confirmation link has been sent to your email address. ' \
+          'Please open the link to activate your account. ' \
+          'If you do not receive the confirmation email, please check your spam filter.'
+    expect(page.text).to have_text(msg)
+  end
+
+  def expect_request_new_confirmation_link_message
+    msg = 'You have to confirm your account before continuing. ' \
+          '(Click to request a new confirmation email)'
+    expect(page.text).to have_text(msg)
   end
 end
