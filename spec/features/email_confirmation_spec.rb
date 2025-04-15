e1658e7b2bd4d3d77456a93a964a5b8007ce4f8d
aaronskiba <askiba@ualberta.ca>
2025-04-15 16:37:54 -0600

    Update email conf test file for DMPRoadmap
    - Removed the 'A user attempts to sign in via the "Sign in with institutional or social ID"...' test which is specific to DMP Assistant functionality.
    - Removed hardcoding of I18n.default_locale = :'en-CA'
      - However, the current test config is preventing us from using the `I18n.default_locale` value that is specific to the app. This in turn is causing issues with I18n.t() using the desired locale.
        - https://github.com/DMPRoadmap/roadmap/pull/3511 seeks to remedy the above issues.

--- spec/features/email_confirmation_spec.rb
+++ spec/features/email_confirmation_spec.rb
@@ -6,81 +6,24 @@ require 'rails_helper'
 # (See `app/controllers/concerns/email_confirmation_handler.rb`)
 # Here, we define "unconfirmable" as a user that is both unconfirmed and has no outstanding confirmation token
 RSpec.describe 'Email Confirmation', type: :feature do
-  before(:each) do
-    @default_locale = I18n.default_locale
-    I18n.default_locale = :'en-CA'
-  end
-
-  after(:each) do
-    I18n.default_locale = @default_locale
-  end
-
   scenario 'A user attempts to sign in via the "Sign In" button. However, they are unconfirmable.', :js do
-    # Setup
     user = create(:user, :unconfirmable)
-
-    # Actions
-    sign_in(user)
-    user.reload
-
-    # Expectations
-    expectations_for_unconfirmable_user_after_sign_in_attempt(user)
-
-    # Actions
     sign_in(user)
 
-    # Expectations
-    expect_request_new_confirmation_link_message
-  end
-
-  scenario 'A user attempts to sign in via the "Sign in with institutional or social ID"
-            button with an email that is not currently linked to any account. The chosen
-            SSO email matches an existing user account email. However, they are unconfirmable.', :js do
-    # Setup
-    Rails.application.env_config['devise.mapping'] = Devise.mappings[:user]
-    Rails.application.env_config['omniauth.auth'] = OmniAuth.config.mock_auth[:openid_connect]
-    user = create(:user, :unconfirmable, email: OmniAuth.config.mock_auth[:openid_connect][:info][:email])
-
-    # Actions
-    visit root_path
-    click_link 'Sign in with institutional or social ID'
+    # A flash notice is displayed informing the user that a confirmation email has been sent
+    expect(page.text).to have_text(I18n.t('devise.registrations.signed_up_but_unconfirmed'))
+    # reload the user to check confirmation values
     user.reload
 
-    # Expectations
-    expectations_for_unconfirmable_user_after_sign_in_attempt(user)
-    # An Identifier entry was not created
-    expect(Identifier.count).to be_zero
-
-    # Actions
-    click_link 'Sign in with institutional or social ID'
-
-    # Expectations
-    expect_request_new_confirmation_link_message
-  end
-
-  private
-
-  def expectations_for_unconfirmable_user_after_sign_in_attempt(user)
     # The user remains unconfirmed
     expect(user.confirmed?).to be(false)
     # A confirmation_token now exists
     expect(user.confirmation_token).to be_present
     # The user is not signed in
     expect(current_path).to eq(root_path)
-    # The correct flash message was rendered
-    expect_confirmation_link_has_been_sent_message
-  end
 
-  def expect_confirmation_link_has_been_sent_message
-    msg = 'A message with a confirmation link has been sent to your email address. ' \
-          'Please open the link to activate your account. ' \
-          'If you do not receive the confirmation email, please check your spam filter.'
-    expect(page.text).to have_text(msg)
-  end
-
-  def expect_request_new_confirmation_link_message
-    msg = 'You need to confirm your account before continuing. ' \
-          '(Click to request a new confirmation email)'
-    expect(page.text).to have_text(msg)
+    sign_in(user)
+    # A flash warning is displayed informing the user that they have to confirm their email
+    expect(page.text).to have_text(I18n.t('devise.failure.unconfirmed'))
   end
 end
