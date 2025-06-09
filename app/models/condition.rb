cd2f92560fe9e9d7703a7e714ef4a7925cdf9a88
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-09 16:37:01 +0100

    updated deep-copy method

--- app/models/condition.rb
+++ app/models/condition.rb
@@ -27,19 +27,25 @@
 # Object that represents a condition of a conditional question
 class Condition < ApplicationRecord
   belongs_to :question
-  enum :action_type, { remove: 0, add_webhook: 1 }
-  serialize :option_list, type: Array, coder: JSON
-  serialize :remove_data, type: Array, coder: JSON
-  serialize :webhook_data, coder: JSON
+  enum action_type: %i[remove add_webhook]
+  serialize :option_list, Array
+  serialize :remove_data, Array
+  serialize :webhook_data, JSON
 
   # Sort order: Number ASC
   default_scope { order(number: :asc) }
-
+  # rubocop:disable Metrics/AbcSize
   def deep_copy(**options)
     copy = dup
     copy.question_id = options.fetch(:question_id, nil)
+    # Added to allow options to be passed in for all fields
+    copy.option_list = options.fetch(:option_list, option_list) if options.key?(:option_list)
+    copy.remove_data = options.fetch(:remove_data, remove_data) if options.key?(:remove_data)
+    copy.action_type = options.fetch(:action_type, action_type) if options.key?(:action_type)
+    copy.webhook_data = options.fetch(:webhook_data, webhook_data) if options.key?(:webhook_data)
     # TODO: why call validate false here
     copy.save!(validate: false) if options.fetch(:save, false)
     copy
   end
+  # rubocop:enable Metrics/AbcSize
 end
