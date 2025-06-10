a7ced0f0f7ef057c6c47e5a43e5f3bde34ee9397
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-10 22:26:10 +0100

    updated babel plugin

--- babel.config.js
+++ babel.config.js
@@ -53,7 +53,7 @@ module.exports = function(api) {
         }
       ],
       [
-        require.resolve('@babel/plugin-proposal-private-property-in-object'),
+        '@babel/plugin-transform-private-property-in-object',
         {
           loose: true
         }
