6b47430e21b5a283c3a3c7cbec4f5a246441762a
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-10 22:12:13 +0100

    added require-resolve

--- babel.config.js
+++ babel.config.js
@@ -53,7 +53,7 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-property-in-object',
+        require.resolve('@babel/plugin-proposal-private-property-in-object'),
         {
           loose: true
         }
