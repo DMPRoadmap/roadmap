74584131848ee92da06af81170c112205f38eb79
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-06-13 11:48:16 +0100

    updated rail-credentials reference to an env-variable reference

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -5,7 +5,7 @@ require 'recaptcha/rails'
 # the keys are set in config/credentials.yml.env
 
 Recaptcha.configure do |config|
-  config.site_key = Rails.application.credentials.recaptcha[:site_key]
-  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
+  config.site_key = ENV["RECAPTCHA_SITE_KEY"]
+  config.secret_key = ENV["RECAPTCHA_SECRET_KEY"]
   config.proxy = 'http://someproxy.com:port'
 end
