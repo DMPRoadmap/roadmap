d125ae8241825be35ffc0351f58966794809a928
John Pinto <jpinto@newington.edina.ac.uk>
2025-07-09 11:22:52 +0100

    Annotated new models OrgDomain and OrgRor with schema information.

--- app/models/org_domain.rb
+++ app/models/org_domain.rb
@@ -1,4 +1,23 @@
 # frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: org_domains
+#
+#  id         :bigint(8)        not null, primary key
+#  domain     :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  org_id     :bigint(8)        not null
+#
+# Indexes
+#
+#  index_org_domains_on_org_id  (org_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (org_id => orgs.id)
+#
 class OrgDomain < ApplicationRecord
   belongs_to :org
   
--- app/models/org_ror.rb
+++ app/models/org_ror.rb
@@ -1,5 +1,23 @@
+# frozen_string_literal: true
 
-
+# == Schema Information
+#
+# Table name: org_rors
+#
+#  id         :bigint(8)        not null, primary key
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  org_id     :bigint(8)        not null
+#  ror_id     :text             not null
+#
+# Indexes
+#
+#  index_org_rors_on_org_id  (org_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (org_id => orgs.id)
+#
 class OrgRor < ApplicationRecord
   belongs_to :org
 
