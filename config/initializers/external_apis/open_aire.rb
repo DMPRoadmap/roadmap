82814402757538c5b4ff477c1f73fda1683f5163
dstuckey-uoe <dstuckey@ed.ac.uk>
2025-08-25 13:45:37 +0100

    added doi-integration key-values

--- config/initializers/external_apis/open_aire.rb
+++ config/initializers/external_apis/open_aire.rb
@@ -4,7 +4,10 @@
 # Open Aire Research Project Registry API. For more information about
 # the API and to verify that your configuration settings are correct.
 Rails.configuration.x.open_aire.api_base_url = 'https://api.openaire.eu/'
-# The api_url should contain `%s. This is where the funder is appended!
-Rails.configuration.x.open_aire.search_path = 'projects/dspace/%s/ALL/ALL'
-Rails.configuration.x.open_aire.default_funder = 'H2020'
-Rails.configuration.x.open_aire.active = true
+
+#DOI integration
+Rails.configuration.x.open_aire.client_id = ENV["OPEN_AIRE_CLIENT_ID"]
+Rails.configuration.x.open_aire.client_secret = ENV["OPEN_AIRE_CLIENT_SECRET"]
+Rails.configuration.x.open_aire.grant_type = 'client_credentials'
+Rails.configuration.x.open_aire.open_aire_access_token_endpoint = 'https://aai.openaire.eu/oidc/token'
+Rails.configuration.x.open_aire.research_outputs_search_path = 'search/researchProducts'
