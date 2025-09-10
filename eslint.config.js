7cc07067218fed5635d3fc3c38b28ef3d0e41ce2
Andrea Davanzo <andrea.davanzo@ed.ac.uk>
2025-09-10 15:52:43 +0100

    update js stuff

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: "latest",   // support modern JS
+      sourceType: "module",    // for import/export
+    },
+    rules: {
+      // "no-use-before-define": ["error", { functions: false, classes: true, variables: true }],
+
+    },
+  },
+];
