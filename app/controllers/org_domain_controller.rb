07a2231cfd92ffa1f7bf4b9bde06dcf7eb472356
John Pinto <jpinto@newington.edina.ac.uk>
2025-10-07 13:20:52 +0100

    Added functionality to org_domain_controller.rb, if no orgs found in first call to the Orion service then we repeat the call to higher level domains until we get to domains with 2 parts, e.g., efe.xyz.abc.com ->... --> abc.com.

--- app/controllers/org_domain_controller.rb
+++ app/controllers/org_domain_controller.rb
@@ -14,7 +14,7 @@ class OrgDomainController < ApplicationController
     # check if org exists already using domain provided
     org_results = OrgDomain.search_with_org_info(email_domain)
     result = org_results.map { |record|
-    org_id_new_format = {id: record.id, name: record.org_name}.to_json
+      org_id_new_format = { id: record.id, name: record.org_name }.to_json
 
       {
         id: org_id_new_format,
@@ -36,6 +36,17 @@ class OrgDomainController < ApplicationController
       full_org_json = ::ExternalApis::OrionService.search_by_domain(email_domain)
       puts "full_org_json: #{full_org_json}"
 
+      # If no orgs found, retry with higher level domain by removing subdomains
+      split_domain = email_domain.split('.')
+
+      while !full_org_json&.key?('orgs') && split_domain.length > 2
+        split_domain.shift
+        domain_to_search = split_domain.join('.')
+        puts "Retrying with #{domain_to_search}"
+        full_org_json = ::ExternalApis::OrionService.search_by_domain(domain_to_search)
+        puts "Retry full_org_json with #{domain_to_search}: #{full_org_json}"
+      end
+
       unless full_org_json&.key?('orgs')
         puts 'Invalid response or no orgs key found'
         # Add Other org
@@ -45,10 +56,10 @@ class OrgDomainController < ApplicationController
       end
 
       # Extract the values from API result
-      result = full_org_json['orgs'].map do |org| 
+      result = full_org_json['orgs'].map do |org|
         #  The ror_display value will be in the language of the country, and should always be present.
         ror_display_name_json = org['names'].find { |n| n['lang'] && n['types']&.include?('ror_display') }
-        #puts "ror_display_name_json: #{ror_display_name_json}"
+        # puts "ror_display_name_json: #{ror_display_name_json}"
         org_name = ror_display_name_json ? ror_display_name_json['value'] : nil
         puts "org_name: #{org_name}"
 
@@ -59,9 +70,10 @@ class OrgDomainController < ApplicationController
         {
           id: org_id_new_format,
           org_name: org_name,
-          domain: '',
+          domain: ''
         }
-      rescue => e
+
+      rescue StandardError => e
         puts "Failed request: #{e.message}"
       end
 
@@ -72,22 +84,23 @@ class OrgDomainController < ApplicationController
     end
     render json: result, status: :ok
   end
+
   # rubocop:enable Metrics/AbcSize, Metrics/MethodLength, Metrics/PerceivedComplexity, Metrics/CyclomaticComplexity
 
   def show
     @org_domains = OrgDomain.where(org_id: current_user.org_id).order(:domain)
-
   end
 
   def new
     @org_domain = OrgDomain.new
   end
 
+  # rubocop:disable Metrics/AbcSize
   def create
     domain_input = params[:org_domain][:domain].to_s.downcase.gsub(/\s+/, '')
 
     if domain_input.blank?
-      flash.now[:alert] = "Domain can't be blank."
+      flash.now[:alert] = 'Domain cannot be blank.'
       @org_domain = OrgDomain.new
       render :new and return
     end
@@ -96,53 +109,55 @@ class OrgDomainController < ApplicationController
     @org_domain.org_id = current_user.org_id
 
     if @org_domain.save
-      redirect_to org_domain_show_path, notice: "Domain created successfully."
+      redirect_to org_domain_show_path, notice: 'Domain created successfully.'
     else
       render :new
     end
   end
+  # rubocop:enable Metrics/AbcSize
 
   def edit
     @org_domain = OrgDomain.find(params[:id])
-    redirect_to org_domain_show_path, alert: "Unauthorized" unless @org_domain.org_id == current_user.org_id
+    redirect_to org_domain_show_path, alert: 'Unauthorized' unless @org_domain.org_id == current_user.org_id
   end
 
+  # rubocop:disable Metrics/AbcSize
   def update
     @org_domain = OrgDomain.find(params[:id])
-  
-    if @org_domain.org_id != current_user.org_id
-      redirect_to org_domain_show_path, alert: "Unauthorized"
-    else
+
+    if @org_domain.org_id == current_user.org_id
       domain_input = params[:org_domain][:domain].to_s.downcase.gsub(/\s+/, '')
-  
+
       if domain_input.blank?
-        flash.now[:alert] = "Domain can't be blank."
+        flash.now[:alert] = 'Domain cannot be blank.'
         render :edit and return
       end
-  
+
       if @org_domain.update(domain: domain_input)
-        redirect_to org_domain_show_path, notice: "Domain updated successfully."
+        redirect_to org_domain_show_path, notice: 'Domain updated successfully.'
       else
         render :edit
       end
+    else
+      redirect_to org_domain_show_path, alert: 'Unauthorized'
     end
   end
-  
+  # rubocop:enable Metrics/AbcSize
+
   def destroy
     @org_domain = OrgDomain.find(params[:id])
-  
+
     if @org_domain.org_id != current_user.org_id
-      redirect_to org_domain_show_path, alert: "Unauthorized"
+      redirect_to org_domain_show_path, alert: 'Unauthorized'
       return
     end
-  
+
     if @org_domain.destroy
-      redirect_to org_domain_show_path, notice: "Domain deleted successfully."
+      redirect_to org_domain_show_path, notice: 'Domain deleted successfully.'
     else
-      redirect_to org_domain_show_path, alert: "Failed to delete domain."
+      redirect_to org_domain_show_path, alert: 'Failed to delete domain.'
     end
   end
-  
 
   private
 
@@ -157,17 +172,12 @@ class OrgDomainController < ApplicationController
 
   def other_org_json
     other_org = Org.find_other_org
-    #add if condition here to check if other_org is nil or present
-    if other_org.present?
-      org_id_new_format = { id: other_org.id, name: other_org.name }.to_json
-    else
-      org_id_new_format = { name: "Other" }.to_json
-    end
-      {
-        id: org_id_new_format,
-        org_name: other_org ? other_org.name : "Other",
-        domain: "",
-      }
+    # add if condition here to check if other_org is nil or present
+    org_id_new_format = other_org.present? ? { id: other_org.id, name: other_org.name }.to_json : { name: 'Other' }.to_json
+    {
+      id: org_id_new_format,
+      org_name: other_org ? other_org.name : 'Other',
+      domain: ''
+    }
   end
 end
-
