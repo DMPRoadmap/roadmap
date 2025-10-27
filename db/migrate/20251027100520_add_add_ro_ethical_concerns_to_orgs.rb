bdbfefa55a478c6b121edc0869c71e43a05c0d1d
John Pinto <jpinto@newington.edina.ac.uk>
2025-10-27 10:12:50 +0000

    Updated migration add_add_ro_ethical_concerns_to_orgs for Rails version 7.1.

--- db/migrate/20251027100520_add_add_ro_ethical_concerns_to_orgs.rb
+++ db/migrate/20251027100520_add_add_ro_ethical_concerns_to_orgs.rb
@@ -1,4 +1,4 @@
-class AddAddRoEthicalConcernsToOrgs < ActiveRecord::Migration[6.1]
+class AddAddRoEthicalConcernsToOrgs < ActiveRecord::Migration[7.1]
   def change
     add_column :orgs, :add_ro_ethical_concerns, :boolean, default: true, null: false
     Org.update_all(add_ro_ethical_concerns: true)
